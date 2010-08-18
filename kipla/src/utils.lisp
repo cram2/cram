2c991dca7a5603d4bd47d191c0978e5b85fffbe2
Lorenz Mösenlechner <moesenle@in.tum.de>
2010-08-18 16:57:14 +0200

    Added utility function to turn cl_tf and cl_transform types into jlos

--- kipla/src/utils.lisp
+++ kipla/src/utils.lisp
@@ -26,3 +26,34 @@
                     (princ (char-downcase ch) strm))
                 (setf upcase nil)))))
 
+(defmethod pose->jlo ((p cl-transforms:pose))
+  (let ((p-matrix (cl-transforms:transform->matrix p)))
+    (jlo:make-jlo :parent "/map" :pose (make-array (array-total-size p-matrix)
+                                                   :displaced-to p-matrix
+                                                   :element-type (array-element-type p-matrix)))))
+
+(defmethod pose->jlo ((p cl-transforms:pose))
+  (pose->jlo (cl-transforms:make-transform
+              (cl-transforms:origin p)
+              (cl-transforms:orientation p))))
+
+(defmethod pose->jlo ((p cl-transforms:transform))
+  (let ((p-matrix (cl-transforms:transform->matrix p)))
+    (jlo:make-jlo :parent "/map" :pose (make-array (array-total-size p-matrix)
+                                                   :displaced-to p-matrix
+                                                   :element-type (array-element-type p-matrix)))))
+
+(defmethod pose->jlo ((p cl-tf:stamped-transform))
+  (let ((p-matrix (cl-transforms:transform->matrix p)))
+    (jlo:make-jlo :parent (cl-tf:frame-id p)
+                  :name (cl-tf:child-frame-id p)
+                  :pose (make-array (array-total-size p-matrix)
+                                    :displaced-to p-matrix
+                                    :element-type (array-element-type p-matrix)))))
+
+(defmethod pose->jlo ((p cl-tf:pose-stamped))
+  (let ((p-matrix (cl-transforms:transform->matrix p)))
+    (jlo:make-jlo :parent (cl-tf:frame-id p)
+                  :pose (make-array (array-total-size p-matrix)
+                                    :displaced-to p-matrix
+                                    :element-type (array-element-type p-matrix)))))
