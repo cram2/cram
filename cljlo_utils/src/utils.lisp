7e3d4bd6578cd81201334280f663ef029de272da
Lorenz Moesenlechner <moesenle@in.tum.de>
2010-09-22 16:46:35 +0200

    Moved jlo convertion stuff into separate package

--- cljlo_utils/src/utils.lisp
+++ cljlo_utils/src/utils.lisp
@@ -1,5 +1,33 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of Willow Garage, Inc. nor the names of its
+;;;       contributors may be used to endorse or promote products derived from
+;;;       this software without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
 
-(in-package :kipla-utils)
+(in-package :cljlo-utils)
 
 (defgeneric pose->jlo (pose))
 (defgeneric jlo->pose (jlo))
@@ -44,23 +72,33 @@
                                      (jlo:partial-lo (jlo:frame-query
                                                       (jlo:make-jlo :id 1)
                                                       jlo))))))
-(defun table-cluster->jlo (cluster)
+(defun gaussian->jlo (name mean cov)
+  (declare (type symbol name)
+           (type (or cl-transforms:transform
+                     cl-tf:stamped-transform)
+                 mean)
+           (type (simple-array * (3 3)) cov))
   (flet ((pose-cov->jlo-cov (cov)
            (let ((result (make-array 36 :initial-element 0.0d0)))
              (dotimes (y 3)
                (dotimes (x 3)
                  (setf (aref result (+ (* y 6) x)) (aref cov y x))))
              result)))
-    (jlo:make-jlo :name (symbol-name (table-costmap:name cluster))
+    (jlo:make-jlo :name (symbol-name name)
                   :pose (make-array
                          16 :initial-contents
-                         `(1 0 0 ,(cl-transforms:x (table-costmap:mean cluster))
-                             0 1 0 ,(cl-transforms:y (table-costmap:mean cluster))
-                             0 0 1 ,(cl-transforms:z (table-costmap:mean cluster))
+                         `(1 0 0 ,(cl-transforms:x mean)
+                             0 1 0 ,(cl-transforms:y mean)
+                             0 0 1 ,(cl-transforms:z mean)
                              0 0 0 1))
-                  :cov (let ((cov (pose-cov->jlo-cov (table-costmap:cov cluster))))
+                  :cov (let ((cov (pose-cov->jlo-cov cov)))
                          (dotimes (i 2)
                            (setf (aref cov (+ (* i 6) 2)) 0.0d0)
                            (setf (aref cov (+ (* 2 6) i)) 0.0d0))
                          (setf (aref cov 14) 0.1d0)
-                         cov))))
+                         cov)
+                  :parent (typecase mean
+                            (cl-tf:stamped-transform
+                               (jlo:make-jlo :name (cl-tf:frame-id mean)))                            
+                            (cl-transforms:transform
+                               (jlo:make-jlo :name "/map"))))))
