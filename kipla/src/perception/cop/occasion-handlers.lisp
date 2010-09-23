1915b2e3d1a6c9fe31ecf08f22c0c344baaa4e15
Lorenz Moesenlechner <moesenle@in.tum.de>
2010-09-23 14:42:04 +0200

    Added cop feedback

--- kipla/src/perception/cop/occasion-handlers.lisp
+++ kipla/src/perception/cop/occasion-handlers.lisp
@@ -1,13 +1,47 @@
 
 (in-package :kipla-reasoning)
 
+(defvar *cop-feedback-pub* nil)
+
+(defun init-cop-feedback ()
+  (setf *cop-feedback-pub*
+        (roslisp:advertise
+         "/cop/feedback"
+         "vision_msgs/cop_feedback")))
+(register-ros-init-function init-cop-feedback)
+
+;;; publish a cop_feedback message on the corresponding cop topic.
+;;; The perception_primitive is received in the cop_feedback message
+;;; and should be in the designator passed to the callbacks.
 (defun cop-successful-pick-up (op &key ?obj ?side)
-  (declare (ignore op ?obj ?side))
-  nil)
+  (declare (ignore ?side))
+  (when (eq op :assert)
+    (let ((perceived-object (reference (kipla::newest-valid-designator ?obj))))
+      (when (typep perceived-object 'kipla::cop-perceived-object)
+        (roslisp:publish *cop-feedback-pub*
+                         (roslisp:make-message
+                          "vision_msgs/cop_feedback"
+                          perception_primitive (kipla::perception-primitive
+                                                perceived-object)
+                          evaluation 1.0))))))
 
 (defun cop-failed-pick-up (op &key ?f ?obj ?side)
-  (declare (ignore op ?f ?obj ?side))
-  nil)
+  (declare (ignore ?side))
+  (when (eq op :assert)
+    (let ((perceived-object (reference (kipla::newest-valid-designator ?obj))))
+      (when (typep perceived-object 'kipla::cop-perceived-object)
+        (roslisp:publish *cop-feedback-pub*
+                         (roslisp:make-message
+                          "vision_msgs/cop_feedback"
+                          perception_primitive (kipla::perception-primitive
+                                                perceived-object)
+                          evaluation 0.0
+                          error (vector
+                                 (roslisp:make-message
+                                  "vision_msgs/system_error"
+                                  error_id 0
+                                  node_name roslisp:*ros-node-name*
+                                  error_description (symbol-name ?f)))))))))
 
 (register-production-handler 'object-picked-up #'cop-successful-pick-up)
 (register-production-handler 'object-in-hand-failure #'cop-failed-pick-up)
