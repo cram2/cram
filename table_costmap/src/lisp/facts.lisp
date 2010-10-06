55d2c76ffbbb57a7840028db7b4c3ceb16a887ad
Lorenz Moesenlechner <moesenle@in.tum.de>
2010-10-06 19:52:42 +0200

    Added support for IN REACH designator property

--- table_costmap/src/lisp/facts.lisp
+++ table_costmap/src/lisp/facts.lisp
@@ -1,5 +1,36 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of Willow Garage, Inc. nor the names of its
+;;;       contributors may be used to endorse or promote products derived from
+;;;       this software without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
 
 (in-package :table-costmap)
+
+(defmethod costmap-generator-name->score ((name (eql 'reachable-space)))
+  4)
 
 (defmethod costmap-generator-name->score ((name (eql 'location-neighborhood)))
   5)
@@ -54,6 +85,12 @@ than threshold * highest-probability."
    (let ((p-rel (cl-transforms:v- p p-ref)))
      (atan (cl-transforms:y p-rel) (cl-transforms:x p-rel)))))
 
+;;; Requires the following predicates to be set:
+;;;
+;;; (costmap-size ?w ?h)
+;;; (costmap-manipulation-padding ?p)
+;;; (costmap-padding ?p)
+;;; (costmap-in-reach-padding ?p)
 (def-fact-group table-costmap (desig-costmap
                                desig-orientation
                                desig-z-value
@@ -66,13 +103,21 @@ than threshold * highest-probability."
     (occupancy-grid ?map ?static-occupied (padding ?padding))
     (occupancy-grid ?tables ?tables-occupied (padding ?padding))
     (costmap ?cm)
-    (lisp-fun cl-transforms:make-3d-vector 0 0 0 ?map-origin)
     (costmap-add-function free-space (make-occupancy-grid-cost-function ?free-space) ?cm)
     (costmap-add-function static-occupied (make-occupancy-grid-cost-function ?static-occupied :invert t)
                           ?cm)
     (costmap-add-function tables-occupied (make-occupancy-grid-cost-function ?tables-occupied :invert t)
                           ?cm)
     (costmap-add-generator (make-robot-pos-generator 0.1) ?cm))
+
+  (<- (in-reach-costmap ?cm ?padding ?reaching-distance)
+    (bagof ?one-drivable-cm (drivable-location-costmap ?one-drivable-cm ?padding)
+           ?drivable-costmaps)
+    (lisp-fun merge-costmaps ?drivable-costmaps ?drivable-costmap)
+    (costmap ?cm)
+    (costmap-add-function reachable-space (make-padded-costmap-cost-function
+                                           ?drivable-costmap ?reaching-distance)
+                          ?cm))
   
   (<- (desig-costmap ?desig ?cm)
     (desig-prop ?desig (on table))
@@ -99,6 +144,13 @@ than threshold * highest-probability."
     (costmap-padding ?padding)
     (drivable-location-costmap ?cm ?padding)
     (costmap-add-function location-neighborhood (make-location-cost-function ?loc 0.5) ?cm))
+
+  (<- (desig-costmap ?desig ?cm)
+    (desig-prop ?desig (in reach))
+    (costmap ?cm)
+    (costmap-manipulation-padding ?padding)
+    (costmap-in-reach-padding ?in-reach-padding)
+    (in-reach-costmap ?cm ?padding ?in-reach-padding))
 
   (<- (desig-costmap ?desig ?cm)
     (desig-prop ?desig (to reach))
