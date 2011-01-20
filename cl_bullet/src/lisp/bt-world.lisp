7bf92a0fe9f05c32d9a5a2499aa102415ca2e935
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-01-20 11:27:27 +0100

    Added a lock to BT-WORLD
    Now can achieve some sort of thread safety to a certain degree.

--- cl_bullet/src/lisp/bt-world.lisp
+++ cl_bullet/src/lisp/bt-world.lisp
@@ -53,60 +53,74 @@
    (foreign-alloc-fun :reader foreign-alloc-fun
                       :initform #'new-discrete-dynamics-world)
    (foreign-free-fun :reader foreign-class-free-fun
-                     :initform #'delete-discrete-dynamics-world)))
+                     :initform #'delete-discrete-dynamics-world)
+   (lock :initform (sb-thread:make-mutex))))
+
+(defmacro with-world-locked (world &body body)
+  `(sb-thread:with-mutex ((slot-value ,world 'lock))
+     ,@body))
 
 (defmethod foreign-class-alloc ((world bt-world) &key
                                 &allow-other-keys)
   (funcall (foreign-alloc-fun world) (gravity-vector world)))
 
 (defmethod step-simulation ((world bt-world) time-step)
-  (cffi-step-simulation (foreign-obj world) (coerce time-step 'double-float)))
+  (with-world-locked world
+    (cffi-step-simulation (foreign-obj world) (coerce time-step 'double-float))))
 
 (defmethod add-rigid-body ((world bt-world) (body rigid-body) &optional group mask)
-  (with-slots (bodies foreign-obj) world
-    (push body bodies)
-    (if mask
-        (cffi-add-rigid-body-with-mask
-         foreign-obj (foreign-obj body) group mask)
-        (cffi-add-rigid-body
-         foreign-obj (foreign-obj body)))))
+  (with-world-locked world
+    (with-slots (bodies foreign-obj) world
+      (push body bodies)
+      (if mask
+          (cffi-add-rigid-body-with-mask
+           foreign-obj (foreign-obj body) group mask)
+          (cffi-add-rigid-body
+           foreign-obj (foreign-obj body))))))
 
 (defmethod remove-rigid-body ((world bt-world) body)
-  (cffi-remove-rigid-body (foreign-obj world) (foreign-obj body)))
+  (with-world-locked world
+    (cffi-remove-rigid-body (foreign-obj world) (foreign-obj body))))
 
 (defmethod add-constraint ((world bt-world) constraint &optional disable-collision)
-  (cffi-add-constraint (foreign-obj world) (foreign-obj constraint) disable-collision))
+  (with-world-locked world
+    (cffi-add-constraint (foreign-obj world) (foreign-obj constraint) disable-collision)))
 
 (defmethod remove-constraint ((world bt-world) constraint)
-  (cffi-remove-constraint (foreign-obj world) (foreign-obj constraint)))
+  (with-world-locked world
+    (cffi-remove-constraint (foreign-obj world) (foreign-obj constraint))))
 
 (defmethod set-debug-drawer ((world bt-world) drawer)
-  (cffi-set-debug-drawer (foreign-obj world) (foreign-obj drawer)))
+  (with-world-locked world
+    (cffi-set-debug-drawer (foreign-obj world) (foreign-obj drawer))))
 
 (defmethod debug-draw-world ((world bt-world))
-  (cffi-debug-draw-world (foreign-obj world)))
+  (with-world-locked world
+    (cffi-debug-draw-world (foreign-obj world))))
 
 (defmethod perform-collision-detection ((world bt-world))
-  (cffi-perform-collision-detection (foreign-obj world)))
+  (with-world-locked world
+    (cffi-perform-collision-detection (foreign-obj world))))
 
 (defmethod contact-manifolds ((world bt-world))
-  (flet ((get-contact-points (manifold)
-           (loop for i from 0 below (manifold-get-num-contact-points manifold)
-                 collecting (make-instance 'contact-point
-                                 :point-in-1 (manifold-get-contact-point-0 manifold i)
-                                 :point-in-2 (manifold-get-contact-point-1 manifold i)))))
-    (loop for i from 0 below (get-num-manifolds (foreign-obj world))
-          for manifold = (get-manifold-by-index (foreign-obj world) i)
-          for body-ptr-1 = (manifold-get-body-0 manifold)
-          for body-ptr-2 = (manifold-get-body-1 manifold)
-          when (> (manifold-get-num-contact-points manifold) 0)
-            collecting (make-instance 'contact-manifold
-                            :body-1 (find (manifold-get-body-0 manifold) (bodies world)
-                                          :key #'foreign-obj :test (lambda (a b)
-                                                                     (eql (pointer-address a)
-                                                                          (pointer-address b))))
-                            :body-2 (find (manifold-get-body-1 manifold) (bodies world)
-                                          :key #'foreign-obj :test (lambda (a b)
-                                                                     (eql (pointer-address a)
-                                                                          (pointer-address b))))
-                            :contact-points (get-contact-points manifold)))))
+  (with-world-locked world
+    (flet ((get-contact-points (manifold)
+             (loop for i from 0 below (manifold-get-num-contact-points manifold)
+                   collecting (make-instance 'contact-point
+                                   :point-in-1 (manifold-get-contact-point-0 manifold i)
+                                   :point-in-2 (manifold-get-contact-point-1 manifold i)))))
+      (loop for i from 0 below (get-num-manifolds (foreign-obj world))
+            for manifold = (get-manifold-by-index (foreign-obj world) i)
+            for body-ptr-1 = (manifold-get-body-0 manifold)
+            for body-ptr-2 = (manifold-get-body-1 manifold)
+            when (> (manifold-get-num-contact-points manifold) 0)
+              collecting (make-instance 'contact-manifold
+                              :body-1 (find (manifold-get-body-0 manifold) (bodies world)
+                                            :key #'foreign-obj :test (lambda (a b)
+                                                                       (eql (pointer-address a)
+                                                                            (pointer-address b))))
+                              :body-2 (find (manifold-get-body-1 manifold) (bodies world)
+                                            :key #'foreign-obj :test (lambda (a b)
+                                                                       (eql (pointer-address a)
+                                                                            (pointer-address b))))
+                              :contact-points (get-contact-points manifold))))))
