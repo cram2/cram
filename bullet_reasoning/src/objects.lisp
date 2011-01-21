1a4a341d05566f38e924db5fac7eac441e7edaca
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-01-21 10:32:45 +0100

    Initial commit of bullet_reasoning package
    A couple of simple predicates are already implemented, such as
    
     * (contact ?world ?a ?b)
    
     * (stable ?world ?obj)
    
     * (above ?world ?obj-1 obj-2) and (below ?world ?obj-1 ?obj2)
    
     * (supported-by ?world ?top ?bottom)

---
+++ bullet_reasoning/src/objects.lisp
@@ -0,0 +1,108 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(in-package :btr)
+
+(defgeneric add-object (world type name pose mass &key &allow-other-keys)
+  (:documentation "Adds an object of `type' named `name' to the bullet
+  world `world'. The object is placed at `pose'"))
+
+(defgeneric remove-object (world name)
+  (:documentation "Removes the object named `name' from the `world'")
+  (:method ((world bt-world) name)
+    (let ((body (find name (bodies world) :key #'name :test #'equal)))
+      (if body
+          (remove-rigid-body world body)
+          (warn 'simple-warning :format-control "Could not find a body named `~a'" name)))))
+
+(defun ensure-pose (pose)
+  (etypecase pose
+    (list (destructuring-bind
+                ((x y z) (ax ay az aw))
+              pose
+            (cl-transforms:make-pose
+             (cl-transforms:make-3d-vector x y z)
+             (cl-transforms:make-quaternion ax ay az aw))))
+    (cl-transforms:pose pose)))
+
+(defmethod add-object ((world bt-world) (type (eql 'box)) name pose mass &key size)
+  (let ((pose (ensure-pose pose)))
+    (destructuring-bind (size-x size-y size-z) size
+      (add-rigid-body world (make-instance
+                             'rigid-body
+                             :name name :mass mass :pose pose
+                             :collision-shape (make-instance
+                                               'box-shape
+                                               :half-extents (cl-transforms:make-3d-vector
+                                                              (/ size-x 2)
+                                                              (/ size-y 2)
+                                                              (/ size-z 2))))))))
+
+(defmethod add-object ((world bt-world) (type (eql 'static-plane)) name pose mass &key
+                       normal constant)
+  (let ((pose (ensure-pose pose)))
+    (destructuring-bind (normal-x normal-y normal-z) normal
+      (add-rigid-body world (make-instance
+                             'rigid-body
+                             :name name :mass mass :pose pose
+                             :collision-shape (make-instance
+                                               'static-plane-shape
+                                               :normal (cl-transforms:make-3d-vector
+                                                        normal-x normal-y normal-z)
+                                               :constant constant))))))
+
+(defmethod add-object ((world bt-world) (type (eql 'sphere)) name pose mass &key radius)
+  (let ((pose (ensure-pose pose)))
+    (add-rigid-body world (make-instance
+                          'rigid-body
+                          :name name :mass mass :pose pose
+                          :collision-shape (make-instance 'sphere-shape :radius radius)))))
+
+(defmethod add-object ((world bt-world) (type (eql 'cylinder)) name pose mass &key size)
+  (let ((pose (ensure-pose pose)))
+    (destructuring-bind (size-x size-y size-z) size
+      (add-rigid-body world (make-instance
+                             'rigid-body
+                             :name name :mass mass :pose pose
+                             :collision-shape (make-instance 'cylinder-shape
+                                                             :half-extents (cl-transforms:make-3d-vector
+                                                                            (/ size-x 2)
+                                                                            (/ size-y 2)
+                                                                            (/ size-z 2))))))))
+
+(defmethod add-object ((world bt-world) (type (eql 'cone)) name pose mass &key
+                       radius height)
+  (let ((pose (ensure-pose pose)))
+    (add-rigid-body world (make-instance
+                           'rigid-body
+                           :name name :mass mass :pose pose
+                           :collision-shape (make-instance 'cone-shape
+                                                           :radius radius
+                                                           :height height)))))
---
+++ bullet_reasoning/src/prolog-facts.lisp
@@ -0,0 +1,179 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(in-package :btr)
+
+(def-fact-group bullet-world-facts ()
+  
+  (<- (bullet-world ?world)
+    (instance-of bt-world ?world))
+
+  (<- (assert-object ?world ?object-type ?name ?pose ?mass . ?args)
+    (lisp-fun apply add-object
+              ?world ?object-type
+              ?name ?pose ?mass
+              ?args
+              ?_))
+
+  (<- (retract-object ?world ?name)
+    (lisp-fun remove-object ?world ?name))
+
+  (<- (object ?world ?name ?obj)
+    (bound ?name)
+    (not (bound ?obj))
+    (lisp-fun get-object ?world ?name ?obj))
+
+  (<- (object ?world ?name ?obj)
+    (not (bound ?name))
+    (bound ?obj)
+    (lispfun name ?obj ?name))
+
+  (<- (object ?world ?name ?obj)
+    (not (bound ?name))
+    (not (bound ?obj))
+    (lisp-fun find-objects ?world ?objs)
+    (member ?obj ?objs)
+    (lisp-fun name ?obj ?name))
+  
+  ;; Performs one simulation step in the world
+  (<- (step ?world)
+    (step ?world 0.01))
+
+  ;; Performs one simulation step of length ?dt in the world
+  (<- (step ?world ?dt)
+    (lisp-fun step-simulation ?world ?dt))
+
+  ;; Simulates the next ?t seconds
+  (<- (simulate ?world ?t)
+    (lisp-fun simulate ?world ?t ?_))
+
+  ;; Simulates the next ?t seconds with a step of ?dt
+  (<- (simulate ?world ?t ?dt)
+    (lisp-fun simulate ?world ?t ?dt ?_)))
+
+(def-fact-group poses ()
+
+  (<- (pose ?world ?obj ?pose)
+    (lisp-fun body ?world ?obj ?body)
+    (lisp-fun pose ?body ?pose))
+
+  (<- (pose ?world ?obj ?position ?orientation)
+    (pose ?obj ?p)
+    (position ?p ?position)
+    (orientation ?p ?orientation))
+
+  (<- (position ?pose (?x ?y ?z))
+    (lisp-fun cl-transforms:origin ?pose ?p)
+    (lisp-fun cl-transforms:x ?p ?x)
+    (lisp-fun cl-transforms:y ?p ?y)
+    (lisp-fun cl-transforms:z ?p ?z))
+
+  (<- (orientation ?pose (?x ?y ?z ?w))
+    (lisp-fun cl-transforms:orientation ?pose ?o)
+    (lisp-fun cl-transforms:x ?o ?x)
+    (lisp-fun cl-transforms:y ?o ?y)
+    (lisp-fun cl-transforms:z ?o ?z)
+    (lisp-fun cl-transforms:w ?o ?w))
+
+  (<- (poses-equal ?pose-1 ?pose-2 (?dist-sigma ?ang-sigma))
+    (lisp-pred poses-equal-p ?pose-1 ?pose-2 ?dist-sigma ?ang-sigma)))
+
+(def-fact-group force-dynamic-states ()
+
+  (<- (contact ?world ?obj-1 ?obj-2)
+    (bound ?obj-1)
+    (bound ?obj-2)
+    (lisp-fun perform-collision-detection ?world ?_)
+    (lisp-pred contact-p ?world ?obj-1 ?obj-2))
+
+  (<- (contact ?world ?obj-1 ?obj-2)
+    (bound ?obj-1)
+    (not (bound ?obj-2))
+    (lisp-fun perform-collision-detection ?world ?_)
+    (lisp-pred find-objects-in-contact ?world ?obj-1 ?objs)
+    (member ?obj-2 ?objs))
+
+  (<- (contact ?world ?obj-1 ?obj-2)
+    (lisp-fun perform-collision-detection ?world ?_)
+    (lisp-fun find-all-contacts ?world ?contacts)
+    (member (?obj-1 ?obj-2) ?contacts))
+
+  (<- (stable ?world ?obj)
+    (bound ?obj)
+    (pose ?world ?obj ?pose-1)
+    (with-stored-world ?world
+      (simulate ?world 2.5)
+      (lisp-pred stable-p ?world ?obj)
+      (pose ?world ?obj ?pose-2))
+    (poses-equal ?pose-1 ?pose-2 (0.01 0.01))))
+
+(def-fact-group spatial-relations ()
+
+  (<- (above ?world ?obj-1 ?obj-2)
+    (bound ?obj-1)
+    (bound ?obj-2)
+    (lisp-pred above-p ?world ?obj-1 ?obj-2))
+
+  (<- (above ?world ?obj-1 ?obj-2)
+    (not (bound ?obj-1))
+    (bound ?obj-2)
+    (lisp-fun find-objects-above ?world ?obj-2 ?objs)
+    (member ?obj-1 ?objs))
+
+  (<- (above ?world ?obj-1 ?obj-2)
+    (bound ?obj-1)
+    (not (bound ?obj-2))
+    (lisp-fun find-objects-below ?world ?obj-1 ?objs)
+    (member ?obj-2 ?objs))
+
+  (<- (below ?world ?obj-1 ?obj-2)
+    (bound ?obj-1 ?obj-2)
+    (lisp-pred below-p ?world ?obj-1 ?obj-2))
+
+  (<- (below ?world ?obj-1 ?obj-2)
+    (not (bound ?obj-1))
+    (bound ?obj-2)
+    (lisp-fun find-objects-below ?world ?obj-2 ?objs)
+    (member ?obj-1 ?objs))
+
+  (<- (below ?world ?obj-1 ?obj-2)
+    (bound ?obj-1)
+    (not (bound ?obj-2))
+    (lisp-fun find-objects-above ?world ?obj-1 ?objs)
+    (member ?obj-2 ?objs))
+
+  (<- (supported-by ?world ?top ?bottom)
+    (above ?world ?top ?bottom)
+    (contact ?world ?top ?bottom)
+    (stable ?world ?top)))
+
+(def-fact-group debug ()
+  (<- (debug-window ?world)
+    (lisp-fun add-debug-window ?world ?_)))
