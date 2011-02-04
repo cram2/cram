77a5501abad763c2f1af872ca60a0a486e4dfb23
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-04 16:28:57 +0100

    Added json-prolog prolog handler
    Moved this predicate from rosie_executive into cl_json_pl_client. It
    allows for interfacing json-prolog/knowrob from the cram-reasoning
    prolog engine.

--- cl_json_pl_client/src/prolog-handlers.lisp
+++ cl_json_pl_client/src/prolog-handlers.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Copyright (c) 2009, Lorenz Moesenlechner <moesenle@cs.tum.edu>
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
@@ -10,9 +10,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Willow Garage, Inc. nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
 ;;; 
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -27,15 +28,18 @@
 ;;; POSSIBILITY OF SUCH DAMAGE.
 ;;;
 
-(in-package :cl-user)
+(in-package :json-prolog)
 
-(defpackage :json-prolog
-    (:use #:common-lisp #:cram-utilities #:roslisp)
-  (:export #:*service-namespace*
-           #:prolog
-           #:prolog-1
-           #:prolog-simple
-           #:prolog-simple-1
-           #:finish-query
-           #:json-prolog
-           #:wait-for-prolog-service))
+(crs:def-prolog-handler json-prolog (bdgs form &rest key-args &key prologify lispify package)
+  (declare (ignore prologify lispify package))
+  (when (wait-for-prolog-service 0.5)
+    (let* ((form (substitute-vars form bdgs))
+           (vars (remove-if #'is-unnamed-var (vars-in form)))
+           ;; force-ll to make sure the json query is finished immediately
+           (result (force-ll (apply #'prolog form key-args))))
+      (lazy-mapcan (lambda (binding)
+                     (crs:prolog `(and ,@(mapcar (lambda (var)
+                                                   `(== ,var ,(var-value var binding)))
+                                                 vars))
+                                 bdgs))
+                   result))))
