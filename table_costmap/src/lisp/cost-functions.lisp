6ae871617d1ce83138912ac2effbf048c5fb6d54
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-07 16:21:42 +0100

    Return cost function only when there is a table matching it

--- table_costmap/src/lisp/cost-functions.lisp
+++ table_costmap/src/lisp/cost-functions.lisp
@@ -31,7 +31,8 @@
 (in-package :table-costmap)
 
 (defun make-table-cost-function (name)
-  (let* ((table-cluster (get-table-cluster name))
-         (mean (3d-vector->mean (mean table-cluster)))
-         (cov (2d-cov (cov table-cluster))))
-    (make-gauss-cost-function mean cov)))
+  (let ((table-cluster (get-table-cluster name)))
+    (when table-cluster
+      (let ((mean (3d-vector->mean (mean table-cluster)))
+            (cov (2d-cov (cov table-cluster))))
+        (make-gauss-cost-function mean cov)))))
