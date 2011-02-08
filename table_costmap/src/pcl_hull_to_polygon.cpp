40167596405b9707fb4293af2cc2d9d74e975beb
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-08 14:05:03 +0100

    Refactored pcl_hull_to_poly to work in unstable and cturtle
    Radu changed PCL _again_ so that pcl_ros::Subscribers are not present
    anymore.

--- table_costmap/src/pcl_hull_to_polygon.cpp
+++ table_costmap/src/pcl_hull_to_polygon.cpp
@@ -2,18 +2,21 @@
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
 
+#include <ros/ros.h>
 #include <pcl/point_types.h>
-#include <pcl_ros/subscriber.h>
+#include <pcl/ros/conversions.h>
 #include <geometry_msgs/PolygonStamped.h>
+#include <sensor_msgs/PointCloud2.h>
 
-typedef pcl_ros::Subscriber<pcl::PointXYZ> PointCloudSubscriber;
-
-void pointCloudToPolygonCb(const PointCloudSubscriber::PointCloud::ConstPtr &cloud, ros::Publisher &polygon_pub)
+void pointCloudToPolygonCb(const sensor_msgs::PointCloud2::ConstPtr &cloud_raw, ros::Publisher &polygon_pub)
 {
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg(*cloud_raw, cloud);
+  
   geometry_msgs::PolygonStamped polygon;
 
-  polygon.header = cloud->header;
-  BOOST_FOREACH(const pcl::PointXYZ &pt, cloud->points)
+  polygon.header = cloud.header;
+  BOOST_FOREACH(const pcl::PointXYZ &pt, cloud.points)
   {
     geometry_msgs::Point32 out_pt;
     out_pt.x = pt.x;
@@ -32,8 +35,8 @@ int main(int argc, char *argv[])
   ros::NodeHandle nh("~");
 
   ros::Publisher polygon_pub = nh.advertise<geometry_msgs::PolygonStamped>("output", 10);
-  PointCloudSubscriber pcl_subscription = PointCloudSubscriber(
-    nh, "input", 10, boost::bind(&pointCloudToPolygonCb, _1, polygon_pub));
+  ros::Subscriber pcl_subscription = nh.subscribe<sensor_msgs::PointCloud2>(
+    "input", 10, boost::bind(&pointCloudToPolygonCb, _1, polygon_pub));
 
   ros::spin();
 }
