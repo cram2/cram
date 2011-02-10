1add31b4d616a82f027b22ee1e6eb817c0939144
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-10 09:54:22 +0100

    Added some household objects to bullet_reasoning
    Currently, we only have mugs and plates.

---
+++ bullet_reasoning/src/household-objects.lisp
@@ -0,0 +1,101 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(in-package :btr)
+
+(defparameter *mesh-files* '((mug . "package://bullet_reasoning/resource/mug.stl")
+                             (plate . "package://bullet_reasoning/resource/plate.stl")))
+
+(defun make-octagon-prism-shape (radius height)
+  "Returns a collision shape that is a octagon prism, i.e. that has an
+  octagon at its base."
+  (let ((compound-shape (make-instance 'compound-shape)))
+    (dotimes (i 4)
+      (add-child-shape compound-shape
+                       (cl-transforms:make-pose
+                        (cl-transforms:make-3d-vector 0 0 0)
+                        (cl-transforms:axis-angle->quaternion
+                         (cl-transforms:make-3d-vector 0 0 1)
+                         (/ (* i pi)
+                            4)))
+                       (make-instance
+                        'box-shape
+                        :half-extents (cl-transforms:make-3d-vector
+                                       radius (* radius (sin (/ pi 8)))
+                                       (/ height 2)))))
+    compound-shape))
+
+(defun make-cup-shape (radius height handle-size)
+  (let ((collision-shape (make-octagon-prism-shape radius height)))
+    (add-child-shape collision-shape
+                     (cl-transforms:make-pose
+                      (cl-transforms:make-3d-vector
+                       (+ (* radius (cos (/ pi 8)))
+                          (/ (cl-transforms:x handle-size)
+                             2))
+                       0 0)
+                      (cl-transforms:make-quaternion 0 0 0 1))
+                     (make-instance
+                      'box-shape
+                      :half-extents (cl-transforms:v* handle-size 0.5)))
+    collision-shape))
+
+(defmethod add-object ((world bt-world) (type (eql 'generic-cup)) name pose &key
+                       mass radius height
+                       (handle-size (cl-transforms:make-3d-vector
+                                     0.03 0.01 (* height 0.8))))
+  (make-object world name
+               (list
+                (make-instance
+                 'rigid-body
+                 :name name :mass mass :pose (ensure-pose pose)
+                 :collision-shape (make-cup-shape radius height handle-size)))))
+
+(defmethod add-object ((world bt-world) (type (eql 'mug)) name pose &key
+                       mass)
+  (add-object world 'mesh name pose :mass mass :mesh 'mug))
+
+(defmethod add-object ((world bt-world) (type (eql 'mesh)) name pose &key
+                       mass mesh (color '(0.5 0.5 0.5 1.0)))
+  (let ((mesh (physics-utils:load-3d-model
+                (physics-utils:parse-uri (etypecase mesh
+                                           (symbol (cdr (assoc mesh *mesh-files*)))
+                                           (string mesh)
+                                           (pathname mesh))))))
+    (make-object world name
+                 (list
+                  (make-instance
+                   'rigid-body
+                   :name name :mass mass :pose (ensure-pose pose)
+                   :collision-shape (make-instance
+                                     'convex-hull-mesh-shape
+                                     :points (physics-utils:3d-model-vertices mesh)
+                                     :faces (physics-utils:3d-model-faces mesh)
+                                     :color color))))))
