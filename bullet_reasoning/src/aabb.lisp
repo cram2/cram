a396f18fdb22cc5a4f60bd3debdf41a07bb3f440
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-16 13:22:40 +0100

    Added AABB functionality for objects and utils file

---
+++ bullet_reasoning/src/aabb.lisp
@@ -0,0 +1,71 @@
+;;;
+;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(in-package :btr)
+
+(defun merge-bounding-boxes (box-1 box-2)
+  (let* ((box-1/2 (cl-transforms:v*
+                   (bounding-box-dimensions box-1)
+                   0.5))
+         (box-2/2 (cl-transforms:v*
+                   (bounding-box-dimensions box-2)
+                   0.5))
+         (min-box-1 (cl-transforms:v- (bounding-box-center box-1) box-1/2))
+         (max-box-1 (cl-transforms:v+ (bounding-box-center box-1) box-1/2))
+         (min-box-2 (cl-transforms:v- (bounding-box-center box-2) box-2/2))
+         (max-box-2 (cl-transforms:v+ (bounding-box-center box-2) box-2/2))
+         (new-min (cl-transforms:make-3d-vector
+                   (min (cl-transforms:x min-box-1)
+                        (cl-transforms:x min-box-2))
+                   (min (cl-transforms:y min-box-1)
+                        (cl-transforms:y min-box-2))
+                   (min (cl-transforms:z min-box-1)
+                        (cl-transforms:z min-box-2))))
+         (new-max (cl-transforms:make-3d-vector
+                   (max (cl-transforms:x max-box-1)
+                        (cl-transforms:x max-box-2))
+                   (max (cl-transforms:y max-box-1)
+                        (cl-transforms:y max-box-2))
+                   (max (cl-transforms:z max-box-1)
+                        (cl-transforms:z max-box-2)))))
+    (make-bounding-box
+     :center (cl-transforms:v* (cl-transforms:v+ new-max new-min)
+                               0.5)
+     :dimensions (cl-transforms:v- new-max new-min))))
+
+(defun translate-bounding-box (aabb vector)
+  "Translates the bounding box by `vector'"
+  (make-bounding-box
+   :center (cl-transforms:v+ vector (bounding-box-center aabb))
+   :dimensions (bounding-box-dimensions aabb)))
+
+(defmethod aabb ((obj object))
+  (reduce #'merge-bounding-boxes
+          (mapcar #'aabb (rigid-bodies obj))))
