506a6d29155d74a43df4c96b8388bd1bf504a860
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-02-23 11:43:43 +0100

    Implemented service to get weighted IK
    It basically uses the KDL::ChainIkSolverVel_wdls class to solve IK. The
    service takes weight matrices and the lambda value, the pose, a tool and
    the seed state of the robot.

--- kdl_arm_kinematics/src/arm_kinematics.cpp
+++ kdl_arm_kinematics/src/arm_kinematics.cpp
@@ -16,7 +16,7 @@
 #include <kinematics_msgs/GetPositionIK.h>
 #include <kinematics_msgs/GetKinematicSolverInfo.h>
 #include <kinematics_msgs/KinematicSolverInfo.h>
-#include <kdl_arm_kinematics/SetWeights.h>
+#include <kdl_arm_kinematics/GetWeightedIK.h>
 
 using std::string;
 
@@ -38,10 +38,9 @@ private:
   KDL::JntArray joint_min, joint_max;
   KDL::Chain chain;
   unsigned int num_joints;
-
-  boost::shared_ptr<KDL::ChainFkSolverPos_recursive> fk_solver;
-  boost::shared_ptr<KDL::ChainIkSolverPos_NR_JL> ik_solver_pos;
-  boost::shared_ptr<KDL::ChainIkSolverVel_wdls> ik_solver_vel;
+  int max_iterations;
+  double epsilon;
+  Eigen::MatrixXd weight_ts;
 
   ros::ServiceServer ik_service,ik_solver_info_service;
   ros::ServiceServer fk_service,fk_solver_info_service;
@@ -55,6 +54,15 @@ private:
   bool readJoints(urdf::Model &robot_model);
   int getJointIndex(const std::string &name);
   int getKDLSegmentIndex(const std::string &name);
+  bool solveCartToJnt(const KDL::JntArray &q_init, const KDL::Frame &q_in, KDL::JntArray &q_out,
+    const KDL::Frame &tool_frame = KDL::Frame::Identity());
+  bool solveCartToJnt(const KDL::JntArray &q_init, const KDL::Frame &q_in, KDL::JntArray &q_out,
+    const KDL::Frame &tool_frame, const Eigen::MatrixXd &weight_ts,
+    const Eigen::MatrixXd &weight_js, const double lambda=0.0);
+  double calculateEps(const KDL::Frame &f, const KDL::Frame &ref,
+    const Eigen::MatrixXd &weight=Eigen::MatrixXd::Identity(6,6));
+  void initializeWeights(const kdl_arm_kinematics::KDLWeights &msg,
+    Eigen::MatrixXd &weight_ts, Eigen::MatrixXd &weight_js, double &lambda);
 
   /**
    * @brief This is the basic IK service method that will compute and return an IK solution.
@@ -62,7 +70,7 @@ private:
    * @param The response message. See service definition for GetPositionIK for more information on this message.
    */
   bool getPositionIK(kinematics_msgs::GetPositionIK::Request &request,
-                     kinematics_msgs::GetPositionIK::Response &response);
+    kinematics_msgs::GetPositionIK::Response &response);
 
   /**
    * @brief This is the basic kinematics info service that will return information about the kinematics node.
@@ -70,7 +78,7 @@ private:
    * @param The response message. See service definition for GetKinematicSolverInfo for more information on this message.
    */
   bool getIKSolverInfo(kinematics_msgs::GetKinematicSolverInfo::Request &request,
-                       kinematics_msgs::GetKinematicSolverInfo::Response &response);
+    kinematics_msgs::GetKinematicSolverInfo::Response &response);
 
   /**
    * @brief This is the basic kinematics info service that will return information about the kinematics node.
@@ -78,7 +86,7 @@ private:
    * @param The response message. See service definition for GetKinematicSolverInfo for more information on this message.
    */
   bool getFKSolverInfo(kinematics_msgs::GetKinematicSolverInfo::Request &request,
-                       kinematics_msgs::GetKinematicSolverInfo::Response &response);
+    kinematics_msgs::GetKinematicSolverInfo::Response &response);
 
   /**
    * @brief This is the basic forward kinematics service that will return information about the kinematics node.
@@ -86,20 +94,18 @@ private:
    * @param The response message. See service definition for GetPositionFK for more information on this message.
    */
   bool getPositionFK(kinematics_msgs::GetPositionFK::Request &request,
-                     kinematics_msgs::GetPositionFK::Response &response);
+    kinematics_msgs::GetPositionFK::Response &response);
 
-  /**
-   * @brief Sets the weights used for IK calculation
-   */
-  bool setWeights(kdl_arm_kinematics::SetWeights::Request &request,
-                  kdl_arm_kinematics::SetWeights::Response &response);
+  bool getWeightedIK(kdl_arm_kinematics::GetWeightedIK::Request &request,
+    kdl_arm_kinematics::GetWeightedIK::Response &response);
 };
 
 
 
 Kinematics::Kinematics()
   : nh(),
-    nh_private ("~")
+    nh_private ("~"),
+    weight_ts(Eigen::MatrixXd::Identity(6,6))
 {
   // Get URDF XML
   std::string urdf_xml, full_urdf_xml;
@@ -121,25 +127,14 @@ Kinematics::Kinematics()
   if (!loadModel(result))
     throw Kinematics::InitFailed("Could not load models!");
 
-  // Get Solver Parameters
-  int maxIterations;
-  double epsilon;
-
-  nh_private.param("maxIterations", maxIterations, 1000);
+  nh_private.param("maxIterations", max_iterations, 1000);
   nh_private.param("epsilon", epsilon, 1e-2);
-
-  // Build Solvers
-  fk_solver.reset(new KDL::ChainFkSolverPos_recursive(chain));
-  ik_solver_vel.reset(new KDL::ChainIkSolverVel_wdls(chain));
-  ik_solver_pos.reset(new KDL::ChainIkSolverPos_NR_JL(chain, joint_min, joint_max,
-      *fk_solver, *ik_solver_vel, maxIterations, epsilon));
 
   ROS_INFO("Advertising services");
   fk_service = nh_private.advertiseService("get_fk", &Kinematics::getPositionFK,this);
   ik_service = nh_private.advertiseService("get_ik", &Kinematics::getPositionIK,this);
   ik_solver_info_service = nh_private.advertiseService("get_ik_solver_info", &Kinematics::getIKSolverInfo,this);
   fk_solver_info_service = nh_private.advertiseService("get_fk_solver_info", &Kinematics::getFKSolverInfo,this);
-  set_weights_service = nh_private.advertiseService("set_weights", &Kinematics::setWeights, this);
 }
 
 bool Kinematics::loadModel(const std::string xml) {
@@ -245,6 +240,115 @@ int Kinematics::getKDLSegmentIndex(const std::string &name) {
   return -1;
 }
 
+bool Kinematics::solveCartToJnt(const KDL::JntArray &q_init, const KDL::Frame &q_in, KDL::JntArray &q_out,
+  const KDL::Frame &tool_frame)
+{
+  return solveCartToJnt(q_init, q_in, q_out, tool_frame,
+    Eigen::MatrixXd::Identity(6, 6),
+    Eigen::MatrixXd::Identity(chain.getNrOfJoints(), chain.getNrOfJoints()));
+}
+
+bool Kinematics::solveCartToJnt(const KDL::JntArray &q_init, const KDL::Frame &q_in, KDL::JntArray &q_out,
+  const KDL::Frame &tool_frame, const Eigen::MatrixXd &weight_ts, const Eigen::MatrixXd &weight_js,
+  const double lambda)
+{
+  KDL::Frame f;
+  KDL::JntArray delta_q;
+  KDL::Twist delta_twist;  
+  int i;
+
+  KDL::Chain chain_with_tool(chain);
+  chain_with_tool.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::None), tool_frame));
+  
+  KDL::ChainFkSolverPos_recursive fk_solver(chain_with_tool);
+  KDL::ChainIkSolverVel_wdls ik_solver(chain_with_tool);
+
+  ik_solver.setWeightTS(weight_ts);
+  ik_solver.setWeightJS(weight_js);
+  ik_solver.setLambda(lambda);
+  
+  q_out = q_init;
+
+  for(i=0;i<max_iterations;i++)
+  {
+    fk_solver.JntToCart(q_out, f);
+
+    if( calculateEps(f, q_in, weight_ts) < epsilon )
+      break;
+
+    ik_solver.CartToJnt(q_out, delta_twist, delta_q);
+    KDL::Add(q_out, delta_q, q_out);
+
+    for(unsigned int j=0; j<joint_min.rows(); j++)
+    {
+      if(q_out(j) < joint_min(j))
+        q_out(j) = joint_min(j);
+    }
+
+    for(unsigned int j=0; j<joint_max.rows(); j++)
+    {
+      if(q_out(j) > joint_max(j))
+        q_out(j) = joint_max(j);
+    }
+  }
+
+  if(i < max_iterations)
+    return true;
+  return false;
+}
+
+double Kinematics::calculateEps(const KDL::Frame &f, const KDL::Frame &ref, const Eigen::MatrixXd &weight)
+{
+  double f_roll, f_pitch, f_yaw;
+  double ref_roll, ref_pitch, ref_yaw;
+  f.M.GetRPY(f_roll, f_pitch, f_yaw);
+  ref.M.GetRPY(ref_roll, ref_pitch, ref_yaw);
+
+  double x_diff = f.p.x() - ref.p.x();
+  double y_diff = f.p.y() - ref.p.y();
+  double z_diff = f.p.z() - ref.p.z();
+  double roll_diff = f_roll - ref_roll;
+  double pitch_diff = f_pitch - ref_pitch;
+  double yaw_diff = f_yaw - ref_yaw;
+
+  return (x_diff * x_diff * weight(0, 0)) +
+    (y_diff * y_diff * weight(1, 1)) +
+    (z_diff * z_diff * weight(2, 2)) +
+    (roll_diff * roll_diff * weight(3, 3)) +
+    (pitch_diff * pitch_diff * weight(4, 4)) +
+    (yaw_diff * yaw_diff * weight(5, 5));
+}
+
+void Kinematics::initializeWeights(const kdl_arm_kinematics::KDLWeights &msg,
+    Eigen::MatrixXd &weight_ts, Eigen::MatrixXd &weight_js, double &lambda)
+{
+  if(msg.mode == kdl_arm_kinematics::KDLWeights::INVALID_MODE)
+  {
+    ROS_WARN("Mode for setting weights invalid. Maybe it has not been set?");
+    return;
+  }
+  if(msg.mode & kdl_arm_kinematics::KDLWeights::SET_TS)
+  {
+    for(int i=0, a=0; i<6; i++)
+    {
+      for(int j=0; j<6; j++, a++)
+        weight_ts(i, j) = msg.weight_ts[a];
+    }
+  }
+  if(msg.mode & kdl_arm_kinematics::KDLWeights::SET_JS)
+  {
+    int dim = sqrt(msg.weight_js.size());
+    weight_js = Eigen::MatrixXd(dim, dim);
+    for(int i=0, a=0; i<dim; i++)
+    {
+      for(int j=0; j<dim; j++, a++)
+        weight_js(i, j) = msg.weight_js[a];
+    }
+  }
+  if(msg.mode & kdl_arm_kinematics::KDLWeights::SET_LAMBDA)
+    lambda = msg.lambda;
+}
+
 bool Kinematics::getPositionIK(kinematics_msgs::GetPositionIK::Request &request,
                                kinematics_msgs::GetPositionIK::Response &response) {
 
@@ -286,12 +390,12 @@ bool Kinematics::getPositionIK(kinematics_msgs::GetPositionIK::Request &request,
   KDL::Frame F_dest;
   tf::TransformTFToKDL(transform_root, F_dest);
 
-  int ik_valid = ik_solver_pos->CartToJnt(jnt_pos_in, F_dest, jnt_pos_out);
-
-  if (ik_valid >= 0) {
+  if (solveCartToJnt(jnt_pos_in, F_dest, jnt_pos_out))
+  {
     response.solution.joint_state.name = info.joint_names;
     response.solution.joint_state.position.resize(num_joints);
-    for (unsigned int i=0; i < num_joints; i++) {
+    for (unsigned int i=0; i < num_joints; i++)
+    {
       response.solution.joint_state.position[i] = jnt_pos_out(i);
       ROS_INFO("IK Solution: %s %d: %f",response.solution.joint_state.name[i].c_str(),i,jnt_pos_out(i));
     }
@@ -323,6 +427,8 @@ bool Kinematics::getPositionFK(kinematics_msgs::GetPositionFK::Request &request,
   geometry_msgs::PoseStamped pose;
   tf::Stamped<tf::Pose> tf_pose;
 
+  KDL::ChainFkSolverPos_recursive fk_solver(chain);
+
   jnt_pos_in.resize(num_joints);
   for (unsigned int i=0; i < num_joints; i++) {
     int tmp_index = getJointIndex(request.robot_state.joint_state.name[i]);
@@ -338,7 +444,7 @@ bool Kinematics::getPositionFK(kinematics_msgs::GetPositionFK::Request &request,
     int segmentIndex = getKDLSegmentIndex(request.fk_link_names[i]);
     ROS_DEBUG("End effector index: %d",segmentIndex);
     ROS_DEBUG("Chain indices: %d",chain.getNrOfSegments());
-    if (fk_solver->JntToCart(jnt_pos_in,p_out,segmentIndex) >=0) {
+    if (fk_solver.JntToCart(jnt_pos_in,p_out,segmentIndex) >=0) {
       tf_pose.frame_id_ = root_name;
       tf_pose.stamp_ = ros::Time();
       tf::PoseKDLToTF(p_out,tf_pose);
@@ -362,38 +468,78 @@ bool Kinematics::getPositionFK(kinematics_msgs::GetPositionFK::Request &request,
   return true;
 }
 
-bool Kinematics::setWeights(kdl_arm_kinematics::SetWeights::Request &request,
-                            kdl_arm_kinematics::SetWeights::Response &response)
+bool Kinematics::getWeightedIK(kdl_arm_kinematics::GetWeightedIK::Request &request,
+  kdl_arm_kinematics::GetWeightedIK::Response &response)
 {
-  if(request.mode == kdl_arm_kinematics::SetWeights::Request::INVALID_MODE)
+  geometry_msgs::PoseStamped pose_msg_in = request.pose;
+  tf::Stamped<tf::Pose> transform;
+  tf::Stamped<tf::Pose> transform_root;
+  tf::poseStampedMsgToTF( pose_msg_in, transform );
+
+  //Do the IK
+  KDL::JntArray jnt_pos_in;
+  KDL::JntArray jnt_pos_out;
+  jnt_pos_in.resize(num_joints);
+
+  if(request.ik_seed.name.size() < num_joints ||
+     request.ik_seed.position.size() < num_joints)
   {
-    ROS_WARN("Mode for setting weights invalid. Maybe it has not been set?");
+    ROS_ERROR("Invalid seed state. It must contain states for all joints used by the IK solver.");
     return false;
   }
-  if(request.mode & kdl_arm_kinematics::SetWeights::Request::SET_TS)
+  for (unsigned int i=0; i < num_joints; i++)
   {
-    Eigen::MatrixXd ts(6, 6);
-    for(int i=0, a=0; i<6; i++)
+    int tmp_index = getJointIndex(request.ik_seed.name[i]);
+    if (tmp_index >=0)
+      jnt_pos_in(tmp_index) = request.ik_seed.position[i];
+    else
     {
-      for(int j=0; j<6; j++, a++)
-        ts(i, j) = request.weight_ts[a];
+      ROS_ERROR("i: %d, No joint index for %s",i,request.ik_seed.name[i].c_str());
+      return false;
     }
-    ik_solver_vel->setWeightTS(ts);
   }
-  if(request.mode & kdl_arm_kinematics::SetWeights::Request::SET_JS)
+
+  //Convert F to our root_frame
+  try
   {
-    int dim = sqrt(request.weight_js.size());
-    Eigen::MatrixXd js(dim, dim);
-    for(int i=0, a=0; i<dim; i++)
-    {
-      for(int j=0; j<dim; j++, a++)
-        js(i, j) = request.weight_js[a];
-    }
-    ik_solver_vel->setWeightJS(js);
+    tf_listener.transformPose(root_name, transform, transform_root);
   }
-  if(request.mode & kdl_arm_kinematics::SetWeights::Request::SET_LAMBDA)
-    ik_solver_vel->setLambda(request.lambda);
-  return true;
+  catch (...)
+  {
+    ROS_ERROR("Could not transform IK pose to frame: %s", root_name.c_str());
+    response.error_code.val = response.error_code.FRAME_TRANSFORM_FAILURE;
+    return false;
+  }
+
+  KDL::Frame F_dest;
+  tf::TransformTFToKDL(transform_root, F_dest);
+
+  KDL::Frame tool;
+  tf::PoseMsgToKDL(request.tool_frame, tool);
+
+  /// Get the weight matrices
+  Eigen::MatrixXd weight_ts(Eigen::MatrixXd::Identity(6, 6));
+  Eigen::MatrixXd weight_js(Eigen::MatrixXd::Identity(chain.getNrOfJoints(), chain.getNrOfJoints()));
+  double lambda = 0.0;
+
+  initializeWeights(request.weights, weight_ts, weight_js, lambda);
+  
+  if (solveCartToJnt(jnt_pos_in, F_dest, jnt_pos_out, tool, weight_ts, weight_js, lambda))
+  {
+    response.solution.name = info.joint_names;
+    response.solution.position.resize(num_joints);
+    for (unsigned int i=0; i < num_joints; i++)
+    {
+      response.solution.position[i] = jnt_pos_out(i);
+      ROS_INFO("IK Solution: %s %d: %f",response.solution.name[i].c_str(),i,jnt_pos_out(i));
+    }
+    response.error_code.val = response.error_code.SUCCESS;
+    return true;
+  } else {
+    ROS_INFO("An IK solution could not be found");
+    response.error_code.val = response.error_code.NO_IK_SOLUTION;
+    return true;
+  }
 }
 
 int main(int argc, char **argv) {
