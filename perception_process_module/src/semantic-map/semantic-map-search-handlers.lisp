e7ab29a6c9fc9f8a52081155eb490e3a7277d2f3
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-05-18 16:31:03 +0200

    Refactored semantic-map support of perception process module
    We really ask the semantic map now for poses of top-level objects.

--- perception_process_module/src/semantic-map/semantic-map-search-handlers.lisp
+++ perception_process_module/src/semantic-map/semantic-map-search-handlers.lisp
@@ -29,82 +29,24 @@
 
 (in-package :perception-pm)
 
-(defclass semantic-map-object ()
-  ((pose :initarg :pose :reader object-pose)
-   (desig :initarg :desig :accessor object-desig :initform nil)
-   (timestamp :initarg :timestamp :reader object-timestamp)
-   (properties :initarg :properties :reader object-properties)
-   (probability :initform 0.0 :accessor perceived-object-probability)
-   (position-idx :initarg :position-idx
-                 :reader position-idx)
-   (height-idx :initarg :height-idx
-               :reader height-idx)))
+(defun prologify-obj-name (name)
+  (etypecase name
+    (symbol (rosify-lisp-name name))
+    (string name)))
 
-(defun link-name->pose (link-name)
-  (let ((tf (tf:lookup-transform *tf* :target-frame "/map" :source-frame link-name)))
-    (tf:make-pose-stamped (tf:frame-id tf) 0
-                          (cl-transforms:translation tf)
-                          (cl-transforms:rotation tf))))
-
-(def-fact-group semantic-map-obj-desigs ()
-  (<- (link-pose ?link-name ?pose)
-    (bound ?link-name)
-    (lisp-fun link-name->pose ?link-name ?pose))
-  
-  (<- (sem-map-obj-position left-of-sink "/ias_kitchen/drawer_sink_col1_center_link" 4))
-  (<- (sem-map-obj-position fridge "/ias_kitchen/handle_drawer_fridge_top_link" 8))
-  (<- (height low 0))
-  (<- (height middle 1))
-  (<- (height top 2))
-  (<- (height fridge 3))
-  
-  (<- (semantic-map-obj ?desig ?descr)
-    (desig-prop ?desig (type fridge))
-    (instance-of semantic-map-object ?descr)
-    (sem-map-obj-position fridge ?frame ?p-id)
-    (height fridge ?h-id)
-    (link-pose ?frame ?pose)
-    (slot-value ?descr pose ?pose)
-    (slot-value ?descr position-idx ?p-id)
-    (slot-value ?descr height-idx ?h-id)
-    (lisp-fun cut:current-timestamp ?timestamp)
-    (lisp-fun description ?desig ?props)
-    (slot-value ?descr timestamp ?timestamp)
-    (slot-value ?descr properties ?props))
-
-  (<- (semantic-map-obj ?desig ?descr)
-    (desig-prop ?desig (type drawer))
-    (desig-prop ?desig (sem-map-obj-position ?p))
-    (desig-prop ?desig (height ?h))
-    (sem-map-obj-position ?p ?frame ?p-id)
-    (height ?h ?h-id)
-    (link-pose ?frame ?pose)
-    (instance-of semantic-map-object ?descr)
-    (slot-value ?descr pose ?pose)    
-    (slot-value ?descr position-idx ?p-id)
-    (slot-value ?descr height-idx ?h-id)
-    (lisp-fun cut:current-timestamp ?timestamp)
-    (lisp-fun description ?desig ?props)
-    (slot-value ?descr timestamp ?timestamp)
-    (slot-value ?descr properties ?props)))
-
-(defmethod make-new-desig-description ((old-desig object-designator)
-                                       (po semantic-map-object))
-  (let ((obj-loc-desig (make-designator 'location `((pose ,(object-pose po))))))
-    (cons `(at ,obj-loc-desig)
-          (remove 'at (description old-desig) :key #'car))))
-
-(defun assert-semantic-map-desig (desig)
-  (with-vars-bound (?obj)
-      (lazy-car (prolog `(semantic-map-obj ,desig ?obj)))
-    (unless (is-var ?obj)
-      (assert-perceived-object ?obj (description desig))
-      ?obj)))
-
-(defmethod object-search-function ((type (cl:eql 'fridge)) desig &optional perceived-object)
-  (declare (ignore perceived-object))  
-  (assert-semantic-map-desig desig))
-
-(defmethod object-search-function ((type (cl:eql 'drawer)) desig &optional perceived-object)
+(def-object-search-function query-semantic-map-object semantic-map
+    (((name ?name)) desig perceived-object)
   (declare (ignore perceived-object))
-  (assert-semantic-map-desig desig))
+  (with-desig-props (name) desig
+    (with-vars-bound (?pose)
+        (json-prolog:prolog
+         `(and ("rootObjects" ?objs)
+               ("member" ?o ?objs)
+               ("rdf_atom_no_ns" ?o ,(format nil "'~a'" (prologify-obj-name name)))
+               ("objectPose" ?o ?pose)))
+      (unless (is-var ?pose)
+        (make-instance
+         'perceived-object
+         :pose ?pose
+         :probability 1.0
+         :desig desig)))))
