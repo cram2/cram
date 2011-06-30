3422f807b8a5271c03b7739da277e49c29200b5b
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-06-30 15:55:47 +0200

    Use a separate worker thread for triangulation and place the robot correctly on each query

--- bullet_reasoning/src/executive-integration/perception.lisp
+++ bullet_reasoning/src/executive-integration/perception.lisp
@@ -36,6 +36,9 @@
   "A weak hash-table that is used to map perceived-object instances to
   a corresponding object name used in the reasoning system.")
 
+(defvar *perception-worker-thread* nil)
+(defvar *perception-worker-input-queue* (make-instance 'physics-utils:event-queue))
+
 (defun get-obj-name (perceived-object)
   (or (gethash perceived-object *perceived-object-mappings*)
       (setf (gethash perceived-object *perceived-object-mappings*)
@@ -48,19 +51,64 @@
        `(cop-object ,name ,(perception-pm:object-pose perceived-object)
                     :object-id ,(perception-pm:object-id perceived-object)))))
 
+(defun maybe-create-perception-worker ()
+  (flet ((perception-worker ()
+           (loop do
+             (handler-case
+                 (let ((event (physics-utils:get-next-event *perception-worker-input-queue*)))
+                   (cond ((eql event :exit)
+                          (return nil))
+                         (t (funcall event))))
+               (error (e)
+                 (warn 'simple-warning
+                       :format-control "Worker thread throw an error: ~a"
+                       :format-arguments (list e)))))))
+    (unless (and *perception-worker-thread*
+                 (sb-thread:thread-alive-p *perception-worker-thread*))
+      (setf *perception-worker-thread*
+            (sb-thread:make-thread #'perception-worker :name "PERCEPTION-WORKER")))))
+
 (defun add-perceived-object (op &key ?desig ?perceived-object)
-  (unless *disable-rete-integration*
-    (when (eq op :assert)
-      (let* ((object-name (get-obj-name ?perceived-object))
-             (object-descr (get-prolog-object-description
-                            object-name ?desig ?perceived-object)))
-        (force-ll
-         (prolog `(and (bullet-world ?w)
-                       (assert-object ?w ,@object-descr)
-                       (contact ?w ,object-name ?c)
-                       (retract-object ?w ?c))))))))
+  (flet ((add ()
+           (let* ((object-name (get-obj-name ?perceived-object))
+                  (object-descr (get-prolog-object-description
+                                 object-name ?desig ?perceived-object)))
+             (force-ll
+              (prolog `(and (bullet-world ?w)
+                            (assert-object ?w ,@object-descr)))))))
+    (unless *disable-rete-integration*
+      (when (eq op :assert)
+        (maybe-create-perception-worker)
+        (physics-utils:post-event
+         *perception-worker-input-queue*
+         #'add)))))
+
+(defun on-perception-started (input)
+  (declare (ignore input))
+  (maybe-create-perception-worker)
+  ;; TODO: Don't be PR2 specific here
+  (flet ((place-robot ()
+           (with-vars-bound (?robot)
+               (lazy-car
+                (prolog `(and (bullet-world ?w)
+                              (robot ?robot-name)
+                              (%object ?w ?robot-name ?robot))))
+             (when ?robot
+               (set-robot-state-from-tf cram-roslisp-common:*tf* ?robot)))))
+    (physics-utils:post-event *perception-worker-input-queue* #'place-robot)))
+
+(defun on-pm-execute (op &key ?module ?input)
+  (when (eql ?module :perception)
+    (case op
+      (:assert
+         (roslisp:ros-info (perception reasoning) "Perception process module started")
+         (on-perception-started ?input))
+      (:retract
+         (roslisp:ros-info (perception reasoning) "Perception process module finished. Synchronizing.")
+         (physics-utils:wait-for-queue-empty *perception-worker-input-queue*)))))
 
 (def-production on-object-perceived
   (perception-pm:object-perceived ?desig ?perceived-object))
 
 (register-production-handler 'on-object-perceived 'add-perceived-object)
+(register-production-handler 'cram-plan-knowledge:on-pm-execute 'on-pm-execute)
