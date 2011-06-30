5c55a2181b6d5345004e579f20f011a54276cda7
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-06-30 11:34:54 +0200

    Added rete production for execution of process modules

--- cram_occasions_events/src/process-modules.lisp
+++ cram_occasions_events/src/process-modules.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; Copyright (c) 2011, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
@@ -10,9 +10,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Willow Garage, Inc. nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
 ;;; 
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -27,22 +28,13 @@
 ;;; POSSIBILITY OF SUCH DAMAGE.
 ;;;
 
-(defsystem cram-plan-knowledge
-  :author "Lorenz Moesenlechner <moesenle@in.tum.de>"
-  :license "BSD"
-  :description "Utilities for working with occasions and rete"
+(in-package :cram-plan-knowledge)
 
-  :depends-on (cram-reasoning
-               cram-language
-               cram-utilities
-               cram-execution-trace
-               alexandria
-               designators)
-  :components
-  ((:module "src"
-            :components
-            ((:file "package")
-             (:file "occasions" :depends-on ("package"))
-             (:file "tasks" :depends-on ("package" "time"))
-             (:file "time" :depends-on ("package"))
-             (:file "process-modules" :depends-on ("package"))))))
+(defmethod cram-process-modules:pm-execute :before (module input &key &allow-other-keys)
+  (rete-assert `(pm-executing ,module ,input)))
+
+(defmethod cram-process-modules:pm-execute :after (module input &key &allow-other-keys)
+  (rete-retract `(pm-executing ,module ,input)))
+
+(def-production on-pm-execute
+  (pm-executing ?module ?input))
