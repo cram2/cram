e7b211ebc6782ea67d7e0cb85d9249b8a797f0e4
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-01-18 16:06:35 +0100

    Removed unused code

--- bullet_reasoning/src/semantic-map.lisp
+++ bullet_reasoning/src/semantic-map.lisp
@@ -55,35 +55,6 @@
            (declare (ignore link-name))
            (typep obj 'household-object))))
 
-;; (defmethod (setf joint-state) :around (new-value (sem-map semantic-map-object) name)
-;;   (with-slots (urdf links) sem-map
-;;     ;; We shouldn't use parent here but child for drawers. The problem
-;;     ;; is that in the current semantic map, the drawer is connected to
-;;     ;; the door by the joint. We will keep this code until the
-;;     ;; semantic map is fixed.
-;;     (let* ((child-link-name (cl-urdf:name (cl-urdf:child (gethash name (cl-urdf:joints urdf)))))
-;;            (parent-link-name (cl-urdf:name (cl-urdf:parent (gethash name (cl-urdf:joints urdf)))))
-;;            (child-link (gethash child-link-name links))
-;;            (parent-sem-map-obj (lazy-car
-;;                                 (sem-map-utils:sub-parts-with-name
-;;                                  sem-map (owl-name-from-urdf-name sem-map parent-link-name))))
-;;            (child-sem-map-obj (lazy-car
-;;                                 (sem-map-utils:sub-parts-with-name
-;;                                  sem-map  (owl-name-from-urdf-name sem-map child-link-name))))
-;;            (sem-map-obj (string-case (sem-map-utils:obj-type parent-sem-map-obj)
-;;                           ("Drawer" parent-sem-map-obj)
-;;                           (t child-sem-map-obj)))
-;;            (original-link-pose (pose child-link)))
-;;       (call-next-method)
-;;       (let* ((new-link-pose (pose child-link))
-;;              (diff (cl-transforms:transform*
-;;                     (cl-transforms:reference-transform new-link-pose)
-;;                     (cl-transforms:transform-inv
-;;                      (cl-transforms:reference-transform original-link-pose)))))
-;;         (format t "before 1~a~%" (sem-map-utils:pose sem-map-obj))
-;;         (sem-map-utils:update-pose sem-map-obj diff :relative t :recursive t)
-;;         (format t "after 1~a~%" (sem-map-utils:pose sem-map-obj))))))
-
 (defmethod (setf joint-state) :after (new-value (sem-map semantic-map-object) name)
   (update-semantic-map-joint sem-map name))
 
