fc1923255e927b0cd015c31ff819e046425151b5
Georg Bartels <georg.bartels@gmail.com>
2012-04-20 10:45:37 +0200

    Refactored testing of register-location-generator.
    Used macro with-location-designators to have all test cases in one test function.

--- cram_designators/test/designators-tests.lisp
+++ cram_designators/test/designators-tests.lisp
@@ -28,12 +28,12 @@
 
 (in-package :cram-designators-tests)
 
-;; main function to call to run tests
+;;; main function to call to run tests
 
 (defun run-designators-tests ()
   (run-tests))
 
-;; auxiliary generator and validation functions for testing
+;;; auxiliary generator and validation functions for testing
 
 (defun generator-1 (designator)
   (list :foo))
@@ -50,7 +50,7 @@
 (defun validation-3 (designator solution)
   (eq solution :bar))
 
-;; auxiliary functions
+;;; auxiliary functions
 
 (defun cleanup-generators-and-validators ()
   (delete-location-generator-function 'generator-1)
@@ -59,31 +59,31 @@
   (delete-location-validation-function 'validation-2)
   (delete-location-validation-function 'validation-3))
 
-;; auxiliary macros
+;;; auxiliary macros
 
 (defmacro with-location-designators ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (make-designator 'location nil)))
      ,@body))
 
 
-;; actual test cases
+;;; actual test cases
 
 (define-test register-location-generator-1
-  (with-location-designators (desig)
+  (with-location-designators (desig1 desig2)
     (cleanup-generators-and-validators)
+    ;; test basic registration functionality with one generator
     (register-location-generator 1 generator-1)
-    (assert-eql :foo (reference desig))
-    (assert-false (next-solution desig))))
-
-(define-test register-location-generator-2
-  (with-location-designators (desig)
-    (cleanup-generators-and-validators)
-    (register-location-generator 1 generator-1)
+    (assert-eql :foo (reference desig1))
+    (assert-false (next-solution desig1))
+    ;; see if caching after first reference worked
     (register-location-generator 2 generator-2)
-    (assert-eql :foo (reference desig))
-    (assert-true (next-solution desig))
-    (assert-eql :bar (reference (next-solution desig)))
-    (assert-false (next-solution (next-solution desig)))))
+    (assert-eql :foo (reference desig1))
+    (assert-false (next-solution desig1))
+    ;; test registration functionality with two generators
+    (assert-eql :foo (reference desig2))
+    (assert-true (next-solution desig2))
+    (assert-eql :bar (reference (next-solution desig2)))
+    (assert-false (next-solution (next-solution desig2)))))
 
 (define-test register-location-validation-function-1
   (with-location-designators (desig)
