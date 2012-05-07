70e47f687038c8b488093e65ec35cb2f24214da8
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-05-07 18:30:35 +0200

    Added new predicate TRAJECTORY-POINT to cram_plan_knowledge.
    It's inteded use case is for reachability reasoning. With this
    predicate, the manipulation process module can provide approximated
    trajectory points to execute an action.

--- cram_plan_knowledge/src/prolog.lisp
+++ cram_plan_knowledge/src/prolog.lisp
@@ -1,5 +1,4 @@
-;;;
-;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; Copyright (c) 2012, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
@@ -10,9 +9,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Willow Garage, Inc. nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
 ;;; 
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -25,30 +25,18 @@
 ;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
-;;;
 
-(defsystem cram-plan-knowledge
-  :author "Lorenz Moesenlechner <moesenle@in.tum.de>"
-  :license "BSD"
-  :description "Utilities for working with occasions and rete"
+(in-package :cram-plan-knowledge)
 
-  :depends-on (cram-reasoning
-               cram-language
-               cram-utilities
-               cram-execution-trace
-               process-modules
-               alexandria
-               designators
-               designators-ros)
-  :components
-  ((:module "src"
-            :components
-            ((:file "package")
-             (:file "occasions" :depends-on ("package"))
-             (:file "tasks" :depends-on ("package" "time"))
-             (:file "time" :depends-on ("package"))
-             (:file "process-modules" :depends-on ("package"))
-             (:file "event-protocol" :depends-on ("package"))
-             (:file "default-plan-events" :depends-on ("package" "event-protocol"))
-             (:file "belief-state" :depends-on ("package" "default-plan-events"))
-             (:file "prolog" :depends-on ("package"))))))
+(def-fact-group manipulateion (trajectory-point)
+
+  ;; Yields points that are part of a manipulation
+  ;; trajectory. ?designator is bound to an action designator. The
+  ;; intended use of this predicate is for reasoning about
+  ;; reachability. Since it is not always possible to generate the
+  ;; exactly same points that are used for the manipulation actions
+  ;; here, e.g. when grasp planning has to be done, ?point is not
+  ;; necessarily an exact trajectory point but it should at least be
+  ;; close to the actual point.
+  (<- (trajectory-point ?designator ?point)
+    (fail)))
