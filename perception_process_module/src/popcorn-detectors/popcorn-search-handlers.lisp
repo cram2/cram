309619c8002944f0f138cb9a4bd813a8435311ee
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-05-10 14:15:10 +0200

    Refactoring of popcorn perception search handlers.
     * The action call is in a function now since it was replicated for
       every single search handler.
    
     * Search spaces are specified via vectors now.
    
     * Support for more than one search result, don't throw away other
       search results.

--- perception_process_module/src/popcorn-detectors/popcorn-search-handlers.lisp
+++ perception_process_module/src/popcorn-detectors/popcorn-search-handlers.lisp
@@ -42,169 +42,86 @@
 (defclass lid-perceived-object (perceived-object)
   ())
 
-(def-object-search-function lid-search-function popcorn-detector
-    ;; Calls the perception routine behind the `ias_perception_actions' actiolib server
-    ;; and receives a common header containing the time stamp and the list of poses
-    ;; corresponding to the detected lids.    
-    ;; Takes the first pose, transforms it relative to a fix reference `/map' and then
-    ;; uses it to instantiate the `lid-perceived-object' which is returned.
-    (((type lid)) desig perceived-object)
-  (declare (ignore desig perceived-object))
-  (with-fields (objects)      
-      (actionlib:send-goal-and-wait
-       *popcorn-detectors-action*
-       (actionlib:make-action-goal *popcorn-detectors-action*
-         className "Lid"
-         numHits 1
-         (keywords_v keywords) (vector (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "min"
-                                                 (x v_value) 0.420
-                                                 (y v_value) 2.150
-                                                 (z v_value) 0.785)
-                                       (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "max"
-                                                 (x v_value) 0.610
-                                                 (y v_value) 2.400
-                                                 (z v_value) 0.900))))
-    (with-fields (header poses) objects
-      (with-fields (stamp frame_id) header
-        (when (> (length poses) 0)
-          (let ((pose (tf:msg->pose (elt poses 0))))
-            (make-instance 'lid-perceived-object
-              :pose (tf:copy-pose-stamped
-                     (tf:transform-pose
-                      ;; hack(moesenle): remove the time stamp because
-                      ;; tf fails to transform for some weird reason
-                      *tf*
-                      :pose (tf:pose->pose-stamped frame_id stamp pose)
-                      :target-frame "/map")
-                     :stamp 0.0)
-              :probability 1.0)))))))
-
 (defclass small-bowl-perceived-object (perceived-object)
   ())
-
-(def-object-search-function small-bowl-search-function popcorn-detector
-    ;; Calls the perception routine behind the `ias_perception_actions' actiolib server
-    ;; and receives a common header containing the time stamp and the list of poses
-    ;; corresponding to the small bowls detected.    
-    ;; Takes the first pose, transforms it relative to a fix reference `/map' and then
-    ;; uses it to instantiate the `small-bowl-perceived-object' which is returned.
-    (((type small-bowl)) desig perceived-object)
-  (declare (ignore desig perceived-object))
-  (with-fields (objects)      
-      (actionlib:send-goal-and-wait
-       *popcorn-detectors-action*
-       (actionlib:make-action-goal *popcorn-detectors-action*
-         className "SmallBowl"
-         numHits 1
-         (keywords_v keywords) (vector (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "min"
-                                                 (x v_value) 0.600
-                                                 (y v_value) 1.950
-                                                 (z v_value) 0.770)
-                                       (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "max"
-                                                 (x v_value) 0.900
-                                                 (y v_value) 2.100
-                                                 (z v_value) 0.900))))
-    (with-fields (header poses) objects
-      (with-fields (stamp frame_id) header
-        (when (> (length poses) 0)
-          (let ((pose (tf:msg->pose (elt poses 0))))
-            (make-instance 'small-bowl-perceived-object
-              :pose (tf:copy-pose-stamped
-                     (tf:transform-pose
-                      ;; hack(moesenle): remove the time stamp because
-                      ;; tf fails to transform for some weird reason
-                      *tf*
-                      :pose (tf:pose->pose-stamped frame_id stamp pose)
-                      :target-frame "/map")
-                     :stamp 0.0)
-              :probability 1.0)))))))
 
 (defclass pot-perceived-object (perceived-object)
   ())
 
-(def-object-search-function pot-search-function popcorn-detector
-    ;; Calls the perception routine behind the `ias_perception_actions' actiolib server
-    ;; and receives a common header containing the time stamp and the list of poses
-    ;; corresponding to the pot detected.    
-    ;; Takes the first pose, transforms it relative to a fix reference `/map' and then
-    ;; uses it to instantiate the `pot-perceived-object' which is returned.
-    (((type pot)) desig perceived-object)
-  (declare (ignore desig perceived-object))
-  (with-fields (objects)      
-      (actionlib:send-goal-and-wait
-       *popcorn-detectors-action*
-       (actionlib:make-action-goal *popcorn-detectors-action*
-         className "Pot"
-         numHits 1
-         (keywords_v keywords) (vector (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "min"
-                                                 (x v_value) -2.000
-                                                 (y v_value)  1.260
-                                                 (z v_value)  0.880)
-                                       (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "max"
-                                                 (x v_value) -1.700
-                                                 (y v_value)  1.700
-                                                 (z v_value)  1.000))))
-    (with-fields (header poses) objects
-      (with-fields (stamp frame_id) header
-        (when (> (length poses) 0)
-          (let ((pose (tf:msg->pose (elt poses 0))))
-            (list 
-             (make-instance 'pot-perceived-object
-               :pose (tf:copy-pose-stamped
-                      (tf:transform-pose
-                       ;; hack(moesenle): remove the time stamp because
-                       ;; tf fails to transform for some weird reason
-                       *tf*
-                       :pose (tf:pose->pose-stamped frame_id stamp pose)
-                       :target-frame "/map")
-                      :stamp 0.0)
-               :probability 1.0))))))))
-
 (defclass big-plate-perceived-object (perceived-object)
   ())
 
-(def-object-search-function big-plate-search-function popcorn-detector
-    ;; Calls the perception routine behind the `ias_perception_actions' actiolib server
-    ;; and receives a common header containing the time stamp and the list of poses
-    ;; corresponding to the big plates detected.    
-    ;; Takes the first pose, transforms it relative to a fix reference `/map' and then
-    ;; uses it to instantiate the `big-plate-perceived-object' which is returned.
-    (((type big-plate)) desig perceived-object)
-  (declare (ignore desig perceived-object))
+(defun call-popcorn-perception-action
+    (&key class-name (num-hits 1) max min additional-keywords)
+  "Calls the perception routine behind the `ias_perception_actions'
+  actiolib server and receives a common header containing the time
+  stamp and the list of poses corresponding to the detected
+  lids. Takes all poses, transforms them relative to a fix
+  reference *FIXED-FRAME* and returns the pose"
   (with-fields (objects)      
       (actionlib:send-goal-and-wait
        *popcorn-detectors-action*
        (actionlib:make-action-goal *popcorn-detectors-action*
-         className "BigPlate"
-         numHits 1
-         (keywords_v keywords) (vector (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "min"
-                                                 (x v_value) -2.000
-                                                 (y v_value)  1.260
-                                                 (z v_value)  0.910)
-                                       (make-msg "ias_perception_actions/Keyword_v"
-                                                 key "max"
-                                                 (x v_value) -1.700
-                                                 (y v_value)  1.700
-                                                 (z v_value)  1.000))))
-    (with-fields (header poses) objects
-      (with-fields (stamp frame_id) header
-        (when (> (length poses) 0)
-          (let ((pose (tf:msg->pose (elt poses 0))))
-            (list 
-             (make-instance 'big-plate-perceived-object
-               :pose (tf:copy-pose-stamped
-                      (tf:transform-pose
-                       ;; hack(moesenle): remove the time stamp because
-                       ;; tf fails to transform for some weird reason
-                       *tf*
-                       :pose (tf:pose->pose-stamped frame_id stamp pose)
-                       :target-frame "/map")
-                      :stamp 0.0)
-               :probability 1.0))))))))
+         className class-name
+         numHits num-hits
+         (keywords_v keywords) (concatenate
+                                'vector
+                                (vector
+                                 (make-msg "ias_perception_actions/Keyword_v"
+                                           key "min" v_value (tf:point->msg min))
+                                 (make-msg "ias_perception_actions/Keyword_v"
+                                           key "max" v_value (tf:point->msg max)))
+                                additional-keywords)))
+    (with-fields ((stamp (stamp header))
+                  (frame-id (frame_id header))
+                  (poses poses))
+        objects
+      (map 'list (lambda (pose-message)
+                   (tf:transform-pose
+                    *tf*
+                    :target-frame *fixed-frame*
+                    :pose (tf:pose->pose-stamped
+                           frame-id stamp
+                           (tf:msg->pose pose-message))))
+           poses))))
+
+;; TODO(moesenle): Add support for doc-strings in DEF-OBJECT-SEARCH-FUNCTION.
+(def-object-search-function lid-search-function popcorn-detector
+    (((type lid)) desig perceived-object)
+  (declare (ignore desig perceived-object))
+  (mapcar (lambda (pose)
+            (make-instance 'lid-perceived-object :pose pose :probability 1.0))
+          (call-popcorn-perception-action
+           :class-name "Lid"
+           :min (cl-transforms:make-3d-vector 0.420 2.150 0.785)
+           :max (cl-transforms:make-3d-vector 0.610 2.400 0.900))))
+
+(def-object-search-function small-bowl-search-function popcorn-detector
+    (((type small-bowl)) desig perceived-object)
+  (declare (ignore desig perceived-object))
+  (mapcar (lambda (pose)
+            (make-instance 'small-bowl-perceived-object :pose pose :probability 1.0))
+          (call-popcorn-perception-action
+           :class-name "SmallBowl"
+           :min (cl-transforms:make-3d-vector 0.600 1.950 0.770)
+           :max (cl-transforms:make-3d-vector 0.900 2.100 0.900))))
+
+(def-object-search-function pot-search-function popcorn-detector
+    (((type pot)) desig perceived-object)
+  (declare (ignore desig perceived-object))
+  (mapcar (lambda (pose)
+            (make-instance 'pot-perceived-object :pose pose :probability 1.0))
+          (call-popcorn-perception-action
+           :class-name "Pot"
+           :min (cl-transforms:make-3d-vector -2.0 1.26 0.88)
+           :max (cl-transforms:make-3d-vector -1.7 1.7 1.0))))
+
+(def-object-search-function big-plate-search-function popcorn-detector
+    (((type big-plate)) desig perceived-object)
+  (declare (ignore desig perceived-object))
+  (mapcar (lambda (pose)
+            (make-instance 'big-plate-perceived-object :pose pose :probability 1.0))
+          (call-popcorn-perception-action
+           :class-name "BigPlate"
+           :min (cl-transforms:make-3d-vector -2.0 1.26 0.91)
+           :max (cl-transforms:make-3d-vector -1.7 1.7 1.0))))
