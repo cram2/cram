604125a3593311f2b59abf08f04b24fa19c59f65
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-05-25 15:59:47 +0200

    Added handlers for OBJECT-ATTACHED and OBJECT-DETACHED events.

--- bullet_reasoning_demo/src/event-handlers.lisp
+++ bullet_reasoning_demo/src/event-handlers.lisp
@@ -1,5 +1,4 @@
-;;;
-;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; Copyright (c) 2012, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
@@ -26,28 +25,32 @@
 ;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
-;;;
 
-(defsystem bullet-reasoning-demo
-    :author "Lorenz Moesenlechner"
-    :license "BSD"
-    
-    :depends-on (semantic-map-costmap
-                 occupancy-grid-costmap
-                 perception-process-module
-                 cram-roslisp-common
-                 cram-plan-knowledge
-                 bullet-reasoning
-                 bullet-reasoning-designators
-                 pr2-manipulation-knowledge)
-    :components
-    ((:module "src"
-              :components
-              ((:file "designator-config")
-               (:file "pr2-metadata")
-               (:file "event-handlers")
-               ;; (:module "executive-integration"
-               ;;  :components
-               ;;  ((:file "perception")
-               ;;   (:file "perception-facts")))
-               ))))
+(in-package :btr)
+
+(defun get-robot-object ()
+  (with-vars-bound (?robot-name)
+      (lazy-car (prolog `(robot ?robot-name)))
+    (unless (is-var ?robot-name)
+      (object *current-bullet-world* ?robot-name))))
+
+(defun get-designator-object (object-designator)
+  (let ((object-designator (desig:newest-valid-designator object-designator)))
+    (when object-designator
+      (object
+       *current-bullet-world*
+       (desig:object-identifier (desig:reference object-designator))))))
+
+(defmethod plan-knowledge:on-event attach-objects
+    ((event plan-knowledge:object-attached))
+  (let ((robot (get-robot-object))
+        (object (get-designator-object (plan-knowledge:event-object event))))
+    (when object
+      (attach-object robot object (plan-knowledge:event-link event)))))
+
+(defmethod plan-knowledge:on-event detach-objects
+    ((event plan-knowledge:object-detached))
+  (let ((robot (get-robot-object))
+        (object (get-designator-object (plan-knowledge:event-object event))))
+    (when object
+      (detach-object robot object (plan-knowledge:event-link event)))))
