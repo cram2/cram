2c85c4e39d151004bdabcbf7fc137c90a4649f38
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-05-31 15:26:08 +0200

    Fixed a small bug in semantic-map-height-generator.
    When a point is not on an object, the sequence HEIGHTS will be NIL.
    Then, RANDOM-ELT will throw an error. Handle that case by setting the
    height to 0.0.

--- semantic_map_costmap/src/cost-functions.lisp
+++ semantic_map_costmap/src/cost-functions.lisp
@@ -1,20 +1,20 @@
 ;;;
 ;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
-;;; 
+;;;
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions are met:
-;;; 
+;;;
 ;;;     * Redistributions of source code must retain the above copyright
 ;;;       notice, this list of conditions and the following disclaimer.
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
 ;;;     * Neither the name of the Intelligent Autonomous Systems Group/
-;;;       Technische Universitaet Muenchen nor the names of its contributors 
-;;;       may be used to endorse or promote products derived from this software 
+;;;       Technische Universitaet Muenchen nor the names of its contributors
+;;;       may be used to endorse or promote products derived from this software
 ;;;       without specific prior written permission.
-;;; 
+;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -158,7 +158,9 @@ list of SEM-MAP-UTILS:SEMANTIC-MAP-GEOMs"
     (let ((heights (loop for obj in (cut:force-ll objects)
                          when (point-on-object obj (cl-transforms:make-3d-vector x y 0))
                            collecting (float (obj-z-value obj type-tag) 0.0d0))))
-      (alexandria:random-elt heights))))
+      (if heights
+          (alexandria:random-elt heights)
+          0.0d0))))
 
 (defun make-constant-height-function (height)
   (lambda (x y)
