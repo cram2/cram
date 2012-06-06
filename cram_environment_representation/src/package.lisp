5d022381a85c319f5c961375e768721375b23cbd
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-06-06 13:34:42 +0200

    Specified designator properties for cram-environment-representation.

--- cram_environment_representation/src/package.lisp
+++ cram_environment_representation/src/package.lisp
@@ -28,10 +28,11 @@
 
 (in-package :cl-user)
 
-(defpackage cram-environment-representation
+(desig-props:def-desig-package cram-environment-representation
   (:use #:common-lisp #:cram-plan-library #:cram-plan-knowledge
         #:bullet-reasoning #:cram-reasoning #:cram-utilities)
   (:shadowing-import-from
    #:bullet-reasoning side robot at throughout object during holds)
   (:shadowing-import-from #:cram-plan-knowledge event)
-  (:export))
+  (:export)
+  (:desig-properties pose in gripper z-offset))
