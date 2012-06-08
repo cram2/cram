795409279bfeef54927d448817bde338e0598049
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-06-08 16:30:29 +0200

    Improved/fixed implementation of the TRAJECTORY-POINT predicate.
    For putting down objects, use the information stored in the object
    designator to calculate the put-down gripper pose.

--- pr2_manipulation_knowledge/src/pick-and-place.lisp
+++ pr2_manipulation_knowledge/src/pick-and-place.lisp
@@ -28,6 +28,25 @@
 
 (in-package :pr2-manipulation-knowledge)
 
+(defun calculate-put-down-hand-pose (object-designator put-down-pose)
+  (let ((current-object (desig:current-desig object-designator)))
+    (desig:with-desig-props (desig-props:at) current-object
+      (assert desig-props:at () "Object ~a needs to have an `at' property"
+              current-object)
+      (desig:with-desig-props (in pose z-offset) at
+        (assert (eq in 'gripper) ()
+                "Object ~a needs to be in the gripper" current-object)
+        (assert pose () "Object ~a needs to have a `pose' property" current-object)
+        (assert z-offset () "Object ~a needs to have a `height' property" current-object)
+        (cl-transforms:transform->pose
+         (cl-transforms:transform*
+          (cl-transforms:pose->transform put-down-pose)
+          (cl-transforms:make-transform
+           (cl-transforms:make-3d-vector 0 0 desig-props:z-offset)
+           (cl-transforms:make-identity-rotation))
+          (cl-transforms:transform-inv
+           (cl-transforms:pose->transform desig-props:pose))))))))
+
 (def-fact-group pick-and-place-manipulation (trajectory-point)
 
   (<- (trajectory-point ?designator ?point ?side)
@@ -35,13 +54,15 @@
     (desig-prop ?designator (to grasp))
     (desig-prop ?designator (obj ?obj))
     (desig-prop ?designator (side ?side))
-    (obj-desig-location ?obj ?pose)
+    (desig-location-prop ?obj ?pose)
     (lisp-fun cl-transforms:origin ?pose ?point))
 
   (<- (trajectory-point ?designator ?point ?side)
     (trajectory-desig? ?designator)
     (desig-prop ?designator (to put-down))
-    (desig-prop ?designator (obj ?_))
+    (desig-prop ?designator (obj ?object))
     (desig-prop ?designator (side ?side))
-    (desig-prop ?designator (at ?pose))
-    (lisp-fun cl-transforms:origin ?pose ?point)))
+    (desig-prop ?designator (at ?location))
+    (lisp-fun current-desig ?location ?current-location)
+    (lisp-fun reference ?current-location ?put-down-pose)
+    (lisp-fun calculate-put-down-hand-pose ?object ?put-down-pose ?point)))
