92b997159ce2fcb835ff42f849e9a829c0b30288
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-07-27 14:13:02 +0200

    Changed orientations that are generated.
    Now we generate 8 rotations of the top grasp and 8 rotations of side
    grasps.

--- pr2_reachability_costmap/src/generate-reachability-map.lisp
+++ pr2_reachability_costmap/src/generate-reachability-map.lisp
@@ -31,9 +31,11 @@
 (defparameter *minimum* (cl-transforms:make-3d-vector -1.0 -1.5 -0.75))
 (defparameter *maximum* (cl-transforms:make-3d-vector 1.0 1.5 0.5))
 (defparameter *steps* (cl-transforms:make-3d-vector 0.05 0.05 0.05))
-(defparameter *angles* (list*
-                        (cl-transforms:make-identity-rotation)
-                        (cl-transforms:euler->quaternion :ay (/ pi -2))
+(defparameter *angles* (append
+                        (loop for i from (- pi) below pi by (/ pi 4)
+                              collecting (cl-transforms:q*
+                                          (cl-transforms:euler->quaternion :az i)
+                                          (cl-transforms:euler->quaternion :ay (/ pi 2))))
                         (loop for i from (- pi) below pi by (/ pi 4)
                               collecting (cl-transforms:euler->quaternion :az i))))
 
