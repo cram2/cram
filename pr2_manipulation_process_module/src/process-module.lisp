c8e5d9b6b9ffad0d73b48e902844dd12eda3739c
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-08-01 10:40:46 +0200

    Fixed indentation an line breaks in manipulation process module.

--- pr2_manipulation_process_module/src/process-module.lisp
+++ pr2_manipulation_process_module/src/process-module.lisp
@@ -216,80 +216,106 @@ for the currently type of grasped object."
         (t (put-down-grasped-object-with-single-arm object-designator location side obstacles))))
 
 (defun grab-object-with-handles (obj side)
-  "Grasp an object `obj' on one of its handles with the specified gripper side `side'. This includes going into pregrasp for the nearest handle, opening the gripper, going into the grasp position, closing the gripper and lifting the object by 0.2m by default."
+  "Grasp an object `obj' on one of its handles with the specified
+gripper side `side'. This includes going into pregrasp for the nearest
+handle, opening the gripper, going into the grasp position, closing
+the gripper and lifting the object by 0.2m by default."
   (with-desig-props (handles) obj
-		    ;; Check if there are handles
-		    (assert (> (length handles) 0) () "Object ~a needs at least one handle." obj)
-		    ;, Get the nearest (atm the first) handle
-		    (let ((nearest-handle (nearest-handle-for-side obj side)))
-		      ;; Go into pregrasp for that handle on side `side'
-		      (pregrasp-handled-object-with-relative-location obj side nearest-handle)
-          ;; Open gripper
-          (open-gripper side :position 0.04)
-          ;; Go into grasp pose
-          (grasp-handled-object-with-relative-location obj side nearest-handle)
-          ;; Close gripper
-          (close-gripper side :position 0.01)
-          ;; Lift object
-          (lift-handled-object-with-relative-location obj side nearest-handle))))
+    ;; Check if there are handles
+    (assert (> (length handles) 0) () "Object ~a needs at least one handle." obj)
+                                        ;, Get the nearest (atm the first) handle
+    (let ((nearest-handle (nearest-handle-for-side obj side)))
+      ;; Go into pregrasp for that handle on side `side'
+      (pregrasp-handled-object-with-relative-location obj side nearest-handle)
+      ;; Open gripper
+      (open-gripper side :position 0.04)
+      ;; Go into grasp pose
+      (grasp-handled-object-with-relative-location obj side nearest-handle)
+      ;; Close gripper
+      (close-gripper side :position 0.01)
+      ;; Lift object
+      (lift-handled-object-with-relative-location obj side nearest-handle))))
 
-(defun taxi-handled-object (obj side relative-handle-loc &key (relative-gripper-pose (tf:make-identity-pose)))
-  "Commutes the arm to a certain absolute pose. The target pose is determined through the absolute object pose of object `obj', the relative object handle location `relative-handle-loc' and the relative gripper pose `relative-gripper-pose' w.r.t. the handle. The relative gripper pose defaults to an identity pose."
-  (let ((absolute-loc (object-handle-absolute obj relative-handle-loc :handle-offset-pose relative-gripper-pose)))
+(defun taxi-handled-object (obj side relative-handle-loc
+                            &key (relative-gripper-pose (tf:make-identity-pose)))
+  "Commutes the arm to a certain absolute pose. The target pose is
+determined through the absolute object pose of object `obj', the
+relative object handle location `relative-handle-loc' and the relative
+gripper pose `relative-gripper-pose' w.r.t. the handle. The relative
+gripper pose defaults to an identity pose."
+  (let ((absolute-loc (object-handle-absolute
+                       obj relative-handle-loc :handle-offset-pose relative-gripper-pose)))
     (let ((move-ik (get-ik side (desig-prop-value absolute-loc 'desig-props:pose))))
-      (assert (not (eq move-ik nil)) () "IK solution generation for side ~a failed during taxi for location designator ~a.~%" side absolute-loc)
+      (assert
+       (not (eq move-ik nil)) ()
+       "IK solution generation for side ~a failed during taxi for location designator ~a.~%"
+       side absolute-loc)
       (let ((move-trajectory (ik->trajectory (first move-ik) :duration 5.0)))
-        (assert (not (eq move-trajectory nil)) () "Trajectory generation for side ~a failed during taxi for location designator ~a.~%" side move-trajectory)
+        (assert
+         (not (eq move-trajectory nil)) ()
+         "Trajectory generation for side ~a failed during taxi for location designator ~a.~%"
+         side move-trajectory)
         (multiple-value-bind (result resultflag) (execute-arm-trajectory side move-trajectory)
           resultflag)))))
 
 (defun lift-handled-object-with-relative-location (obj side relative-handle-loc &key (lift-height 0.2))
-  "Moves the gripper side `side' into the lift position with respect to the object's `obj' handle `handle'. The lift height is specified through the key parameter `lift-height'."
-  (taxi-handled-object obj side relative-handle-loc :relative-gripper-pose 
-                       (cl-transforms:transform-pose
-                        (tf:make-transform
-                         (tf:make-3d-vector 0.12 0.0 0.0)
-                         (tf:euler->quaternion :az pi :ax (/ pi 2)))
-                        (tf:make-pose
-                         (tf:make-3d-vector 0.0 0.0 lift-height)
-                         (tf:make-identity-rotation)))))
-
+  "Moves the gripper side `side' into the lift position with respect
+to the object's `obj' handle `handle'. The lift height is specified
+through the key parameter `lift-height'."
+  (taxi-handled-object
+   obj side relative-handle-loc :relative-gripper-pose 
+   (cl-transforms:transform-pose
+    (tf:make-transform
+     (tf:make-3d-vector 0.12 0.0 0.0)
+     (tf:euler->quaternion :az pi :ax (/ pi 2)))
+    (tf:make-pose
+     (tf:make-3d-vector 0.0 0.0 lift-height)
+     (tf:make-identity-rotation)))))
 
 (defun grasp-handled-object-with-relative-location (obj side relative-handle-loc)
-  "Moves the gripper side `side' into the grasp position with respect to the object's `obj' handle `handle'."
-  (taxi-handled-object obj side relative-handle-loc :relative-gripper-pose
-                       (tf:make-pose (tf:make-3d-vector 0.12 0.0 0.0) (tf:euler->quaternion :az pi :ax (/ pi 2)))))
+  "Moves the gripper side `side' into the grasp position with respect
+to the object's `obj' handle `handle'."
+  (taxi-handled-object
+   obj side relative-handle-loc :relative-gripper-pose
+   (tf:make-pose
+    (tf:make-3d-vector 0.12 0.0 0.0)
+    (tf:euler->quaternion :az pi :ax (/ pi 2)))))
 
 (defun pregrasp-handled-object-with-relative-location (obj side relative-handle-loc)
-  "Moves the gripper side `side' into the pregrasp position with respect to the object's `obj' handle `handle'."
-  (taxi-handled-object obj side relative-handle-loc :relative-gripper-pose
-                       (tf:make-pose
-                        (tf:make-3d-vector 0.2 0.0 0.0)
-                        (tf:euler->quaternion :az pi :ax (/ pi 2)))))
-
+  "Moves the gripper side `side' into the pregrasp position with
+respect to the object's `obj' handle `handle'."
+  (taxi-handled-object
+   obj side relative-handle-loc :relative-gripper-pose
+   (tf:make-pose
+    (tf:make-3d-vector 0.2 0.0 0.0)
+    (tf:euler->quaternion :az pi :ax (/ pi 2)))))
 
 (defun object-handle-absolute (obj handle &key (handle-offset-pose (tf:make-identity-pose)))
-  "Transforms the relative handle location `handle' of object `obj' into the object's coordinate system and returns the appropriate location designator. The optional parameter `handle-offset-pose' is applied to the handle pose before the absolute object pose is applied."
+  "Transforms the relative handle location `handle' of object `obj'
+into the object's coordinate system and returns the appropriate
+location designator. The optional parameter `handle-offset-pose' is
+applied to the handle pose before the absolute object pose is
+applied."
   (let* ((absolute-object-loc (desig-prop-value obj 'desig-props:location))
-	 (absolute-object-pose-stamped (desig-prop-value absolute-object-loc 'desig-props:pose)))
+         (absolute-object-pose-stamped (desig-prop-value absolute-object-loc 'desig-props:pose)))
     (let ((relative-handle-pose
-	   (cl-transforms:transform-pose
-	    (tf:pose->transform
-	     (desig-prop-value handle 'desig-props:pose))
-	     handle-offset-pose)))
+            (cl-transforms:transform-pose
+             (tf:pose->transform
+              (desig-prop-value handle 'desig-props:pose))
+             handle-offset-pose)))
       (make-designator 'location `((desig-props:pose
-				    ,(tf:pose->pose-stamped
-				      (tf:frame-id absolute-object-pose-stamped)
-				      (tf:stamp absolute-object-pose-stamped)
-				      (cl-transforms:transform-pose
-				       (tf:pose->transform absolute-object-pose-stamped)
-				       relative-handle-pose))))))))
+                                    ,(tf:pose->pose-stamped
+                                      (tf:frame-id absolute-object-pose-stamped)
+                                      (tf:stamp absolute-object-pose-stamped)
+                                      (cl-transforms:transform-pose
+                                       (tf:pose->transform absolute-object-pose-stamped)
+                                       relative-handle-pose))))))))
 
 (defun nearest-handle-for-side (obj side)
   "Get the nearest handle location designator on object `obj' in respect to the chosen gripper side `side'."
   ;; TODO(winkler): Implement *actual* calculations concerning distance here. Atm, this always returns the first handle on the object. This is no problem as long as we only have one handle.
   (with-desig-props (handles) obj
-		    (first handles)))
+    (first handles)))
 
 (defun put-down-grasped-object-with-single-arm (obj location side obstacles)
   (roslisp:ros-info (pr2-manip process-module) "Putting down object single-handedly.")
@@ -347,26 +373,26 @@ for the currently type of grasped object."
          ;; result is a pose and still needs to get poseStamped
          ;; NOTE: intended frame_id: /base_footprint
          (left-put-down-pose (cl-transforms:transform-pose
-                (cl-transforms:pose->transform
-                 (cl-transforms:transform-pose
-                  goal-transform-obj
-                  *pot-relative-left-handle-transform*))
-                (cl-transforms:make-pose
-                 (cl-transforms:make-3d-vector
-                  (+ (- *grasp-distance*)) 0.0 0.0)
-                 (cl-transforms:make-identity-rotation))))
+                              (cl-transforms:pose->transform
+                               (cl-transforms:transform-pose
+                                goal-transform-obj
+                                *pot-relative-left-handle-transform*))
+                              (cl-transforms:make-pose
+                               (cl-transforms:make-3d-vector
+                                (+ (- *grasp-distance*)) 0.0 0.0)
+                               (cl-transforms:make-identity-rotation))))
          ;; T_centerPot_in_base * T_rightHandle_in_centerPot * T_tool_in_wrist_INVERSE
          ;; result is a pose and still needs to get poseStamped
          ;; NOTE: intended frame_id: /base_footprint
          (right-put-down-pose (cl-transforms:transform-pose
-                (cl-transforms:pose->transform
-                 (cl-transforms:transform-pose
-                  goal-transform-obj
-                  *pot-relative-right-handle-transform*))
-                (cl-transforms:make-pose
-                 (cl-transforms:make-3d-vector
-                  (+ (- *grasp-distance*)) 0.0 0.0)
-                 (cl-transforms:make-identity-rotation))))
+                               (cl-transforms:pose->transform
+                                (cl-transforms:transform-pose
+                                 goal-transform-obj
+                                 *pot-relative-right-handle-transform*))
+                               (cl-transforms:make-pose
+                                (cl-transforms:make-3d-vector
+                                 (+ (- *grasp-distance*)) 0.0 0.0)
+                                (cl-transforms:make-identity-rotation))))
          ;; make 'em stamped poses
          (left-put-down-stamped-pose (cl-tf:make-pose-stamped
                                       "/base_footprint"
@@ -387,10 +413,10 @@ for the currently type of grasped object."
                                            (cl-transforms:origin left-put-down-stamped-pose)
                                            (cl-transforms:make-3d-vector 0 0 *pre-put-down-distance*))))
          (right-pre-put-down-pose (tf:copy-pose-stamped
-                                  right-put-down-stamped-pose
-                                  :origin (cl-transforms:v+
-                                           (cl-transforms:origin right-put-down-stamped-pose)
-                                           (cl-transforms:make-3d-vector 0 0 *pre-put-down-distance*))))
+                                   right-put-down-stamped-pose
+                                   :origin (cl-transforms:v+
+                                            (cl-transforms:origin right-put-down-stamped-pose)
+                                            (cl-transforms:make-3d-vector 0 0 *pre-put-down-distance*))))
          ;; calc unhand poses
          (left-unhand-pose (cl-transforms:transform-pose
                             (cl-transforms:reference-transform left-put-down-stamped-pose)
@@ -398,10 +424,10 @@ for the currently type of grasped object."
                              (cl-transforms:make-3d-vector (- *pre-put-down-distance*) 0 0)
                              (cl-transforms:make-identity-rotation))))
          (right-unhand-pose (cl-transforms:transform-pose
-                            (cl-transforms:reference-transform right-put-down-stamped-pose)
-                            (cl-transforms:make-pose
-                             (cl-transforms:make-3d-vector (- *pre-put-down-distance*) 0 0)
-                             (cl-transforms:make-identity-rotation))))
+                             (cl-transforms:reference-transform right-put-down-stamped-pose)
+                             (cl-transforms:make-pose
+                              (cl-transforms:make-3d-vector (- *pre-put-down-distance*) 0 0)
+                              (cl-transforms:make-identity-rotation))))
          (left-unhand-pose-stamped (tf:make-pose-stamped
                                     (tf:frame-id left-put-down-stamped-pose)
                                     (tf:stamp left-put-down-stamped-pose)
