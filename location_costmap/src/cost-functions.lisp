3aefa12ab1cb917aa10265daae1a0fe890fb3b50
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-08-06 19:29:58 +0200

    Revert "Fixed wrong calculation of steps in grid costmap."
    This reverts commit f3d314ec1bd56d748779dff1852805c172f20d74.

--- location_costmap/src/cost-functions.lisp
+++ location_costmap/src/cost-functions.lisp
@@ -123,7 +123,8 @@ in a value of 1.0"
                          below (map-coordinate->array-index
                                 end-y (resolution costmap-metadata)
                                 (origin-y costmap-metadata))
-                           by (/ (resolution grid) (resolution costmap-metadata))
+                           by (/ (resolution costmap-metadata)
+                                 (resolution costmap-metadata))
                        do (loop for x-source-index from (map-coordinate->array-index
                                                          start-x (resolution grid) (origin-x grid))
                                   below (map-coordinate->array-index
@@ -135,7 +136,8 @@ in a value of 1.0"
                                   below (map-coordinate->array-index
                                          end-x (resolution costmap-metadata)
                                          (origin-x costmap-metadata))
-                                    by (/ (resolution grid) (resolution costmap-metadata))
+                                    by (/ (resolution costmap-metadata)
+                                          (resolution costmap-metadata))
                                 do (setf
                                     (aref matrix
                                           (truncate y-destination-index)
