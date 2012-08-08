22abb0ce59e7956ac55065310523a45a73ce9eb1
Jan Winkler <winkler@cs.uni-bremen.de>
2012-08-08 10:38:32 +0200

    Removed unnecessary lines
    Removed lines only containing ";;;".

--- perception_process_module/src/package.lisp
+++ perception_process_module/src/package.lisp
@@ -1,4 +1,3 @@
-;;;
 ;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
 ;;;
@@ -25,7 +24,6 @@
 ;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
-;;;
 
 (in-package :cl-user)
 
