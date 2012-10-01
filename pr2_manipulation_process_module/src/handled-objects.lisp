cbf0e17499d456f47c9bdc04d52c8f07c50ae540
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2012-10-01 12:34:13 +0200

    Added elbow-up and elbow-down functionality for seed-state calculation.

--- pr2_manipulation_process_module/src/handled-objects.lisp
+++ pr2_manipulation_process_module/src/handled-objects.lisp
@@ -59,10 +59,11 @@ gripper pose defaults to an identity pose."
          (absolute-pose-map (reference absolute-loc))
          (absolute-pose (tf:transform-pose *tf*
                                            :pose absolute-pose-map
-                                           :target-frame "torso_lift_link"))
+                                           :target-frame "torso_lift_link"))  
          ;; This makes reach the carry-pose impossible
          ;; atm. :seed-state (calc-seed-state-elbow-up side))))
-         (move-ik (get-ik side absolute-pose)))
+         (move-ik (get-ik side absolute-pose
+                          :seed-state (calc-seed-state-elbow-up side :elbow-up t :elbow-out t))))
     (unless move-ik (cpl:fail
                      'cram-plan-failures:manipulation-pose-unreachable))
     (let ((move-trajectory (ik->trajectory (first move-ik) :duration 5.0)))
