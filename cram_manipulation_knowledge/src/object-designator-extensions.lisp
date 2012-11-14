fb741023d5c83a7d38221ac0de2427297bf35bc7
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-11-14 11:31:54 +0100

    Fixed shape message generation from the mesh mixin.

--- cram_manipulation_knowledge/src/object-designator-extensions.lisp
+++ cram_manipulation_knowledge/src/object-designator-extensions.lisp
@@ -78,15 +78,16 @@
        "arm_navigation_msgs/Shape"
        :type (roslisp-msg-protocol:symbol-code
               'arm_navigation_msgs-msg:shape :mesh)
-       :triangles (reduce (lambda (result triangle)
-                            (destructuring-bind (vector . index)
-                                result
-                              (assert (eql (list-length triangle) 3))
-                              (loop for point in triangle
-                                    for i from index
-                                    do (setf (aref vector i) point)
-                                    finally (return (cons vector i)))))
-                          faces :initial-value (make-array (* 3 (length faces))))
+       :triangles (car
+                   (reduce (lambda (result triangle)
+                             (destructuring-bind (vector . index)
+                                 result
+                               (assert (eql (list-length triangle) 3))
+                               (loop for point in triangle
+                                     for i from index
+                                     do (setf (aref vector i) point)
+                                     finally (return (cons vector i)))))
+                           faces :initial-value (cons (make-array (* 3 (length faces))) 0)))
        :vertices (map 'vector (lambda (point)
                                 (roslisp:make-msg
                                  "geometry_msgs/Point"
