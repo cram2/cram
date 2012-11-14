5bb58f5e895ca46d8537b6ea27a768eac28b02b4
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-11-14 16:36:46 +0100

    Implemented simple-semantic-map for maps without an URDF.
     * Created a base class SEMANTIC-MAP-OBJECT and renamed the old one to
       URDF-SEMANTIC-MAP-OBJECT.
    
     * Added SIMPLE-SEMANTIC-MAP-OBJECT that is used when no urdf has been
       specified. It adds all geoms as simple boxes.

--- bullet_reasoning/src/semantic-map.lisp
+++ bullet_reasoning/src/semantic-map.lisp
@@ -30,15 +30,10 @@
 
 (in-package :btr)
 
-(defclass semantic-map-object (robot-object)
+(defclass semantic-map-object (object)
   ((semantic-map :reader semantic-map
                  :initform (sem-map-utils:get-semantic-map)
-                 :initarg :semantic-map)
-   (link-offsets :initform (make-hash-table :test 'equal)
-                 :reader link-offsets
-                 :documentation "Mapping of link names to pose offsets
-                 between the body that corresponds to the link and the
-                 corresponding object in the semantic map.")))
+                 :initarg :semantic-map)))
 
 (defgeneric semantic-map-part-pose (semantic-map part-name)
   (:documentation "Returns the pose of the part `part-name'.")
@@ -55,132 +50,27 @@
                :format-arguments (list part-name)))
       (sem-map-utils:pose part))))
 
-(defmethod initialize-instance :after ((semantic-map semantic-map-object)
-                                       &key)
-  (with-slots (link-offsets) semantic-map
-    (dolist (part (sem-map-utils:semantic-map-parts
-                   (semantic-map semantic-map) :recursive t))
-      (when (and (sem-map-utils:urdf-name part)
-                 (typep part 'sem-map-utils:semantic-map-geom))
-        (let ((link-pose (link-pose semantic-map (sem-map-utils:urdf-name part))))
-          (assert link-pose)
-          (setf (gethash (sem-map-utils:urdf-name part) link-offsets)
-                (cl-transforms:transform*
-                 (cl-transforms:transform-inv
-                  (cl-transforms:reference-transform link-pose))                   
-                 (cl-transforms:reference-transform (sem-map-utils:pose part)))))))))
-
-(defmethod invalidate-object :around ((obj semantic-map-object))
-  (with-slots (joint-states) obj
-    (let ((old-joint-states (alexandria:copy-hash-table joint-states)))
-      (call-next-method)
-      (update-semantic-map-poses
-       obj
-       (loop for name being the hash-keys in joint-states
-             using (hash-value new-state)
-             for old-state being the hash-values in old-joint-states
-             unless (eql new-state old-state) collect name)))))
-
-(defmethod (setf joint-state) :before (new-value (sem-map semantic-map-object) name)
-  (attach-contacting-objects
-   sem-map :detach-invalid t
-   :test (lambda (obj link-name)
-           (declare (ignore link-name))
-           (typep obj 'household-object))))
-
-(defmethod (setf joint-state) :after (new-value (sem-map semantic-map-object) name)
-  (update-semantic-map-joint sem-map name))
-
-(defmethod (setf link-pose) :before (new-value (sem-map semantic-map-object) name)
-  (attach-contacting-objects
-   sem-map :detach-invalid t
-   :test (lambda (obj link-name)
-           (declare (ignore link-name))
-           (typep obj 'household-object))))
-
-(defmethod (setf link-pose) :around (new-value (sem-map semantic-map-object) name)
-  (with-slots (urdf links link-offsets) sem-map
-    (let* ((link (gethash name links))
-           (sem-map-obj (lazy-car
-                         (sem-map-utils:sub-parts-with-name
-                          sem-map (owl-name-from-urdf-name sem-map name)))))
-      (sem-map-utils:update-pose
-       sem-map-obj (cl-transforms:transform->pose
-                    (cl-transforms:transform*
-                     (cl-transforms:reference-transform (pose link))
-                     (gethash (sem-map-utils:urdf-name sem-map-obj)
-                              link-offsets)))
-       :relative nil :recursive t))))
-
 (defmethod copy-object ((obj semantic-map-object) (world bt-reasoning-world))
   (with-slots (semantic-map link-offsets) obj
     (change-class
      (call-next-method) 'semantic-map-object
      :semantic-map (sem-map-utils:copy-semantic-map-object semantic-map))))
 
-(defmethod add-object ((world bt-world) (type (eql 'semantic-map)) name pose &key urdf)
-  (make-instance 'semantic-map-object
-    :name name
-    :world world
-    :pose (ensure-pose pose)
-    :urdf (etypecase urdf
-            (cl-urdf:robot urdf)
-            (string (handler-bind ((cl-urdf:urdf-type-not-supported #'muffle-warning))
-                      (cl-urdf:parse-urdf urdf))))
-    :collision-group :static-filter
-    :collision-mask '(:default-filter :character-filter)))
-
-(defun update-semantic-map-joint (sem-map joint-name)
-  (with-slots (urdf links link-offsets semantic-map joint-states)
-      sem-map
-    ;; We shouldn't use parent here but child for drawers. The problem
-    ;; is that in the current semantic map, the drawer is connected to
-    ;; the door by the joint. We will keep this code until the
-    ;; semantic map is fixed.
-    (let* ((child-link-name (cl-urdf:name (cl-urdf:child (gethash joint-name (cl-urdf:joints urdf)))))
-           (parent-link-name (cl-urdf:name (cl-urdf:parent (gethash joint-name (cl-urdf:joints urdf)))))
-           (child-link (gethash child-link-name links))
-           (parent-sem-map-obj (lazy-car
-                                (sem-map-utils:sub-parts-with-name
-                                 semantic-map (owl-name-from-urdf-name sem-map parent-link-name))))
-           (child-sem-map-obj (lazy-car
-                               (sem-map-utils:sub-parts-with-name
-                                semantic-map (owl-name-from-urdf-name sem-map child-link-name))))
-           (sem-map-obj  (string-case (if parent-sem-map-obj
-                                          (sem-map-utils:obj-type parent-sem-map-obj)
-                                          "")
-                          ("Drawer" parent-sem-map-obj)
-                          (t child-sem-map-obj))))
-      (when sem-map-obj
-        (sem-map-utils:update-pose
-         sem-map-obj (cl-transforms:transform->pose
-                      (cl-transforms:transform*
-                       (cl-transforms:reference-transform (pose child-link))
-                       (or
-                        (gethash (sem-map-utils:urdf-name sem-map-obj)
-                                 link-offsets)
-                        (cl-transforms:make-identity-transform))))
-         :relative nil :recursive t)
-        (let ((joint (find-if (lambda (object)
-                                (typep object 'sem-map-utils:semantic-map-joint))
-                              (sem-map-utils:sub-parts sem-map-obj))))
-          (when joint
-            (setf (sem-map-utils:joint-position joint)
-                  (gethash joint-name joint-states))))))))
-
-(defun update-semantic-map-poses (sem-map &optional (joint-names nil joint-names-p))
-  ;; Instead of iterating over all links, just iterate over all
-  ;; non-fixed joints and use code similar to (setf joint-state) The
-  ;; problem with this code is that the mapping between urdf links and
-  ;; sem-map parts somehow seems to be broken
-  (with-slots (urdf) sem-map
-    (let ((joint-names (if joint-names-p
-                           joint-names
-                           (alexandria:hash-table-keys
-                            (cl-urdf:joints urdf)))))
-      (dolist (joint-name joint-names)
-        (let ((joint (gethash joint-name (cl-urdf:joints urdf))))
-          (unless (eq (cl-urdf:joint-type joint)
-                      :fixed)
-            (update-semantic-map-joint sem-map joint-name)))))))
-
+ (defmethod add-object ((world bt-world) (type (eql 'semantic-map)) name pose &key urdf)
+   (if urdf
+       (make-instance 'urdf-semantic-map-object
+         :name name
+         :world world
+         :pose (ensure-pose pose)
+         :urdf (etypecase urdf
+                 (cl-urdf:robot urdf)
+                 (string (handler-bind ((cl-urdf:urdf-type-not-supported #'muffle-warning))
+                           (cl-urdf:parse-urdf urdf))))
+         :collision-group :static-filter
+         :collision-mask '(:default-filter :character-filter))
+       (make-instance 'simple-semantic-map-object
+         :name name
+         :world world
+         :pose (ensure-pose pose)
+         :collision-group :static-filter
+         :collision-mask '(:default-filter :character-filter))))
