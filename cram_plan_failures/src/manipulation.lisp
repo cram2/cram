6d0b090a3c3c921d814fe165690b84fcb2d3b9ef
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-11-20 15:06:09 +0100

    Added initform for slot RESULT in manipulation-failure.

--- cram_plan_failures/src/manipulation.lisp
+++ cram_plan_failures/src/manipulation.lisp
@@ -30,7 +30,7 @@
 (in-package :cram-plan-failures)
 
 (define-condition manipulation-failure (simple-plan-failure)
-  ((result :initarg :result :reader result))
+  ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "manipulation-failure"))
 
 (define-condition object-lost (manipulation-failure) ())
