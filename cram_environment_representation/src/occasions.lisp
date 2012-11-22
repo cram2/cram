e426024b3347a1b6f6006851aee489a485277cf9
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-11-22 10:38:35 +0100

    Added clear-belief implementation and setup function for bullet world.

--- cram_environment_representation/src/occasions.lisp
+++ cram_environment_representation/src/occasions.lisp
@@ -103,7 +103,40 @@ is returned."
                   (desig:get-all-designators))
    :test #'desig:desig-equal))
 
+(defmethod cram-plan-knowledge:clear-belief cram-environment-representation ()
+  (setf *current-bullet-world* (make-instance 'bt-reasoning-world))
+  (setup-world-database)
+  (set-robot-state-from-tf
+   cram-roslisp-common:*tf*
+   (var-value '?robot-instance (lazy-car (prolog `(and (robot ?robot)
+                                                       (%object ?_ ?robot ?robot-instance)))))))
+
 (defmethod cram-plan-knowledge:holds (occasion &optional time-specification)
   (if time-specification
       (prolog `(holds ?_  ,occasion ,time-specification))
       (prolog `(holds ,occasion))))
+
+(defvar *robot-urdf* nil)
+(defvar *kitchen-urdf* nil)
+
+(defun setup-world-database ()
+  (let ((robot (or *robot-urdf*
+                   (setf *robot-urdf*
+                         (cl-urdf:parse-urdf
+                          (roslisp:get-param "robot_description_lowres")))))
+        (kitchen (or *kitchen-urdf*
+                     (let ((kitchen-urdf-string
+                             (roslisp:get-param "kitchen_description" nil)))
+                       (when kitchen-urdf-string
+                         (setf *kitchen-urdf* (cl-urdf:parse-urdf
+                                               kitchen-urdf-string)))))))
+    (assert
+     (force-ll (prolog `(and
+                         (bullet-world ?w)
+                         (assert ?w (object static-plane floor ((0 0 0) (0 0 0 1))
+                                            :normal (0 0 1) :constant 0))
+                         (assert ?w (object semantic-map sem-map ((0 0 0) (0 0 0 1))
+                                            ,@(when kitchen
+                                                `(:urdf ,kitchen))))
+                         (robot ?robot)
+                         (assert ?w (object urdf ?robot ((0 0 0) (0 0 0 1)) :urdf ,robot))))))))
