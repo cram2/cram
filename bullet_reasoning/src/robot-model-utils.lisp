e470a4fbbc911576c0efd97fdf1f47ae49a4700f
Lorenz Moesenlechner <moesenle@in.tum.de>
2012-11-28 12:24:39 +0100

    Added timestamp parameter when initializing robot model from tf.

--- bullet_reasoning/src/robot-model-utils.lisp
+++ bullet_reasoning/src/robot-model-utils.lisp
@@ -37,19 +37,22 @@
   "An alist that maps from ROS namespaces to persistent service
   handles.")
 
-(defun set-robot-state-from-tf (tf robot &optional (reference-frame "/map"))
+(defun set-robot-state-from-tf (tf robot &key (reference-frame "/map") timestamp)
   (loop for name being the hash-keys in  (slot-value robot 'links) do
     (let ((tf-name (if (eql (elt name 0) #\/) name (concatenate 'string "/" name))))
       ;; We wait transforms instead of calling can-transform because
       ;; in particular at initialization time, the TF tree might not
       ;; be fully received yet.
       (when (tf:wait-for-transform
-             tf :timeout 0.5 :source-frame tf-name
-                :target-frame reference-frame)
+             tf :timeout 2.0 :source-frame tf-name
+                :target-frame reference-frame
+                :time timestamp)
         (handler-case
             (setf (link-pose robot name)
                   (cl-transforms:transform->pose
-                   (tf:lookup-transform tf :source-frame tf-name :target-frame reference-frame)))
+                   (tf:lookup-transform
+                    tf :source-frame tf-name :target-frame reference-frame
+                       :time timestamp)))
           (tf:tf-lookup-error ()
             nil))))))
 
