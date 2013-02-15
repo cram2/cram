569b66381af0cf46e5a8c55e56fdbf3660838bbb
Jan Winkler <winkler@cs.uni-bremen.de>
2013-02-15 16:42:51 +0100

    Added support for waiting for finished controller states

--- pr2_manipulation_process_module/src/feature-controllers.lisp
+++ pr2_manipulation_process_module/src/feature-controllers.lisp
@@ -31,6 +31,11 @@
 (defvar *left-feature-constraints-config-pub* nil)
 (defvar *left-feature-constraints-command-pub* nil)
 
+(defvar *controller-state-subscriber* nil)
+(defvar *controller-state-active-fluent*
+  (cram-language:make-fluent :name :feature-controller-state-fluent
+                             :allow-tracing nil))
+
 (defun init-feature-constraints-controller ()
   (setf *left-feature-constraints-command-pub*
         (roslisp:advertise
@@ -39,7 +44,31 @@
   (setf *left-feature-constraints-config-pub*
         (roslisp:advertise
          "/left_arm_feature_controller/constraint_config"
-         "constraint_msgs/ConstraintConfig")))
+         "constraint_msgs/ConstraintConfig"))
+  (setf *controller-state-subscriber*
+        (roslisp:subscribe
+         "/left_arm_feature_controller/constraint_state"
+         "constraint_msgs/ConstraintState"
+         #'controller-state-callback)))
+
+(register-ros-init-function init-feature-constraints-controller)
+
+(defun controller-state-callback (msg)
+  (roslisp:with-fields (weights) msg
+    (let ((max-weight (loop for i from 0 below (length weights)
+                            for weight = (elt weights i)
+                            maximizing weight into max-weight
+                            finally (return max-weight))))
+      (cond ((< max-weight 1.0)
+             ;; All weights are < 1.0, meaning that all constraints are
+             ;; satisfied.
+             (setf (cram-language:value *controller-state-active-fluent*) T)
+             (cram-language:pulse *controller-state-active-fluent*))
+            (t (setf (cram-language:value
+                      *controller-state-active-fluent*) nil))))))
+
+(defun wait-for-controller (&optional (timeout nil))
+  (cram-language:wait-for *controller-state-active-fluent* :timeout timeout))
 
 (defun send-constraints-config (constraints)
   ;; TODO(Georg): differentiate arms
