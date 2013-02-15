7561e8f31541d3823a978590945dd29143f63f4a
Jan Winkler <winkler@cs.uni-bremen.de>
2013-02-15 19:57:45 +0100

    Moved action handler related code into a separate file
    Since the action handlers are getting quite numerous and make the process-module.lisp file seem a bit cluttered, they are moved to a designated file.

--- pr2_manipulation_process_module/src/process-module.lisp
+++ pr2_manipulation_process_module/src/process-module.lisp
@@ -89,11 +89,6 @@
 
 (defgeneric call-action (action &rest params))
 
-(defmacro def-action-handler (name args &body body)
-  (alexandria:with-gensyms (action-sym params)
-    `(defmethod call-action ((,action-sym (eql ',name)) &rest ,params)
-       (destructuring-bind ,args ,params ,@body))))
-
 (defmethod call-action ((action-sym t) &rest params)
   (roslisp:ros-info (pr2-manip process-module)
                     "Unimplemented operation `~a' with parameters ~a. Doing nothing."
@@ -105,91 +100,6 @@
                     action-sym params)
   (prog1 (call-next-method)
     (roslisp:ros-info (pr2-manip process-module) "Manipulation action done.")))
-
-(def-action-handler container-opened (handle side)
-  (let* ((handle-pose (designator-pose (newest-effective-designator handle)))
-         (new-object-pose (open-drawer handle-pose side)))
-    (update-object-designator-pose handle new-object-pose)))
-
-(def-action-handler container-closed (handle side)
-  (let* ((handle-pose (designator-pose (newest-effective-designator handle)))
-         (new-object-pose (close-drawer handle-pose side)))
-    (update-object-designator-pose handle new-object-pose)))
-
-(def-action-handler park (obj arms &optional obstacles)
-  (roslisp:ros-info (pr2-manip process-module) "Park arms ~a ~a"
-                    obj arms)
-  (force-ll arms)
-  (cond ((not arms)
-         (when obstacles
-           (clear-collision-objects)
-           (sem-map-coll-env:publish-semantic-map-collision-objects)
-           (dolist (obstacle (cut:force-ll obstacles))
-             (register-collision-object obstacle)))
-         (cpl:par-loop (arm arms)
-           (let ((orientation (calculate-carry-orientation
-                               obj arm
-                               (list *top-grasp*
-                                     (cl-transforms:make-identity-rotation))))
-                 (carry-pose (ecase arm
-                               (:right *carry-pose-right*)
-                               (:left *carry-pose-left*))))
-             (if orientation
-                 (execute-move-arm-pose
-                  arm
-                  (tf:copy-pose-stamped carry-pose :orientation orientation)
-                  :allowed-collision-objects (list "\"all\""))
-                 (execute-move-arm-pose
-                  arm carry-pose
-                  :allowed-collision-objects (list "\"all\""))))))
-        ((eql (length arms) 1)
-         (park-grasped-object-with-one-arm obj (first arms) obstacles))
-        ((eql (length arms) 2)
-         (park-grasped-object-with-two-arms obj arms obstacles))
-        ((> (length arms) 1)
-         (error 'simple-error :format-control "Parking with several arms not implemented, yet."))
-        (t (error 'simple-error :format-control "No arms for parking inferred."))))
-
-(def-action-handler lift (arms distance)
-  ;; Note(Georg) Curious! We do not need the object designator
-  ;; for execution of this action?
-  ;; NOTE(winkler): No, we actually don't. The lifting is done by just
-  ;; recalculating the new position based on the old one and the fact
-  ;; that the gripper should stick to it's current orientation.
-  (force-ll arms)
-  (cond ((eql (length arms) 1)
-         (lift-grasped-object-with-one-arm (first arms) distance))
-        ;; TODO(Georg): the next cases is actually deprecated because
-        ;; it still relies on the :both arms setup
-        
-        ;; NOTE(winkler): Apparently it is replaced by a `(par'
-        ;; solution already. Nevertheless, it should be extended to be
-        ;; more general and support `n' arms when lifting (although we
-        ;; only have :left and :right on the PR2). This current
-        ;; solution works great, though.
-        ((> (length arms) 1)
-         (lift-grasped-object-with-both-arms distance))
-        (t (error 'simple-error :format-control "No arms for lifting inferred."))))
-
-(def-action-handler grasp-handles (obj arms-handles obstacles)
-  (assert (> (length arms-handles) 0) ()
-          "No arms/handles combinations have been specified in
-          `grasp-handles'.")
-  (roslisp:ros-info (pr2-manip-pm grasp-handles)
-                    "Calling grasp-handled-object-constraint-aware
-                      with ~a arms." (length arms-handles))
-  (grab-handled-object-constraint-aware obj arms-handles obstacles
-                                        :obj-as-obstacle t))
-
-(def-action-handler put-down (object-designator location arms obstacles)
-  "Delegates the type of the put down action which suppose to be executed
-for the currently type of grasped object."
-  ;; TODO(moesenle): don't check for type pot here but either use the
-  ;; predicate REQUIRED-ARMS or OBJECT-IN-HAND
-  (cond ((eq (desig-prop-value object-designator 'type) 'pot)
-         (put-down-grasped-object-with-both-arms object-designator location))
-        (t (put-down-grasped-object-with-single-arm
-            object-designator location (first arms) obstacles))))
 
 (defun execute-goal (server goal)
   (multiple-value-bind (result status)
