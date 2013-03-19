73265be15e28fb9cfb044a8c9bb56c590ab275d9
Jan Winkler <winkler@cs.uni-bremen.de>
2013-03-19 15:07:22 +0100

    Added publishing of location costmaps once they are generated/merged

--- location_costmap/src/location-costmap.lisp
+++ location_costmap/src/location-costmap.lisp
@@ -168,7 +168,10 @@ calls the generator functions and runs normalization."
 (defun merge-costmaps (cm-1 &rest costmaps)
   "merges cost functions and copies one height-map, returns one costmap"
   (etypecase cm-1
-    (list (apply #'merge-costmaps (append (force-ll cm-1) costmaps)))
+    (list (let ((merged (apply #'merge-costmaps
+                               (append (force-ll cm-1) costmaps))))
+            (publish-location-costmap merged)
+            merged))
     (location-costmap
        ;; Todo: assert equal size of all costmaps
      (make-instance 'location-costmap
