96d8fece09f9b6f0990da336ebb7ee642ae6cad5
Jan Winkler <winkler@cs.uni-bremen.de>
2013-03-19 08:45:13 +0100

    Added the `name' symbol

--- cram_designators/src/package.lisp
+++ cram_designators/src/package.lisp
@@ -97,6 +97,7 @@
            #:desig-location-prop
            #:equated-desigs
            #:desig
+           #:name
            #:trajectory-desig?)
   (:desig-properties #:obj #:location #:object #:pose #:of #:at
-                     #:type #:trajectory #:action))
+                     #:type #:trajectory #:action #:name))
