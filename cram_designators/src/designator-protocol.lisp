9d68d049d9653af176fef7bc930af879b00b0a0a
Jan Winkler <winkler@cs.uni-bremen.de>
2013-03-27 22:10:56 +0100

    Corrected typo

--- cram_designators/src/designator-protocol.lisp
+++ cram_designators/src/designator-protocol.lisp
@@ -190,7 +190,7 @@ class (derived from class DESIGNATOR), e.g. OBJECT-DESIGNATOR."
       (first-desig (parent desig))))
 
 (defmethod current-desig ((desig designator))
-  "gets the last know successor in chain, being the most current"
+  "gets the last known successor in chain, being the most current"
   (if (null (successor desig))
       desig
       (current-desig (successor desig))))
