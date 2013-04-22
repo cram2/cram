4698eae2788056a45ec93e30d50cad97a55aff20
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-04-22 15:50:31 +0200

    Corrected in-package function call, and corrected BSD-headers.

--- pr2_itasc_process_module/src/itasc-integration.lisp
+++ pr2_itasc_process_module/src/itasc-integration.lisp
@@ -1,17 +1,18 @@
-;;; Copyright (c) 2012, Georg Bartels <georg.bartels@cs.uni-bremen.de>
+;;; Copyright (c) 2013, Georg Bartels <georg.bartels@cs.uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions are met:
 ;;;
-;;;     * Redistributions of source code must retain the above copyright
-;;;       notice, this list of conditions and the following disclaimer.
-;;;     * Redistributions in binary form must reproduce the above copyright
-;;;       notice, this list of conditions and the following disclaimer in the
-;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Willow Garage, Inc. nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
+;;; * Redistributions of source code must retain the above copyright
+;;; notice, this list of conditions and the following disclaimer.
+;;; * Redistributions in binary form must reproduce the above copyright
+;;; notice, this list of conditions and the following disclaimer in the
+;;; documentation and/or other materials provided with the distribution.
+;;; * Neither the name of the Institute for Artificial Intelligence/
+;;; Universitaet Bremen nor the names of its contributors may be used to 
+;;; endorse or promote products derived from this software without specific 
+;;; prior written permission.
 ;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -25,7 +26,7 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :pr2-manip-pm)
+(in-package :pr2-itasc-pm)
 
 (defvar *itasc-action-parser* nil)
 
