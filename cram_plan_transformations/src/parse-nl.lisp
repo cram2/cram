9cb2572946219e282210caf3904335a5fc049eab
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-01-15 16:22:55 +0100

    plan transforms: better plists instead of alists

--- cram_plan_transformations/src/parse-nl.lisp
+++ cram_plan_transformations/src/parse-nl.lisp
@@ -3,8 +3,8 @@
 (in-package :cram-plan-transformations)
 
 ;;; STEP 0.5
-(defun transform-into-alist (a-tree &optional an-alist)
-  "Transforms a tree into an association list. `a-tree' is something like
+(defun transform-into-plist (a-tree &optional (a-plist nil))
+  "Transforms a tree into a property list. `a-tree' is something like
    `((action-role flip-1      action-verb)
      (has-sense   flip-1      flip-1-8)
      (is-a        flip-1-8    flip.v.08)
@@ -12,23 +12,23 @@
      (has-sense   pancake-3   pancake-3-1)
      (is-a        pancake-3-1 pancake.n.01))
    for which the result would be
-   `((THEME . PANCAKE.N.01) (ACTION-VERB . FLIP.V.08))"
+   `((THEME PANCAKE.N.01) (ACTION-VERB FLIP.V.08))"
   (let ((tripple (car a-tree)))
    (if (null a-tree)
-       an-alist
+       a-plist
        (cond ((member 'action-role tripple)
-              (transform-into-alist
+              (transform-into-plist
                (cdr a-tree)
-               (cons (cons (third tripple) (second tripple)) an-alist)))
+               (cons (list (third tripple) (second tripple)) a-plist)))
              ((or (member 'has-sense tripple)
                   (member 'is-a tripple))
-              (transform-into-alist
+              (transform-into-plist
                (cdr a-tree)
                (mapcar #'(lambda (x) ; maybe it could stop at the first occurrence
-                         (if (eql (cdr x) (second tripple))
-                             (cons (car x) (third tripple))
-                             x)) an-alist)))))))
+                         (if (eql (car (cdr x)) (second tripple))
+                             (list (car x) (third tripple))
+                             x)) a-plist)))))))
 
 ;;; STEP 1
-(defun transform-into-action-definition (an-alist)
+(defun transform-into-action-definition (a-plist)
   )
