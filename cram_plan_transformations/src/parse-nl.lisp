54e274a607fa73560e369932c253aa5788250e25
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-01-16 14:40:38 +0100

    plan transforms: step 1: generating goal descriptions

--- cram_plan_transformations/src/parse-nl.lisp
+++ cram_plan_transformations/src/parse-nl.lisp
@@ -30,5 +30,19 @@
                              x)) a-plist)))))))
 
 ;;; STEP 1
-(defun transform-into-action-definition (a-plist)
-  )
+(defun get-owl-name (wordnet-name)
+  (gensym "owl-bla"))
+(defun transform-into-goal (a-plist)
+  (flet ((extract-the-actual-name (name-with-id-stuff)
+           "e.g. flip.v.08 -> flip"
+           (let* ((a-string (symbol-name name-with-id-stuff))
+                  (index (position #\. a-string :from-end t :end (position #\. a-string :from-end t)))
+                  (a-substring (subseq a-string 0 index)))
+             (intern a-substring))))
+    (let ((wordnet-action-role-to-class '(action-verb action theme object instrument object)))
+             `(a goal ,(mapcar #'(lambda (x)
+                                   `(,(car x)
+                                     (a ,(getf wordnet-action-role-to-class (car x))
+                                       (name ,(get-owl-name (car (cdr x))))
+                                       (type ,(extract-the-actual-name (car (cdr x)))))))
+                               a-plist)))))
