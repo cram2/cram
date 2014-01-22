9b69be5e308840047416fca2d29e47fa88565103
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-01-22 19:45:31 +0100

    plan transforms: no lazy lists in descriptions now
    + added a little testing function.

--- cram_plan_transformations/src/parse-nl.lisp
+++ cram_plan_transformations/src/parse-nl.lisp
@@ -29,6 +29,7 @@
                              x)) a-plist)))))))
 
 (defun get-owl-name (wordnet-name)
+  (declare (ignore wordnet-name))
   (gensym "owl-bla"))
 
 (defun extract-the-actual-name (name-with-id-stuff)
@@ -44,40 +45,35 @@
     (findall ?description
              (and (member ?key-value-pair ?plist)
                   (description-for-key-value-pair ?key-value-pair ?description))
-             ?descriptions))
+             ?descriptions-lazy)
+    (lisp-fun cut:force-ll ?descriptions-lazy ?descriptions))
 
   (<- (description-for-key-value-pair (action-verb ?wordnet-name) ?description)
     (lisp-fun extract-the-actual-name ?wordnet-name ?name)
     (lisp-fun get-owl-name ?wordnet-name ?owl-name)
-    (equal ?description (instruction (a action (name ?owl-name) (type ?name)))))
+    (equal ?description (instruction (an action (name ?owl-name) (type ?name)))))
 
   (<- (description-for-key-value-pair (theme ?wordnet-name) ?description)
     (lisp-fun extract-the-actual-name ?wordnet-name ?name)
     (lisp-fun get-owl-name ?wordnet-name ?owl-name)
-    (equal ?description (theme (a object (name ?owl-name) (type ?name)))))
+    (equal ?description (theme (an object (name ?owl-name) (type ?name)))))
 
   (<- (description-for-key-value-pair (instrument ?wordnet-name) ?description)
     (lisp-fun extract-the-actual-name ?wordnet-name ?name)
     (lisp-fun get-owl-name ?wordnet-name ?owl-name)
-    (equal ?description (instrument (a object (name ?owl-name) (type ?name)))))
+    (equal ?description (instrument (an object (name ?owl-name) (type ?name)))))
 
   (<- (transform-descriptions-into-action-description ?descriptions ?action-description)
     (member ?description ?descriptions)
-    (format "members:~a~%" ?description)
-    (equal ?description (instruction ?stuff))
-    (findall ?action-property
-             (and (member ?action-property ?stuff)
-                  (not (equal ?action-property a))
-                  (not (equal ?action-property action)))
-             ?action-properties)
-    (format "action-properties: ~a~%" ?action-properties)
+    (equal ?description (instruction (an action . ?action-properties)))
     (findall ?remaining-description
              (and (member ?remaining-description ?descriptions)
                   (not (equal ?remaining-description ?description)))
-             ?remaining-descriptions)
-    (equal ?action-description (instruction (a action
-                                              ?action-properties
-                                              ?remaining-descriptions))))
+             ?remaining-descriptions-lazy)
+    (lisp-fun cut:force-ll ?remaining-descriptions-lazy ?remaining-descriptions)
+    (append (an action) ?action-properties ?bla)
+    (append ?bla ?remaining-descriptions ?all-descriptions)
+    (equal ?action-description (instruction ?all-descriptions)))
 
   (<- (transform-plist-into-action-description ?plist ?action-description)
     (transform-plist-into-descriptions ?plist ?descriptions)
@@ -87,9 +83,38 @@
     ;; first transform the `?items-plist' into descriptions
     (transform-plist-into-descriptions ?items-plist ?items-description)
     ;; then append the descriptions to the action description
-    (equal ?description (instruction (a action ?key-value-pairs)))
-    (append ?key-value-pairs ?items-description ?augmented-key-value-pairs)
-    (equal ?new-description (instruction (a action ?augmented-key-value-pairs)))))
+    (equal ?description (instruction (an action . ?key-value-pairs)))
+    (append (an action) ?key-value-pairs ?bla)
+    (append ?bla ?items-description ?all-descriptions)
+    (equal ?new-description (instruction ?all-descriptions))))
+
+(defun test-stuff ()
+  (let* ((a-tree `((action-role flip-1      action-verb)
+                   (has-sense   flip-1      flip-1-8)
+                   (is-a        flip-1-8    flip.v.08)
+                   (action-role pancake-3   theme)
+                   (has-sense   pancake-3   pancake-3-1)
+                   (is-a        pancake-3-1 pancake.n.01)))
+         (a-plist (transform-into-plist a-tree))
+         (action-desc-prolog (prolog `(transform-plist-into-action-description
+                                       ,a-plist
+                                       ?action-description)))
+         (action-desc (cut:force-ll (cut:var-value '?action-description
+                                                   (car (cut:force-ll action-desc-prolog)))))
+         (additional-tree `((action-role skolem-instrument    instrument)
+                            (has-sense   skolem-instrument    spatula.n.01_senseID)
+                            (is-a        spatula.n.01_senseID spatula.n.01)))
+         (additional-plist (transform-into-plist additional-tree))
+         (new-desc-prolog (prolog `(add-items-to-action-description
+                                    ,action-desc
+                                    ,additional-plist
+                                    ?new-description))))
+    (format t "a-tree: ~a~%~%" a-tree)
+    (format t "a-plist: ~a~%~%" a-plist)
+    (format t "action-desc: ~a~%~%" action-desc)
+    (format t "additionall-tree: ~a~%~%" additional-tree)
+    (format t "additional-plist: ~a~%~%" additional-plist)
+    (cut:force-ll (cut:var-value '?new-description (car (cut:force-ll new-desc-prolog))))))
 
 ;;; STEP 4: compare goal description to general plan and do something ...:D
 ;;; either use create-designator or compare the two and transform the plan as a list
@@ -134,3 +159,5 @@
 ;;     ...                     |   ?needed-roles        ; <- gaya
 ;;   (achieve ...))            |   ?slot-value-pairs    ;
 ;;                             |   ?missing-roles)      ;
+
+;; (type ?old) -> (type ?new)  | (more-appropriate-in-context ?old ?new)
