2012f67aab79acca6eec53b6b4686a5cffbc7ea7
Jan Winkler <winkler@cs.uni-bremen.de>
2014-08-25 16:17:47 +0200

    Added more detailed logging hooks for perception

--- cram_plan_library/src/perceive-object.lisp
+++ cram_plan_library/src/perceive-object.lisp
@@ -29,38 +29,44 @@
 
 (in-package :plan-lib)
 
+(cut:define-hook on-begin-find-objects ())
+(cut:define-hook on-finish-find-objects (id))
+
 (define-condition ambiguous-perception (simple-plan-failure) ())
 
 (def-goal (perceive-object all ?obj-desig)
-  (with-designators ((obj-loc-desig (location `((of ,?obj-desig))))
-                     (loc (location `((to see) (obj ,?obj-desig))))
-                     (perceive-action (action `((to perceive)
-                                                (obj ,?obj-desig)))))
-    (with-retry-counters ((movement-retries 3))
-      (with-failure-handling
-          (((or location-not-reached-failure object-not-found) (e)
-             (declare (ignore e))
-             (ros-warn (perceive plan-lib) "Object not found failure.")
-             (do-retry movement-retries
-               (ros-info
-                (perceive plan-lib) "Retrying at different base location.")
-               (retry-with-updated-location
-                loc (next-different-location-solution loc)))))
-        (at-location (loc)
-          (with-retry-counters ((perception-retries 3))
-            (with-failure-handling
-                ((object-not-found (e)
-                   (declare (ignore e))
-                   (ros-warn (perceive plan-lib) "Object not found failure.")
-                   (do-retry perception-retries
-                     (ros-info
-                      (perceive plan-lib) "Retrying at different look location.")
-                     (retry))))
+  (let ((log-id (first (on-begin-find-objects))))
+    (unwind-protect
+         (with-designators ((obj-loc-desig (location `((of ,?obj-desig))))
+                            (loc (location `((to see) (obj ,?obj-desig))))
+                            (perceive-action (action `((to perceive)
+                                                       (obj ,?obj-desig)))))
+           (with-retry-counters ((movement-retries 3))
+             (with-failure-handling
+                 (((or location-not-reached-failure object-not-found) (e)
+                    (declare (ignore e))
+                    (ros-warn (perceive plan-lib) "Object not found failure.")
+                    (do-retry movement-retries
+                      (ros-info
+                       (perceive plan-lib) "Retrying at different base location.")
+                      (retry-with-updated-location
+                       loc (next-different-location-solution loc)))))
+               (at-location (loc)
+                 (with-retry-counters ((perception-retries 3))
+                   (with-failure-handling
+                       ((object-not-found (e)
+                          (declare (ignore e))
+                          (ros-warn (perceive plan-lib) "Object not found failure.")
+                          (do-retry perception-retries
+                            (ros-info
+                             (perceive plan-lib) "Retrying at different look location.")
+                            (retry))))
                      ;; (retry-with-updated-location
                      ;;  obj-loc-desig (next-solution obj-loc-desig)))))
-              (achieve `(looking-at ,(reference obj-loc-desig)))
-              (perform perceive-action)
-              (monitor-action perceive-action))))))))
+                     (achieve `(looking-at ,(reference obj-loc-desig)))
+                     (perform perceive-action)
+                     (monitor-action perceive-action)))))))
+      (on-finish-find-objects log-id))))
 
 (def-goal (perceive-object a ?obj-desig)
   "Tries to find the object described by ?obj-desig and equates the
