6b17e92a19d4fd90f993ac498bf2598e7e348a05
Jannik Buckelo <jannikbu@tzi.de>
2015-02-10 12:14:51 +0100

    Fixed some errors with the cl-urdf:3d-model replacement

--- bullet_reasoning/src/robot-model.lisp
+++ bullet_reasoning/src/robot-model.lisp
@@ -53,10 +53,18 @@
     :color (apply-aplha-value color)))
 
 (defmethod urdf-make-collision-shape ((mesh cl-urdf:mesh) &optional (color '(0.8 0.8 0.8 1.0)))
-  (make-instance 'convex-hull-mesh-shape
-    :color (apply-aplha-value color)
-    :faces (physics-utils:3d-model-faces (load-mesh mesh))
-    :points (physics-utils:3d-model-vertices (load-mesh mesh))))
+  (let ((model (load-3d-model mesh)))
+    (make-instance 'convex-hull-mesh-shape
+                   :color (apply-aplha-value color)
+                   :faces (physics-utils:3d-model-faces model)
+                   :points (physics-utils:3d-model-vertices model))))
+
+(defmethod load-3d-model ((mesh cl-urdf:mesh))
+  "Loads and resizes the 3d-model"
+  (let ((model (physics-utils:load-3d-model (physics-utils:parse-uri (cl-urdf:filename mesh)))))
+      (cond ((cl-urdf:scale mesh) (physics-utils:scale-3d-model model (cl-urdf:scale mesh)))
+            ((cl-urdf:size mesh) (physics-utils:resize-3d-model model (cl-urdf:size mesh)))
+            (t model))))
 
 (defstruct collision-information
   rigid-body-name flags)
@@ -552,9 +560,3 @@ current joint states"
 (defun apply-aplha-value (color)
   (destructuring-bind (r g b a) color
     (list r g b (or *robot-model-alpha* a))))
-
-(defun load-mesh (mesh)
-  (let ((model (physics-utils:load-3d-model (physics-utils:parse-uri (filename mesh)))))
-      (cond ((scale mesh) (physics-utils:scale-3d-model model (scale mesh)))
-            ((size mesh) (physics-utils:resize-3d-model model (size mesh)))
-            (t model))))
