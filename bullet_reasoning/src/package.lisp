822c0ae1dbf31382a6df1e531f3bc3603a4a11f2
andz <aromero@informatik.uni-bremen.de>
2015-04-02 20:33:54 +0200

    [btr] Changed tolerances for object-pose-different and added predicate 'stable-household'.

--- bullet_reasoning/src/package.lisp
+++ bullet_reasoning/src/package.lisp
@@ -64,7 +64,7 @@
            stable-p above-p find-objects-above below-p
            find-objects-below bullet-world object
            retract step simulate-realtime object-pose object-bottom-pose
-           position orientation poses-equal contact stable
+           position orientation poses-equal contact stable stable-household
            object-not-in-collision ik-solution-not-in-collision
            link-contacts supported-by above below visible
            occluding-objects occluding-object valid-grasp grasp side reachable
