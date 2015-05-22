32c301c6bb87c32bacea56c8f76aeada35c2b6f9
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-05-22 19:56:11 +0200

    [spa_demo] adjust robot torso joint state for moveit

--- spatial_relations_demo/src/utilities/setup.lisp
+++ spatial_relations_demo/src/utilities/setup.lisp
@@ -96,4 +96,7 @@
        (assert (object ?w btr::semantic-map my-kitchen ((-3.45 -4.35 0) ,pi-rotation)
                        :urdf ,*kitchen-urdf*))
        (assert (object ?w urdf ?robot ((0 0 0) (0 0 0 1)) :urdf
-                       ,*robot-urdf-lowres*))))))
+                       ,*robot-urdf-lowres*))
+       (robot-arms-parking-joint-states ?joint-states)
+       (assert (joint-state ?w ?robot ?joint-states))
+       (assert (joint-state ?w ?robot (("torso_lift_joint" 0.16825d0))))))))
