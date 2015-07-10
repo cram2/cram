c348a99a868bdc1f92735d64d608a89bd291acc0
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-07-10 16:53:32 +0200

    SEMANTIC-MAP-NAME predicate doesn't exist anymore

--- bullet_reasoning_utilities/src/setup.lisp
+++ bullet_reasoning_utilities/src/setup.lisp
@@ -48,9 +48,7 @@
   (<- (location-costmap:costmap-in-reach-distance 1.0))
   (<- (location-costmap:costmap-reach-minimal-distance 0.1)))
 
-(def-fact-group semantic-map-data (semantic-map-utils:semantic-map-name)
-  (<- (semantic-map-utils:semantic-map-name
-       "http://knowrob.org/kb/ias_semantic_map.owl#SemanticEnvironmentMap_PM580j"))
+(def-fact-group semantic-map-data ()
   (<- (semantic-map-object-name :kitchen)))
 
 (disable-location-validation-function 'btr-desig::validate-designator-solution)
