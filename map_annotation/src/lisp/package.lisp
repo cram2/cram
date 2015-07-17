df94c46a1d2b9c973ee91c4245732277c86cbd79
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-07-17 22:03:26 +0200

    MAP_ANNOTATION was moved away into CRAM_EXPERIMENTAL:
    not like anyone know what for and how it was used nowadays....

--- map_annotation/src/lisp/package.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :cl-user)
-
-(defpackage map-annotation
-    (:use :cl :roslisp :prolog)
-  (:export #:get-closest-annotation
-           #:get-annotated-point
-           #:annotated-point
-           #:get-annotation-names))
