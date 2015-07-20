687a52f9c47c417da7aeb9ba4a08d0e0b8069fe0
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-07-20 18:24:41 +0200

    [cram_physics_utils] minor nicification

--- cram_physics_utils/src/ros-shape-utils.lisp
+++ cram_physics_utils/src/ros-shape-utils.lisp
@@ -79,22 +79,30 @@ type."
      :faces (let ((result (make-array (truncate (length triangles) 3))))
               (loop for i from 0 below (length triangles) by 3
                     for j from 0 do
-                      (let* ((point-1 (point-msg->3d-vector (aref vertices (aref triangles i))))
-                             (point-2 (point-msg->3d-vector (aref vertices (aref triangles (+ i 2)))))
-                             (point-3 (point-msg->3d-vector (aref vertices (aref triangles (+ i 1)))))
-                             (pose-vec (cl-transforms:v* (cl-transforms:v+ point-1 point-2 point-3) (/ 3)))
+                      (let* ((point-1 (point-msg->3d-vector
+                                       (aref vertices (aref triangles i))))
+                             (point-2 (point-msg->3d-vector
+                                       (aref vertices (aref triangles (+ i 2)))))
+                             (point-3 (point-msg->3d-vector
+                                       (aref vertices (aref triangles (+ i 1)))))
+                             (pose-vec (cl-transforms:v*
+                                        (cl-transforms:v+ point-1 point-2 point-3) (/ 3)))
                              (normal (cl-transforms:cross-product (cl-transforms:v-
                                                                    point-2 point-1)
                                                                   (cl-transforms:v-
                                                                    point-3 point-1)))
                              (normal-normalized (cl-transforms:v*
                                                  normal
-                                                 (if (> (cl-transforms:dot-product pose-vec normal) 0)
+                                                 (if (> (cl-transforms:dot-product
+                                                         pose-vec normal)
+                                                        0)
                                                      (/ (cl-transforms:v-norm normal))
                                                      (/ (- (cl-transforms:v-norm normal)))))))
                         (setf (aref result j)
                               (make-face
-                               :normals (list normal-normalized normal-normalized normal-normalized)
+                               :normals (list normal-normalized
+                                              normal-normalized
+                                              normal-normalized)
                                :points (list point-1 point-2 point-3))))
                     maximizing (aref triangles i) into max-tri-index
                     finally (return result))))))
