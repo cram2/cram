6f6a2c40c15994fe69a0469fb1a0a3564df9c988
Lisset Y Salinas Pinacho <lisset.salinas.pinacho@gmail.com>
2015-12-15 12:37:53 +0100

    Update action-designators.lisp

--- cram_beginner_tutorial/src/action-designators.lisp
+++ cram_beginner_tutorial/src/action-designators.lisp
@@ -5,34 +5,33 @@
   radius
   edges)
   
-  (cram-reasoning:def-fact-group shape-actions (action-desig)
+  (cram-prolog:def-fact-group shape-actions (action-desig)
   ;; for each kind of shape, call make-turtle-shape with the right number of edges
 
   ;; triangle
-  (cram-reasoning:<- (action-desig ?desig (shape ?action))
+  (cram-prolog:<- (action-desig ?desig (shape ?action))
     (desig-prop ?desig (type shape))
     (desig-prop ?desig (shape triangle))
     (desig-prop ?desig (radius ?radius))
-    (cram-reasoning:lisp-fun make-turtle-shape :radius ?radius :edges 3  ?action))
+    (cram-prolog:lisp-fun make-turtle-shape :radius ?radius :edges 3  ?action))
 
   ;; square
-  (cram-reasoning:<- (action-desig ?desig (shape ?action))
+  (cram-prolog:<- (action-desig ?desig (shape ?action))
     (desig-prop ?desig (type shape))
     (desig-prop ?desig (shape square))
     (desig-prop ?desig (radius ?radius))
-    (cram-reasoning:lisp-fun make-turtle-shape :radius ?radius :edges 4  ?action))
+    (cram-prolog:lisp-fun make-turtle-shape :radius ?radius :edges 4  ?action))
 
   ;; pentagon
-  (cram-reasoning:<- (action-desig ?desig (shape ?action))
+  (cram-prolog:<- (action-desig ?desig (shape ?action))
     (desig-prop ?desig (type shape))
     (desig-prop ?desig (shape pentagon))
     (desig-prop ?desig (radius ?radius))
-    (cram-reasoning:lisp-fun make-turtle-shape :radius ?radius :edges 5  ?action))
+    (cram-prolog:lisp-fun make-turtle-shape :radius ?radius :edges 5  ?action))
 
   ;; hexagon
-  (cram-reasoning:<- (action-desig ?desig (shape ?action))
+  (cram-prolog:<- (action-desig ?desig (shape ?action))
     (desig-prop ?desig (type shape))
     (desig-prop ?desig (shape hexagon))
     (desig-prop ?desig (radius ?radius))
-    (cram-reasoning:lisp-fun make-turtle-shape :radius ?radius :edges 6  ?action)))
-
+    (cram-prolog:lisp-fun make-turtle-shape :radius ?radius :edges 6  ?action)))
