2abc4ba23413147246ee7b00c9b27f41c766a9e5
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-18 11:46:51 +0200

    Putting down safety distance from 0.1m to 0.01m

--- pr2_manipulation_process_module/src/action-handlers.lisp
+++ pr2_manipulation_process_module/src/action-handlers.lisp
@@ -566,7 +566,7 @@
                                                           object-designator
                                                           :dimensions))
                                                    2)
-                                                0.1))
+                                                0.01))
                                            0.1)))
          (lazy-putdown-poses
            (prolog:prolog
