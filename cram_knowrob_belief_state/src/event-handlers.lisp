c524770256b2c32d80f0d256e469cc9fcad85a01
Thorin <thorin@uni-bremen.de>
2017-07-19 16:56:30 +0000

    [kr-belief] trying to integrate cram with knowrob: grasping first

--- cram_knowrob_belief_state/src/event-handlers.lisp
+++ cram_knowrob_belief_state/src/event-handlers.lisp
@@ -1,3 +1,4 @@
+;;;
 ;;; Copyright (c) 2017, Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
 ;;; All rights reserved.
 ;;;
@@ -9,10 +10,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of the Intelligent Autonomous Systems Group/
-;;;       Technische Universitaet Muenchen nor the names of its contributors 
-;;;       may be used to endorse or promote products derived from this software 
-;;;       without specific prior written permission.
+;;;     * Neither the name of the Institute for Artificial Intelligence/
+;;;       Universitaet Bremen nor the names of its contributors may be used to
+;;;       endorse or promote products derived from this software without
+;;;       specific prior written permission.
 ;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -26,24 +27,16 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(defsystem cram-knowrob-belief-state
-  :author "Gayane Kazhoyan"
-  :maintainer "Gayane Kazhoyan"
-  :license "BSD"
+(in-package :kr-belief)
 
-  :depends-on (cram-json-prolog
-               cram-utilities
-               cl-transforms-stamped
-               cl-transforms
-               cram-tf)
-  :components
-  ((:module "src"
-    :components
-    ((:file "package")
-     (:file "conversions" :depends-on ("package"))
-     (:file "assertions" :depends-on ("package" "conversions"))
-     (:file "extracting-info" :depends-on ("package" "conversions"))
-     (:file "event-handlers" :depends-on ("package" "conversions" "assertions" "extracting-info"))
-     (:file "grasping" :depends-on ("package"))
-     (:file "placing" :depends-on ("package" "grasping"))
-     (:file "looking" :depends-on ("package"))))))
+(defun set-object-in-hand (arm grasp object-designator)
+  (let* ((object-name (desig:desig-prop-value object-designator :name))
+         (gripper-id "left_gripper")
+         (kr-grasp-class (car (get-possible-object-grasps object-name gripper-id))))
+    (assert-object-grasped gripper-id object-name "boxy" kr-grasp-class)))
+
+(defmethod cram-occasions-events:on-event object-in-hand ((event cpoe:object-gripped))
+  (set-object-in-hand (cpoe:event-arm event) (cpoe:event-grasp event) (cpoe:event-object event)))
+
+(defmethod cram-occasions-events:on-event object-in-hand ((event cpoe:object-released))
+  (delete-object-in-hand (cpoe:event-arm event)))
