8ab908c97afc92027b0aab4740d19f21a734a470
Gayane Kazhoyan <gayanek@gmail.com>
2017-08-10 14:49:48 +0200

    [math] gsll had api breakage, this commit fixes that

--- cram_math/src/functions.lisp
+++ cram_math/src/functions.lisp
@@ -8,6 +8,14 @@
     (declare (ignore out-per))
     (gsll:lu-determinant lu sign)))
 
+(defun invert-matrix (matrix)
+  "Invert the matrix; return the inverse and determinant."
+  (multiple-value-bind (LU permutation signum)
+      (gsl:LU-decomposition (grid:copy matrix))
+    (values
+      (gsl:LU-invert LU permutation)
+      (gsl:LU-determinant LU signum))))
+
 (defun gauss (cov mean)
   "Returns a function that calculates the multivariate gauss for `cov'
    and `mean'
@@ -16,7 +24,7 @@
   (let* ((k (double-vector-size mean))
          (det^1/2 (sqrt (determinant (grid-from-double-matrix cov))))
          (c (/ (* (expt (* 2 pi) (/ k 2)) det^1/2)))
-         (cov-inv (double-matrix-from-grid (gsll:invert-matrix (grid-from-double-matrix cov))))
+         (cov-inv (double-matrix-from-grid (invert-matrix (grid-from-double-matrix cov))))
          (x-mean-trans (make-double-matrix (double-vector-size mean) 1))
          (cov-x-prod (make-double-vector (height cov)))
          (e-factor-mat (make-double-matrix 1 1))
