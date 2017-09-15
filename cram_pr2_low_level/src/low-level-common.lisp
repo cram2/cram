78509d5b4af2d043f80f51cade8712437b4a4291
Gayane Kazhoyan <gaya@cs.uni-bremen.de>
2017-09-15 10:32:13 +0200

    minor

--- cram_pr2_low_level/src/low-level-common.lisp
+++ cram_pr2_low_level/src/low-level-common.lisp
@@ -89,13 +89,13 @@
                                                         (y orientation pose) (cl-transforms:y rot)
                                                         (z orientation pose) (cl-transforms:z rot)
                                                         (w orientation pose) (cl-transforms:w rot)
-                                                        (x scale) 0.2
-                                                        (y scale) 0.1
-                                                        (z scale) 0.02
+                                                        (x scale) 0.05
+                                                        (y scale) 0.03
+                                                        (z scale) 0.01
                                                         (r color) (first r-g-b-list)
                                                         (g color) (second r-g-b-list)
                                                         (b color) (third r-g-b-list)
-                                                        (a color) 1.0)))
+                                                        (a color) 0.7)))
                ;; (roslisp:ros-warn (ll visualize-marker) "asked to visualize a null pose")
                )))
     (if (listp pose/s)
