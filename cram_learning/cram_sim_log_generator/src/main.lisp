1b8848f7da061de9d9ad6d85bd9d3281ea636ae0
Sebastian Koralewski <seba@tzi.de>
2018-03-22 11:02:53 +0100

    defined main method to execute auto benchmark

--- cram_learning/cram_sim_log_generator/src/main.lisp
+++ cram_learning/cram_sim_log_generator/src/main.lisp
@@ -1,1 +1,12 @@
 (in-package :cslg)
+
+(defun main (num-experiments)
+  (roslisp-utilities:startup-ros)
+  (setf ccl::*is-logging-enabled* t)
+  (ccl::connect-to-cloud-logger)
+  (loop for x from 1 to num-experiments
+        do (let ((experiment-id (format nil "~x" (random (expt 16 8)))))
+             (format t "Starting experiment ~a~%" experiment-id)
+             (pr2-proj:with-simulated-robot (demo::demo-random))
+             (ccl::export-log-to-owl (concatenate 'string experiment-id ".owl"))
+             (format t "Done with experiment ~a~%" experiment-id))))
