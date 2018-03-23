ade1fe103edf49f53603730ac96c4bea658d222a
Sebastian Koralewski <seba@tzi.de>
2018-03-23 15:32:23 +0100

    log generator creates folder for each experiment and stores logged prolog predicates

--- cram_integration/cram_learning/cram_sim_log_generator/src/main.lisp
+++ cram_integration/cram_learning/cram_sim_log_generator/src/main.lisp
@@ -8,7 +8,13 @@
   (setq roslisp::*debug-stream* nil)
   (loop for x from 1 to num-experiments
         do (let ((experiment-id (format nil "~x" (random (expt 16 8)))))
-             (format t "Starting experiment ~a~%" experiment-id)
-             (pr2-proj:with-simulated-robot (demo::demo-random))
-             (ccl::export-log-to-owl (concatenate 'string experiment-id ".owl"))
-             (format t "Done with experiment ~a~%" experiment-id))))
+             (let ((experiment-save-path
+                     (concatenate 'string
+                                  "~/projection-experiments/" experiment-id "/")))
+               (ensure-directories-exist experiment-save-path)
+               (setq ccl::*prolog-query-save-path* experiment-save-path)
+               (format t "Starting experiment ~a~%" experiment-id)
+               (pr2-proj:with-simulated-robot (demo::demo-random))
+               (ccl::export-log-to-owl (concatenate 'string experiment-id ".owl"))
+               (format t "Done with experiment ~a~%" experiment-id)
+               (ccl::merge-all-csv-files)))))
