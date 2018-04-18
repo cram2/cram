88b322fdace83b38f4bdd6c108cc88f4feff60bb
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2018-04-18 18:44:39 +0200

    Add another key to reference a location

--- cram_common/cram_designator_specification/src/specs.lisp
+++ cram_common/cram_designator_specification/src/specs.lisp
@@ -97,7 +97,7 @@
 
   (<- (property ?designator (?location-key ?location))
     (lisp-pred typep ?designator desig:action-designator)
-    (member ?location-key (:target :location))
+    (member ?location-key (:target :location :located-at))
     (property-member (?location-key ?location) ?designator)
     (assert-type ?location desig:location-designator "ACTION SPEC:PROPERTY"))
 
