7ac528e8e99ebcfbe16f439e60d6473a0bffb891
Gayane Kazhoyan <gayanek@gmail.com>
2018-06-08 10:36:35 +0200

    Revert "[rs] changed to comply with new RS interface"
    This reverts commit ab09622fad023d7e279317498be8bc60f92d84f3.

--- cram_robosherlock/src/json-parser.lisp
+++ cram_robosherlock/src/json-parser.lisp
@@ -41,141 +41,34 @@
             :format-arguments (list name)))
     (list name node)))
 
-
-(defun parse-hash-table (hash-table)
-  (loop for key being the hash-keys in hash-table using (hash-value value)
-        for key-name = (intern (string-upcase key) :keyword)
-        for parsed-key-value-list = (parse-json-node key-name value)
-        if parsed-key-value-list
-          collecting parsed-key-value-list))
+(defun parse-alist (alist)
+  (remove-if #'null
+             (mapcar (lambda (key-value-cons)
+                       (let ((key-name (intern (string-upcase (car key-value-cons))
+                                               :keyword)))
+                         (parse-json-node key-name (cdr key-value-cons))))
+                     alist)))
 
 (defun parse-nested-node (name node)
   (mapcar (lambda (key-value-pair)
             (destructuring-bind (key value)
                 key-value-pair
-              (list (intern (concatenate 'string (string-upcase name) "-" (string-upcase key))
-                            :keyword) value)))
-          (parse-hash-table node)))
+             (list (intern (concatenate 'string (string-upcase name) "-" (string-upcase key))
+                           :keyword) value)))
+          (parse-alist node)))
 
-(defun parse-number (node)
-  node)
-
-(defun parse-ignored-node ()
-  nil)
-
-(defun parse-case-sensitive-string (node)
-  (intern node :keyword))
-
-(defun parse-string (node)
-  (intern (string-upcase node) :keyword))
-
-(defun parse-pose (node)
-  (let* ((source (gethash "source" node))
-         (pose-stamped (gethash "pose_stamped" node))
-         (header (gethash "header" pose-stamped))
-         (frame-id (gethash "frame_id" header))
-         (stamp-hash-tbl (gethash "stamp" header))
-         (stamp-sec (gethash "sec" stamp-hash-tbl))
-         (stamp-nsec (gethash "nsec" stamp-hash-tbl))
-         (stamp (+ stamp-sec (/ stamp-nsec 1.0d9)))
-         (pose (gethash "pose" pose-stamped))
-         (orientation (gethash "orientation" pose))
-         (rot-x (gethash "x" orientation))
-         (rot-y (gethash "y" orientation))
-         (rot-z (gethash "z" orientation))
-         (rot-w (gethash "w" orientation))
-         (position (gethash "position" pose))
-         (pos-x (gethash "x" position))
-         (pos-y (gethash "y" position))
-         (pos-z (gethash "z" position))
-         (object-pose (cl-transforms-stamped:make-pose-stamped
-                       frame-id
-                       stamp
-                       (cl-transforms:make-3d-vector pos-x pos-y pos-z)
-                       (cl-transforms:make-quaternion rot-x rot-y rot-z rot-w))))
-    (list (parse-string source) object-pose)))
-
-(defun parse-poses (node)
-  (mapcar #'parse-pose node))
-
-;; (defun parse-transform (node)
-;;   (return-from parse-transform (list node))
-;;   (let* ((frame-id (gethash "frame_id" node))
-;;          (child-frame-id (gethash "child_frame_id" node))
-;;          (pos-x (gethash "pos_x" node))
-;;          (pos-y (gethash "pos_y" node))
-;;          (pos-z (gethash "pos_z" node))
-;;          (rot-x (gethash "rot_x" node))
-;;          (rot-y (gethash "rot_y" node))
-;;          (rot-z (gethash "rot_z" node))
-;;          (rot-w (gethash "rot_w" node))
-;;          (stamp (gethash "stamp" node))
-;;          (object-transform
-;;            (cl-transforms:make-transform
-;;             (cl-transforms:make-3d-vector pos-x pos-y pos-z)
-;;             (cl-transforms:make-quaternion rot-x rot-y rot-z rot-w))))
-;;     ;; string knowrob namespace from frame IDs
-;;     (let ((child-frame-position-of-# (position #\# child-frame-id :from-end t)))
-;;       (when child-frame-position-of-#
-;;         (setf child-frame-id (subseq child-frame-id (1+ child-frame-position-of-#)))))
-;;     ;; make sure transform is defined in robot-base-frame
-;;     (let ((object-transform-stamped
-;;             (if (equalp frame-id cram-tf:*robot-base-frame*)
-;;                 (cl-transforms-stamped:transform->transform-stamped
-;;                  frame-id
-;;                  child-frame-id
-;;                  stamp
-;;                  object-transform)
-;;                 (let ((transform-to-base-frame (cl-transforms-stamped:lookup-transform
-;;                                                 cram-tf:*transformer*
-;;                                                 cram-tf:*robot-base-frame*
-;;                                                 frame-id
-;;                                                 :timeout cram-tf:*tf-default-timeout*)))
-;;                   (cl-transforms-stamped:transform->transform-stamped
-;;                    cram-tf:*robot-base-frame*
-;;                    child-frame-id
-;;                    (cl-transforms-stamped:stamp transform-to-base-frame)
-;;                    (cl-transforms:transform* transform-to-base-frame object-transform))))))
-;;       object-transform-stamped)))
-
-(defun parse-color (node)
-  (let* ((colors-sorted
-           (sort (parse-hash-table node)
-                 #'> :key #'second))
-         (num-colors
-           (length colors-sorted))
-         (top-3-colors
-           (subseq colors-sorted 0 (min num-colors 3))))
-    (mapcar #'first top-3-colors)))
-
-(defun parse-class (node)
-  (let* ((parsed-nested-key-values (parse-hash-table node))
-         (class-name (second (assoc :class-name parsed-nested-key-values)))
-         (knowrob-string (string-trim "'" class-name))
-         (position-of-# (position #\# knowrob-string :from-end t))
-         (trimmed-class-name (if position-of-#
-                                 (subseq knowrob-string (1+ position-of-#))
-                                 knowrob-string))
-         (resulting-class (roslisp-utilities:lispify-ros-name
-                           trimmed-class-name
-                           :keyword)))
-    resulting-class))
-
-
-(defmethod parse-json-node (name (node hash-table))
-  (let* ((parsed-node (parse-hash-table node)))
-    (if name
-        (list name parsed-node)
-        parsed-node)))
+(defmacro getassoc (key alist)
+  `(cdr (assoc ,key ,alist :test #'equal)))
 
 (defmethod parse-json-node (name (node list))
-  (let ((parsed-node (mapcar #'parse-hash-table node)))
+  (let ((parsed-node (parse-alist node)))
     (if name
         (list name parsed-node)
         parsed-node)))
 
-(defmethod parse-json-node ((name (eql :timestamp)) node)
-  (parse-ignored-node))
+(defmethod parse-json-node ((name (eql :timestamp)) node) nil)
+(defmethod parse-json-node ((name (eql :pipelineid)) node) nil)
+
 ;; (defmethod parse-json-node ((name (eql :uid)) node)
 ;;   (list :name (roslisp-utilities:lispify-ros-name
 ;;                (let ((knowrob-string (string-trim "'" node)))
@@ -185,60 +78,116 @@
 ;;                        knowrob-string)))
 ;;                :keyword)))
 (defmethod parse-json-node ((name (eql :uid)) node)
-  (list name (parse-case-sensitive-string node)))
+  (list :name (intern node :keyword)))
+
 (defmethod parse-json-node ((name (eql :id)) node)
-  (list :name (parse-case-sensitive-string
-               (let ((first-char (aref node 0)))
-                (if (and (>= (char-code first-char) 49)
-                         (<= (char-code first-char) 57))
-                    (concatenate 'string "OBJ" node)
-                    node)))))
+  (list :id node))
 
 (defmethod parse-json-node ((name (eql :type)) node)
-  (list name (parse-string node)))
+  (list name (intern (string-upcase node) :keyword)))
 (defmethod parse-json-node ((name (eql :shape)) node)
-  (list name (mapcar #'parse-string node)))
+  (list name (intern (string-upcase node) :keyword)))
 (defmethod parse-json-node ((name (eql :size)) node)
-  (list name (parse-string node)))
+  (list name (intern (string-upcase node) :keyword)))
 (defmethod parse-json-node ((name (eql :source)) node)
-  (list name (parse-string node)))
+  (list name (intern (string-upcase node) :keyword)))
 
 (defmethod parse-json-node ((name (eql :segment)) node)
   (parse-nested-node name node))
 (defmethod parse-json-node ((name (eql :boundingbox)) node)
   (parse-nested-node :bb node))
-;; (defmethod parse-json-node ((name (eql :pose)) node)
-;;   (list name (parse-pose node)))
-(defmethod parse-json-node ((name (eql :poses)) node)
-  (list name (parse-poses node)))
 
-;; (defmethod parse-json-node ((name (eql :transform)) node)
-;;   (list name (parse-transform node)))
+(defmethod parse-json-node ((name (eql :pose)) node)
+  (if (getassoc "pose" node)
+      (list name (parse-alist node))
+      (let* ((frame-id (getassoc "frame_id" node))
+             (pos-x (getassoc "pos_x" node))
+             (pos-y (getassoc "pos_y" node))
+             (pos-z (getassoc "pos_z" node))
+             (rot-x (getassoc "rot_x" node))
+             (rot-y (getassoc "rot_y" node))
+             (rot-z (getassoc "rot_z" node))
+             (rot-w (getassoc "rot_w" node))
+             (stamp (getassoc "stamp" node))
+             (object-pose (cl-transforms-stamped:make-pose-stamped
+                           frame-id
+                           stamp
+                           (cl-transforms:make-3d-vector pos-x pos-y pos-z)
+                           (cl-transforms:make-quaternion rot-x rot-y rot-z rot-w))))
+        (list name object-pose))))
+
+(defmethod parse-json-node ((name (eql :transform)) node)
+  (let* ((frame-id (getassoc "frame_id" node))
+         (child-frame-id (getassoc "child_frame_id" node))
+         (pos-x (getassoc "pos_x" node))
+         (pos-y (getassoc "pos_y" node))
+         (pos-z (getassoc "pos_z" node))
+         (rot-x (getassoc "rot_x" node))
+         (rot-y (getassoc "rot_y" node))
+         (rot-z (getassoc "rot_z" node))
+         (rot-w (getassoc "rot_w" node))
+         (stamp (getassoc "stamp" node))
+         (object-transform
+           (cl-transforms:make-transform
+            (cl-transforms:make-3d-vector pos-x pos-y pos-z)
+            (cl-transforms:make-quaternion rot-x rot-y rot-z rot-w))))
+    ;; string knowrob namespace from frame IDs
+    (let ((child-frame-position-of-# (position #\# child-frame-id :from-end t)))
+      (when child-frame-position-of-#
+        (setf child-frame-id (subseq child-frame-id (1+ child-frame-position-of-#)))))
+    ;; make sure transform is defined in robot-base-frame
+    (let ((object-transform-stamped
+            (if (equalp frame-id cram-tf:*robot-base-frame*)
+                (cl-transforms-stamped:transform->transform-stamped
+                 frame-id
+                 child-frame-id
+                 stamp
+                 object-transform)
+                (let ((transform-to-base-frame (cl-transforms-stamped:lookup-transform
+                                                cram-tf:*transformer*
+                                                cram-tf:*robot-base-frame*
+                                                frame-id
+                                                :timeout cram-tf:*tf-default-timeout*)))
+                  (cl-transforms-stamped:transform->transform-stamped
+                   cram-tf:*robot-base-frame*
+                   child-frame-id
+                   (cl-transforms-stamped:stamp transform-to-base-frame)
+                   (cl-transforms:transform* transform-to-base-frame object-transform))))))
+      (list name object-transform-stamped))))
 
 (defmethod parse-json-node ((name (eql :color)) node)
-  (list name (parse-color node)))
+  (list name (mapcar #'first
+                     (subseq (sort (parse-alist node)
+                                   #'> :key #'second)
+                             0 3))))
 
-;; (defmethod parse-json-node ((name (eql :detection)) node)
-;;   ;; (sort ll #'> :key (lambda (x) (second (find :confidence x :key #'car))))
-;;   ;; (cons name (second (find :type (alexandria:extremum
-;;   ;;                                 (second (call-next-method))
-;;   ;;                                 #'>
-;;   ;;                                 :key (lambda (x)
-;;   ;;                                        (second (find :confidence x :key #'car))))
-;;   ;;                          :key #'first)))
-;;   (find :class (parse-alist node) :key #'car))
+(defmethod parse-json-node ((name (eql :detection)) node)
+  ;; (sort ll #'> :key (lambda (x) (second (find :confidence x :key #'car))))
+  ;; (cons name (second (find :type (alexandria:extremum
+  ;;                                 (second (call-next-method))
+  ;;                                 #'>
+  ;;                                 :key (lambda (x)
+  ;;                                        (second (find :confidence x :key #'car))))
+  ;;                          :key #'first)))
+  (find :class (parse-alist node) :key #'car))
 
-(defmethod parse-json-node ((name (eql :class)) node)
-  ;; ignore confidence entry for now
-  (list :type (parse-class node)))
+(defmethod parse-json-node ((name (eql :class)) node) ; ignore confidence entry for now
+  (list :type (let ((parsed-nested-key-values (parse-alist node)))
+                (roslisp-utilities:lispify-ros-name
+                 (let ((knowrob-string (string-trim "'" (second (assoc :name parsed-nested-key-values)))))
+                   (let* ((position-of-# (position #\# knowrob-string :from-end t)))
+                     (if position-of-#
+                         (subseq knowrob-string (1+ position-of-#))
+                         knowrob-string)))
+                 :keyword))))
 
 (defmethod parse-json-node ((name (eql :dimensions-2d)) node)
-  (list name (let ((parsed-dimensions (parse-hash-table node)))
+  (list name (let ((parsed-dimensions (parse-alist node)))
                (cl-transforms:make-3d-vector (second (assoc :width parsed-dimensions))
                                              (second (assoc :height parsed-dimensions)) 0))))
 
 (defmethod parse-json-node ((name (eql :dimensions-3d)) node)
-  (list name (let ((parsed-dimensions (parse-hash-table node)))
+  (list name (let ((parsed-dimensions (parse-alist node)))
                (cl-transforms:make-3d-vector (second (assoc :width parsed-dimensions))
                                              (second (assoc :depth parsed-dimensions))
                                              (second (assoc :height parsed-dimensions))))))
@@ -262,7 +211,7 @@
     flattened-tree))
 
 (defun parse-json-result (json-string)
-  (flatten-one-level (parse-json-node nil (yason:parse json-string :object-as :hash-table))))
+  (flatten-one-level (parse-json-node nil (yason:parse json-string :object-as :alist))))
 
  ;; (#<CRAM-DESIGNATORS:OBJECT-DESIGNATOR
  ;;   ((:TYPE :WHEEL)
