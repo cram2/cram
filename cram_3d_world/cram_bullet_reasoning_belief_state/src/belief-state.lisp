310af2b083dea9e70b0178cba72f97591d79ba40
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2018-06-20 18:43:16 +0200

    Add collision group and mask to prolog predicate to spawn special URDFs.

--- cram_3d_world/cram_bullet_reasoning_belief_state/src/belief-state.lisp
+++ cram_3d_world/cram_bullet_reasoning_belief_state/src/belief-state.lisp
@@ -72,6 +72,8 @@ is replaced with replacement.
                 (btr:assert ?w (btr:object :static-plane :floor ((0 0 0) (0 0 0 1))
                                            :normal (0 0 1) :constant 0))
                 (btr:assert ?w (btr:object :urdf :kitchen ((0 0 0) (0 0 0 1))
+                                           :collision-group :static-filter
+                                           :collision-mask (:default-filter :character-filter)
                                            ,@(when kitchen
                                                `(:urdf ,kitchen))
                                            :compound T))
