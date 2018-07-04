e8ac737372d56d9638a6424908c0d942aeefd848
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-07-04 14:24:58 +0200

    Started to refactor action-parameter-handler

--- cram_integration/cram_cloud_logger/src/action-parameter-handler.lisp
+++ cram_integration/cram_cloud_logger/src/action-parameter-handler.lisp
@@ -1,39 +1,20 @@
 (in-package :ccl)
 
-(defparameter cram-action-designator-parameter-to-knowrob (make-hash-table))
 
-(defun init-map ()
-  (add-to-map :effort 'send-effort-action-parameter)
-  (add-to-map :position 'send-position-action-parameter)
-  (add-to-map :object 'send-object-action-parameter)
-  (add-to-map :arm 'send-arm-action-parameter)
-  (add-to-map :gripper 'send-gripper-action-parameter)
-  (add-to-map :left-poses 'send-left-pose-stamped-list-action-parameter)
-  (add-to-map :right-poses 'send-right-pose-stamped-list-action-parameter)
-  (add-to-map :location 'send-location-action-parameter)
-  (add-to-map :target 'send-target-action-parameter))
+(define-all-action-designator-parameter-logging-functions)
 
-(defun add-to-map (cram-action-designator-property to-knowrob-function)
-  (setf
-   (gethash  cram-action-designator-property cram-action-designator-parameter-to-knowrob)
-   to-knowrob-function))
+(defun log-action-designator-parameters-for-logged-action-designator
+    (action-designator-parameters action-designator-logging-id)
+  (mapcar (lambda (action-designator-parameter)
+            (let ((parameter-name (first action-designator-parameter))
+                  (parameter-value (second action-designator-parameter)))
+              (let ((logging-function (get-logging-function-for-action-designator-parameter parameter-name))
+                    (logging-args (list action-designator-logging-id parameter-value)))
+                (execute-logging-function-with-arguments
+                  logging-function logging-args))))
+          action-designator-parameters))
 
-(init-map)
-(defun log-action-parameter (designator action-id)
-  (mapcar (lambda (key-value-pair)
-            (let ((key (first key-value-pair))
-                  (value (second key-value-pair)))
-              (execute-logging-parameter-function key action-id value)))
-          (desig:properties designator)))
-
-(defun execute-logging-parameter-function (cram-action-designator-property action-id value)
-  (let ((logging-parameter-function
-          (get-logging-parameter-function cram-action-designator-property))
-        (logging-parameter-function-arguments
-          (list action-id value)))
-    (print logging-parameter-function)
+(defun execute-logging-function-with-arguments
+    (logging-parameter-function logging-parameter-function-arguments)
     (when logging-parameter-function 
-      (apply logging-parameter-function logging-parameter-function-arguments))))
-
-(defun get-logging-parameter-function (cram-action-designator-property)
-  (gethash cram-action-designator-property cram-action-designator-parameter-to-knowrob))
+      (apply logging-parameter-function logging-parameter-function-arguments)))
