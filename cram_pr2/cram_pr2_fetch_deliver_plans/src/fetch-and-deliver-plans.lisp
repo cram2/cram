ecdf512b6b24afc5c7cea823b10ed9fba54a4cfc
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2018-08-13 19:37:43 +0200

    [pr2-fd-plans] bugfix in delivering + close drawer on unwind-protect + drop at sink at same location

--- cram_pr2/cram_pr2_fetch_deliver_plans/src/fetch-and-deliver-plans.lisp
+++ cram_pr2/cram_pr2_fetch_deliver_plans/src/fetch-and-deliver-plans.lisp
@@ -344,15 +344,16 @@ and using the grasp and arm specified in `pick-up-action' (if not NIL)."
                (roslisp:ros-warn (fd-plans deliver)
                                  "Undeliverable. Last chance -- another target location.~%~a" e)
                (cpl:do-retry outer-target-location-retries
-                 (setf ?target-location (desig:next-solution ?target-location))
-                 (if ?target-location
-                     (progn
-                       (roslisp:ros-info (fd-plans deliver) "Retrying with new placement...")
-                       (setf ?pose-at-target-location (desig:reference ?target-location))
-                       (cpl:retry))
-                     (progn
-                       (roslisp:ros-warn (fd-plans deliver) "No more placement samples :'(")
-                       (cpl:fail 'common-fail:object-undeliverable))))
+                 (let ((next-target-location (desig:next-solution ?target-location)))
+                   (if next-target-location
+                       (progn
+                         (roslisp:ros-info (fd-plans deliver) "Retrying with new placement...")
+                         (setf ?pose-at-target-location (desig:reference ?target-location))
+                         (setf ?target-location next-target-location)
+                         (cpl:retry))
+                       (progn
+                         (roslisp:ros-warn (fd-plans deliver) "No more placement samples :'(")
+                         (cpl:fail 'common-fail:object-undeliverable)))))
                (roslisp:ros-warn (fd-plans deliver) "No more re-placement retries left :'(")
                (cpl:fail 'common-fail:object-undeliverable)))
 
@@ -364,7 +365,7 @@ and using the grasp and arm specified in `pick-up-action' (if not NIL)."
                                                   (pose ?pose-at-target-location)))))))
 
             ;; take a new `?target-robot-location' sample if a failure happens
-            (cpl:with-retry-counters ((relocation-for-ik-retries 2))
+            (cpl:with-retry-counters ((relocation-for-ik-retries 5))
               (cpl:with-failure-handling
                   (((or common-fail:navigation-goal-in-collision
                         common-fail:object-undeliverable
@@ -396,16 +397,17 @@ and using the grasp and arm specified in `pick-up-action' (if not NIL)."
                             common-fail:object-unreachable) (e)
                          (roslisp:ros-warn (fd-plans deliver) "Placing failed: ~a" e)
                          (cpl:do-retry target-location-retries
-                           (setf ?target-location (desig:next-solution ?target-location))
-                           (if ?target-location
-                               (progn
-                                 (roslisp:ros-warn (fd-plans deliver)
-                                                   "Retrying with new placing location...~%")
-                                 (cpl:retry))
-                               (progn
-                                 (roslisp:ros-warn (fd-plans deliver)
-                                                   "No target location samples left :'(~%")
-                                 (cpl:fail 'common-fail:object-undeliverable))))
+                           (let ((next-target-location (desig:next-solution ?target-location)))
+                            (if next-target-location
+                                (progn
+                                  (roslisp:ros-warn (fd-plans deliver)
+                                                    "Retrying with new placing location...~%")
+                                  (setf ?target-location next-target-location)
+                                  (cpl:retry))
+                                (progn
+                                  (roslisp:ros-warn (fd-plans deliver)
+                                                    "No target location samples left :'(~%")
+                                  (cpl:fail 'common-fail:object-undeliverable)))))
                          (roslisp:ros-warn (fd-plans deliver)
                                            "No target-location-retries left :'(~%")
                          (cpl:fail 'common-fail:object-undeliverable)))
@@ -495,8 +497,9 @@ and using the grasp and arm specified in `pick-up-action' (if not NIL)."
       (exe:perform
        (desig:an action
                  (type placing)
-                 (target (desig:a location
-                                  (pose ?placing-pose))))))))
+                 ;; (target (desig:a location
+                 ;;                  (pose ?placing-pose)))
+                 )))))
 
 
 (cpl:def-cram-function transport (?object-designator ?search-location ?delivering-location ?arm
@@ -507,54 +510,55 @@ and using the grasp and arm specified in `pick-up-action' (if not NIL)."
                            (location ?search-location)
                            (distance 0.3))))
 
-  (let ((?perceived-object-designator
-          (exe:perform (desig:an action
-                                 (type searching)
-                                 (object ?object-designator)
-                                 (location ?search-location)))))
-    (roslisp:ros-info (pp-plans transport)
-                      "Found object of type ~a."
-                      (desig:desig-prop-value ?perceived-object-designator :type))
+  (unwind-protect
+       (let ((?perceived-object-designator
+               (exe:perform (desig:an action
+                                      (type searching)
+                                      (object ?object-designator)
+                                      (location ?search-location)))))
+         (roslisp:ros-info (pp-plans transport)
+                           "Found object of type ~a."
+                           (desig:desig-prop-value ?perceived-object-designator :type))
 
-    (let (?fetch-robot-location
-          ?fetch-pick-up-action
-          ?deliver-robot-location
-          ?deliver-place-action)
+         (let (?fetch-robot-location
+               ?fetch-pick-up-action
+               ?deliver-robot-location
+               ?deliver-place-action)
 
-      ;; If running on the real robot, execute below task tree in projection
-      ;; N times first, then pick the best parameterization
-      ;; and use that parameterization in the real world.
-      ;; If running in projection, just execute the task tree below as normal.
-      (pr2-proj-reasoning:with-projected-task-tree
-          (?fetch-robot-location ?fetch-pick-up-action
-                                 ?deliver-robot-location ?deliver-place-action)
-          3
-          #'pr2-proj-reasoning:pick-best-parameters-by-distance
+           ;; If running on the real robot, execute below task tree in projection
+           ;; N times first, then pick the best parameterization
+           ;; and use that parameterization in the real world.
+           ;; If running in projection, just execute the task tree below as normal.
+           (pr2-proj-reasoning:with-projected-task-tree
+               (?fetch-robot-location ?fetch-pick-up-action
+                                      ?deliver-robot-location ?deliver-place-action)
+               3
+               #'pr2-proj-reasoning:pick-best-parameters-by-distance
 
-        (let ((?fetched-object
-                (exe:perform (desig:an action
-                                       (type fetching)
-                                       (when ?arm
-                                         (arm ?arm))
-                                       (object ?perceived-object-designator)
-                                       (robot-location ?fetch-robot-location)
-                                       (pick-up-action ?fetch-pick-up-action)))))
+             (let ((?fetched-object
+                     (exe:perform (desig:an action
+                                            (type fetching)
+                                            (when ?arm
+                                              (arm ?arm))
+                                            (object ?perceived-object-designator)
+                                            (robot-location ?fetch-robot-location)
+                                            (pick-up-action ?fetch-pick-up-action)))))
 
-          (roslisp:ros-info (pp-plans transport) "Fetched the object.")
-          (cpl:with-failure-handling
-              ((common-fail:object-undeliverable (e)
-                 (declare (ignore e))
-                 (drop-at-sink)
-                 (return)))
-            (exe:perform (desig:an action
-                                   (type delivering)
-                                   (object ?fetched-object)
-                                   (target ?delivering-location)
-                                   (robot-location ?deliver-robot-location)
-                                   (place-action ?deliver-place-action))))))))
+               (roslisp:ros-info (pp-plans transport) "Fetched the object.")
+               (cpl:with-failure-handling
+                   ((common-fail:object-undeliverable (e)
+                      (declare (ignore e))
+                      (drop-at-sink)
+                      (return)))
+                 (exe:perform (desig:an action
+                                        (type delivering)
+                                        (object ?fetched-object)
+                                        (target ?delivering-location)
+                                        (robot-location ?deliver-robot-location)
+                                        (place-action ?deliver-place-action))))))))
 
-  (unless search-location-accessible
-    (exe:perform (desig:an action
-                           (type sealing)
-                           (location ?search-location)
-                           (distance 0.3)))))
+    (unless search-location-accessible
+      (exe:perform (desig:an action
+                             (type sealing)
+                             (location ?search-location)
+                             (distance 0.3))))))
