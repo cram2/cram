b42b67f0a4b6e4ca3bf00e58ce206d2f955ab2b4
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-08-17 10:13:00 +0200

    added launch file and neem generator

--- cram_learning/cram_sim_log_generator/src/neem-generator.lisp
+++ cram_learning/cram_sim_log_generator/src/neem-generator.lisp
@@ -3,19 +3,24 @@
 (defun generate-neem (&optional objects-to-fetch-deliever)
   (setf cram-tf:*tf-broadcasting-enabled* t)
   (roslisp-utilities:startup-ros :name "cram" :anonymous nil)
-  (setf ccl::*is-logging-enabled* t)
-  (setf ccl::*host* "'https://localhost'")
-  (setf ccl::*cert-path* "'/home/koralewski/Desktop/localhost.pem'")
-  (setf ccl::*api-key* "'K103jdr40Rp8UX4egmRf42VbdB1b5PW7qYOOVvTDAoiNG6lcQoaDHONf5KaFcefs'")
-  (ccl::connect-to-cloud-logger)
 
-  (let ((experiment-id (format nil "~d" (truncate (* 1000000 (cram-utilities:current-timestamp))))))
-    (format t "Starting experiment ~a~%" experiment-id)
+  (let ((objects-str (roslisp:get-param "/neem_generator/objects"))
+        objects '())
+    (loop for x in  (split-sequence:split-sequence #\Space objects-str)
+          do (setf objects (append objects (list (values (intern (string-upcase x) "KEYWORD"))))))
+    (setf ccl::*is-logging-enabled* t)
+    (setf ccl::*host* "'https://localhost'")
+    (setf ccl::*cert-path* "'/home/koralewski/Desktop/localhost.pem'")
+    (setf ccl::*api-key* "'K103jdr40Rp8UX4egmRf42VbdB1b5PW7qYOOVvTDAoiNG6lcQoaDHONf5KaFcefs'")
+    (ccl::connect-to-cloud-logger)
+
+    (let ((experiment-id (format nil "~d" (truncate (* 1000000 (cram-utilities:current-timestamp))))))
+      (format t "Starting experiment ~a~%" experiment-id)
     
-    (unwind-protect
-         (if objects-to-fetch-deliever
-             (pr2-proj:with-simulated-robot (demo::demo-random nil '(:bowl :spoon)))
-             (pr2-proj:with-simulated-robot (demo::demo-random)))
-                 (ccl::export-log-to-owl (concatenate 'string experiment-id ".owl"))
-                 (format t "Done with experiment ~a~%" experiment-id)
-                 (ccl::reset-logged-owl))))
+      (unwind-protect
+           (if objects
+               (pr2-proj:with-simulated-robot (demo::demo-random nil objects))
+               (pr2-proj:with-simulated-robot (demo::demo-random)))
+        (ccl::export-log-to-owl (concatenate 'string experiment-id ".owl"))
+        (format t "Done with experiment ~a~%" experiment-id)
+        (ccl::reset-logged-owl)))))
