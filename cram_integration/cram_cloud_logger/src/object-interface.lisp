17b9c8c5e3d7f600829062932d5d23bb6cdcbcaa
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-01-28 14:15:04 +0100

    [ccl] added *is-logging-enabled* checks

--- cram_integration/cram_cloud_logger/src/object-interface.lisp
+++ cram_integration/cram_cloud_logger/src/object-interface.lisp
@@ -2,42 +2,48 @@
 
 
 (defmethod man-int:calculate-object-faces :around (robot-to-object-transform)
-  (let ((pose-id (send-create-transform-pose-stamped robot-to-object-transform)))
-    (let ((query-id
-            (ccl::create-prolog-log-query-str
-             "calculate-object-faces"
-             (list pose-id)))
-          (query-result (call-next-method)))
-      (log-end-of-query query-id)
-      (log-result-of-query
-       query-id
-       (concatenate 'string (write-to-string (car query-result)) " " (write-to-string (cadr query-result))))
-      query-result)))
+  (if *is-logging-enabled*
+      (let ((pose-id (send-create-transform-pose-stamped robot-to-object-transform)))
+        (let ((query-id
+                (ccl::create-prolog-log-query-str
+                 "calculate-object-faces"
+                 (list pose-id)))
+              (query-result (call-next-method)))
+          (log-end-of-query query-id)
+          (log-result-of-query
+           query-id
+           (concatenate 'string (write-to-string (car query-result)) " " (write-to-string (cadr query-result))))
+          query-result))
+      (call-next-method)))
 
 (defmethod man-int:get-object-type-gripping-effort :around (object-type)
-  (let ((query-id
-          (ccl::create-prolog-log-query-str
-           "get-object-type-gripping-effort"
-           (list (write-to-string object-type))))
-        (query-result (call-next-method)))
-    (log-end-of-query query-id)
-    query-result))
+  (if *is-logging-enabled*
+      (let ((query-id
+              (ccl::create-prolog-log-query-str
+               "get-object-type-gripping-effort"
+               (list (write-to-string object-type))))
+            (query-result (call-next-method)))
+        (log-end-of-query query-id)
+        query-result)
+      (call-next-method)))
 
 
 (defmethod man-int:get-object-type-grasps :around (object-type
                                                    arm
                                                    object-transform-in-base)
-  (let ((query-id
-          (ccl::create-prolog-log-query-str
-           "get-object-type-grasps"
-           (list (write-to-string object-type)
-                 (write-to-string nil)
-                 (write-to-string nil)
-                 (write-to-string nil)
-                 (write-to-string arm))))
-        (query-result (call-next-method)))
-    (log-end-of-query query-id)
-      query-result))
+  (if *is-logging-enabled*
+      (let ((query-id
+              (ccl::create-prolog-log-query-str
+               "get-object-type-grasps"
+               (list (write-to-string object-type)
+                     (write-to-string nil)
+                     (write-to-string nil)
+                     (write-to-string nil)
+                     (write-to-string arm))))
+            (query-result (call-next-method)))
+        (log-end-of-query query-id)
+        query-result)
+      (call-next-method)))
 
 (defmethod man-int:get-object-type-gripper-opening :around (object-type)
   ;;(format t "Asking for GRIPPER OPENING for the object: ~a~%" object-type)
