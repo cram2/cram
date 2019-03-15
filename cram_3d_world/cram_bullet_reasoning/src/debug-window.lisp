930530b13db25213dfbe3a94c4791ed81efa8710
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-03-15 20:30:04 +0100

    [btr] made the ADD-VIS-AXIS-OBJECT stuff a bit nicer

--- cram_3d_world/cram_bullet_reasoning/src/debug-window.lisp
+++ cram_3d_world/cram_bullet_reasoning/src/debug-window.lisp
@@ -32,11 +32,11 @@
 
 (defvar *debug-window* nil)
 (defvar *debug-window-lock* (sb-thread:make-mutex))
-(defvar *current-costmap-function* nil)
-(defvar *current-costmap-sample* nil)
-(defvar *vis-axis-x* nil)
-(defvar *vis-axis-y* nil)
-(defvar *vis-axis-z* nil)
+(defvar *current-costmap-function* nil "A math function object for visualizing costmaps.")
+(defvar *current-costmap-sample* nil "A red sphere for visualizing costmap samples.")
+(defvar *vis-axis-x* nil "The X axis of an object for visualizing poses.")
+(defvar *vis-axis-y* nil "The Y axis of an object for visualizing poses.")
+(defvar *vis-axis-z* nil "The Z axis of an object for visualizing poses.")
 (defparameter *costmap-z* 0.0)
 (defparameter *costmap-tilt* (cl-transforms:make-quaternion 0 0 0 1))
 
@@ -85,14 +85,12 @@
       (setf *current-costmap-sample* nil))
     (when (and *vis-axis-x* *debug-window*)
       (setf (gl-objects *debug-window*)
-            (remove *vis-axis-x* (gl-objects *debug-window*)))
-      (setf *vis-axis-x* nil)
-      (setf (gl-objects *debug-window*)
-            (remove *vis-axis-y* (gl-objects *debug-window*)))
-      (setf *vis-axis-y* nil)
-      (setf (gl-objects *debug-window*)
-            (remove *vis-axis-z* (gl-objects *debug-window*)))
-      (setf *vis-axis-z* nil))))
+            (remove *vis-axis-z*
+                    (remove *vis-axis-y*
+                            (remove *vis-axis-x* (gl-objects *debug-window*)))))
+      (setf *vis-axis-x* nil
+            *vis-axis-y* nil
+            *vis-axis-z* nil))))
 
 
 
@@ -129,6 +127,7 @@
       (when *debug-window*
         (push *current-costmap-function* (gl-objects *debug-window*))))))
 
+
 (defun add-costmap-sample-object (point)
   (sb-thread:with-mutex (*debug-window-lock*)
     (when (and *current-costmap-sample* *debug-window*)
@@ -147,88 +146,54 @@
         (push *current-costmap-sample* (gl-objects *debug-window*))))))
 
 
-(defun add-vis-axis-object (object &optional (length 0.30))
-  "spawn axis on the object that is given with 3 rigid bodies type box"
-  (add-vis-axis-pose
-   (btr:pose (btr:object btr:*current-bullet-world* object))length))
+(defgeneric add-vis-axis-object (object-or-pose &optional length)
+  (:documentation "Spawn a coordinate frame at a given pose or at btr:object origin.
+It is built from 3 rigid bodies of primitive box shape.")
 
+  (:method ((object-name symbol) &optional (length 0.30))
+    (add-vis-axis-object
+     (btr:pose (btr:object btr:*current-bullet-world* object-name)) length))
 
-(defun add-vis-axis-pose (pose &optional (length 0.30))
-  "spawn axis on the object that is given with 3 rigid bodies type box"
-  (sb-thread:with-mutex (*debug-window-lock*)
-    (when (and *vis-axis-x* *debug-window*)
-      (setf (gl-objects *debug-window*)
-            (remove *vis-axis-x* (gl-objects *debug-window*)))
-      (setf (gl-objects *debug-window*)
-            (remove *vis-axis-y* (gl-objects *debug-window*)))
-      (setf (gl-objects *debug-window*)
-            (remove *vis-axis-z* (gl-objects *debug-window*))))
-    (setf length (/ length 2))
-    (let* ((object-transform
-             (cl-tf:pose->transform pose))
-           (object-vector nil)
-           (object-rotation (roslisp:with-fields (translation rotation)
-                                object-transform (setf object-vector translation) rotation))
-           (offset-x (cl-tf:make-transform (cl-tf:make-3d-vector length 0 0)
-                                           (cl-tf:make-identity-rotation)))
-           (offset-y (cl-tf:make-transform (cl-tf:make-3d-vector 0 length 0)
-                                           (cl-tf:make-identity-rotation)))
-           (offset-z (cl-tf:make-transform (cl-tf:make-3d-vector 0 0 length)
-                                           (cl-tf:make-identity-rotation)))
-           (actual-pose-x (cl-tf:transform* object-transform offset-x
-                                            (cl-tf:transform-inv object-transform)))
-           (actual-pose-y (cl-tf:transform* object-transform offset-y
-                                            (cl-tf:transform-inv object-transform)))
-           (actual-pose-z (cl-tf:transform* object-transform offset-z
-                                            (cl-tf:transform-inv object-transform))))
+  (:method ((pose cl-transforms:pose) &optional (length 0.30))
+    (sb-thread:with-mutex (*debug-window-lock*)
+      (when (and *vis-axis-x* *debug-window*)
+        (setf (gl-objects *debug-window*)
+              (remove *vis-axis-z*
+                      (remove *vis-axis-y*
+                              (remove *vis-axis-x* (gl-objects *debug-window*))))))
 
-      ;;calculates the offset + the pose 
-      (let ((new-point-x
-              (roslisp:with-fields (translation) actual-pose-x
-                (cl-tf:v+ object-vector translation)))
-            (new-point-y
-              (roslisp:with-fields (translation) actual-pose-y
-                (cl-tf:v+ object-vector translation)))
-            (new-point-z
-              (roslisp:with-fields (translation) actual-pose-z
-                (cl-tf:v+ object-vector translation))))
-        
-        ;;x-axis body
-        (setf *vis-axis-x*
-              (make-instance 'rigid-body
-                             :pose (cl-transforms:make-pose
-                                    new-point-x
-                                    object-rotation)
-                             :collision-shape
-                             (make-instance 'colored-box-shape
-                                            :half-extents (cl-tf:make-3d-vector length 0.01 0.01)
-                                            :color '(0.5 0.0 0 0.5))))
+      (let ((length/2 (/ length 2))
+            (object-transform (cl-transforms:pose->transform pose)))
 
-        ;;y-axis body
-        (setf *vis-axis-y*
-              (make-instance 'rigid-body
-                             :pose (cl-transforms:make-pose
-                                    new-point-y
-                                    object-rotation)
-                             :collision-shape
-                             (make-instance 'colored-box-shape
-                                            :half-extents (cl-tf:make-3d-vector 0.01 length 0.01)
-                                            :color '(0.298 0.5 0 0.5))))
-        
-        ;;z-axis body
-        (setf *vis-axis-z*
-              (make-instance 'rigid-body
-                             :pose (cl-transforms:make-pose
-                                    new-point-z
-                                    object-rotation)
-                             :collision-shape
-                             (make-instance 'colored-box-shape
-                                            :half-extents (cl-tf:make-3d-vector 0.01 0.01 length)
-                                            :color '(0 0 0.5 0.5))))
+        ;; spawn the objects
+        (flet ((make-axis-rigid-body (position-list half-extents-list color)
+                 (make-instance 'rigid-body
+                   :pose (cl-transforms:transform->pose
+                          ;; make the center of axis not in the center of pose
+                          ;; but offset, such that it's a corner and not a cross
+                          (cl-transforms:transform*
+                           object-transform
+                           (cl-transforms:make-transform
+                            (apply #'cl-transforms:make-3d-vector position-list)
+                            (cl-transforms:make-identity-rotation))))
+                   :collision-shape (make-instance 'colored-box-shape
+                                      :half-extents (apply #'cl-transforms:make-3d-vector
+                                                           half-extents-list)
+                                      :color color))))
+
+          (setf *vis-axis-x*
+                (make-axis-rigid-body `(,length/2 0 0) `(,length/2 0.01 0.01) '(0.5 0 0 0.5))
+                *vis-axis-y*
+                (make-axis-rigid-body `(0 ,length/2 0) `(0.01 ,length/2 0.01) '(0 0.5 0 0.5))
+                *vis-axis-z*
+                (make-axis-rigid-body `(0 0 ,length/2) `(0.01 0.01 ,length/2) '(0 0 0.5 0.5))))
+
         (when *debug-window*
           (push *vis-axis-x* (gl-objects *debug-window*))
           (push *vis-axis-y* (gl-objects *debug-window*))
           (push *vis-axis-z* (gl-objects *debug-window*)))))))
+
+
 
 
 (defmethod costmap:on-visualize-costmap opengl ((map costmap:location-costmap))
