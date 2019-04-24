f089fd893cd3f5d85c5a041fe39ffed21f0d6c7b
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-04-24 16:30:57 +0200

    [pr2-shopping] clean up and update to newest branch version

--- cram_pr2/cram_pr2_shopping_demo/src/utils.lisp
+++ cram_pr2/cram_pr2_shopping_demo/src/utils.lisp
@@ -31,30 +31,33 @@
 
 (defun spawn-shelf ()
   (let ((shelve-urdf
-                   (cl-urdf:parse-urdf
-                    (roslisp:get-param "shelf_description"))))
-             (prolog:prolog
-              `(and (btr:bullet-world ?world)
-                    (assert (btr:object ?world :urdf :kitchen ((0 0 0) (0 0 0 1)) :urdf ,shelve-urdf))))))
+          (cl-urdf:parse-urdf
+           (roslisp:get-param "shelf_description"))))
+    (prolog:prolog
+     `(and (btr:bullet-world ?world)
+           (assert (btr:object ?world :urdf :kitchen ((0 0 0) (0 0 0 1))
+                                            :urdf ,shelve-urdf))))))
 
 
-(defun spawn-kitchen()
+(defun spawn-kitchen ()
   (let ((kitchen-urdf
-                   (cl-urdf:parse-urdf
-                    (roslisp:get-param "kitchen_description"))))
-             (prolog:prolog
-              `(and (btr:bullet-world ?world)
-                    (assert (btr:object ?world :urdf :kitchen ((0 0 0) (0 0 0 1)) :urdf ,kitchen-urdf))))))
+          (cl-urdf:parse-urdf
+           (roslisp:get-param "kitchen_description"))))
+    (prolog:prolog
+     `(and (btr:bullet-world ?world)
+           (assert (btr:object ?world :urdf :kitchen ((0 0 0) (0 0 0 1))
+                                            :urdf ,kitchen-urdf))))))
 
 (defun spawn-robot ()
   (setf cram-robot-interfaces:*robot-urdf*
-                   (cl-urdf:parse-urdf
-                    (roslisp:get-param "robot_description")))
-             (prolog:prolog
-              `(and (btr:bullet-world ?world)
-                    (cram-robot-interfaces:robot ?robot)
-                    (assert (btr:object ?world :urdf ?robot ((0 0 0) (0 0 0 1)) :urdf ,cram-robot-interfaces:*robot-urdf*))
-                    (assert (btr:joint-state ?world ?robot (("torso_lift_joint" 0.15d0)))))))
+        (cl-urdf:parse-urdf
+         (roslisp:get-param "robot_description")))
+  (prolog:prolog
+   `(and (btr:bullet-world ?world)
+         (cram-robot-interfaces:robot ?robot)
+         (assert (btr:object ?world :urdf ?robot ((0 0 0) (0 0 0 1))
+                             :urdf ,cram-robot-interfaces:*robot-urdf*))
+         (assert (btr:joint-state ?world ?robot (("torso_lift_joint" 0.15d0)))))))
 
 
 (defun spawn-and-place-objects ()
@@ -62,62 +65,73 @@
   (place-objects)
   (btr:simulate btr:*current-bullet-world* 2))
 
-(defun spawn-objects()
+(defun spawn-objects ()
   (btr-utils:spawn-object 'dove :dove)
   (btr-utils:spawn-object 'somat :somat)
   (btr-utils:spawn-object 'heitmann :heitmann)
   (btr-utils:spawn-object 'denkmit :denkmit))
 
-(defun place-objects()
+(defun place-objects ()
   (btr-utils:move-object 'dove '((-1 -1.1 0.7) (0 0 0 1)))
   (btr-utils:move-object 'somat '((-0.5 -1.1 0.4) (0 0 0 1)))
   (btr-utils:move-object 'heitmann '((-1.3 -1.1 1) (0 0 0 1)))
   (btr-utils:move-object 'denkmit '((-2 -1.1 1.3) (0 0 0 1))))
 
-(defun replace-denkmit()
+(defun replace-denkmit ()
   (cram-occasions-events:on-event
    (make-instance 'cpoe:object-detached-robot
-                  :arm ':right
-                  :object-name 'denkmit))
+     :arm :right
+     :object-name 'denkmit))
   (btr-utils:move-object 'denkmit '((-2 -1.1 1.3) (0 0 0 1))))
 
-(defun replace-dove()
+(defun replace-dove ()
   (cram-occasions-events:on-event
    (make-instance 'cpoe:object-detached-robot
-                  :arm ':left
-                  :object-name 'dove))
+     :arm :left
+     :object-name 'dove))
   (btr-utils:move-object 'dove '((-1 -1.1 0.7) (0 0 0 1))))
 
-(defun replace-heitmann()
- (cram-occasions-events:on-event
-  (make-instance 'cpoe:object-detached-robot
-                 :arm ':left
-                 :object-name 'heitmann))
- (btr-utils:move-object 'heitmann '((-1.3 -1.1 1) (0 0 0 1))))
-
-(defun replace-somat()
+(defun replace-heitmann ()
   (cram-occasions-events:on-event
    (make-instance 'cpoe:object-detached-robot
-                  :arm ':left
-                  :object-name 'somat))
-  (btr-utils:move-object 'somat '((-0.5 -1.1 0.3) (0 0 0 1)))) 
+     :arm :left
+     :object-name 'heitmann))
+  (btr-utils:move-object 'heitmann '((-1.3 -1.1 1) (0 0 0 1))))
 
+(defun replace-somat ()
+  (cram-occasions-events:on-event
+   (make-instance 'cpoe:object-detached-robot
+     :arm :left
+     :object-name 'somat))
+  (btr-utils:move-object 'somat '((-0.5 -1.1 0.3) (0 0 0 1))))
+
+(def-fact-group costmap-metadata ()
+    (<- (location-costmap:costmap-size 12 12))
+    (<- (location-costmap:costmap-origin -6 -6))
+    (<- (location-costmap:costmap-resolution 0.04))
+
+    (<- (location-costmap:costmap-padding 0.3))
+    (<- (location-costmap:costmap-manipulation-padding 0.4))
+    (<- (location-costmap:costmap-in-reach-distance 0.9))
+    (<- (location-costmap:costmap-reach-minimal-distance 0.2))
+    (<- (location-costmap:visibility-costmap-size 2))
+    (<- (location-costmap:orientation-samples 2))
+  (<- (location-costmap:orientation-sample-step 0.1)))
 
 (defun init ()
   (roslisp:start-ros-node "shopping_demo")
-  
+
   (cram-bullet-reasoning-belief-state::ros-time-init)
   (cram-location-costmap::location-costmap-vis-init)
   (cram-tf::init-tf)
 
-  (prolog:prolog '(btr:bullet-world ?world))
-  
-  (prolog:prolog '(and (btr:bullet-world ?world)
+  (prolog:prolog '(and
+                   (btr:bullet-world ?world)
                    (btr:debug-window ?world)))
-  
-  (prolog:prolog '(and (btr:bullet-world ?world)
-                              (assert (btr:object ?world :static-plane :floor ((0 0 0) (0 0 0 1))
-                                                                       :normal (0 0 1) :constant 0))))
-  (btr:add-objects-to-mesh-list "cram_pr2_shopping_demo")
-)
+
+  (prolog:prolog '(and
+                   (btr:bullet-world ?world)
+                   (assert (btr:object ?world :static-plane :floor ((0 0 0) (0 0 0 1))
+                                                            :normal (0 0 1) :constant 0))))
+  (btr:add-objects-to-mesh-list "cram_pr2_shopping_demo"))
 
