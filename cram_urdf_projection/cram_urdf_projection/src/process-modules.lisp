dfd363588721e1d7ec27ab2b67006a9d9f67c408
vanessa-rin <hassouna@uni-bremen.de>
2019-05-13 18:56:07 +0200

    added my name to copyright

--- cram_urdf_projection/cram_urdf_projection/src/process-modules.lisp
+++ cram_urdf_projection/cram_urdf_projection/src/process-modules.lisp
@@ -1,5 +1,6 @@
 ;;;
 ;;; Copyright (c) 2017, Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
+;;;               2019, Vanessa Hassouna <hassouna@uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
--- cram_urdf_projection/cram_urdf_projection/src/projection-environment.lisp
+++ cram_urdf_projection/cram_urdf_projection/src/projection-environment.lisp
@@ -1,4 +1,5 @@
 ;;; Copyright (c) 2012, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;;               2019, Vanessa Hassouna <hassouna@uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
