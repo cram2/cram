747364e698d2105c1c44954340b86bc88f2965ca
vanessa-rin <hassouna@uni-bremen.de>
2019-05-14 19:47:10 +0200

    deleted my name here

--- cram_bullet_world/cram_urdf_projection/src/projection-environment.lisp
+++ cram_bullet_world/cram_urdf_projection/src/projection-environment.lisp
@@ -1,5 +1,4 @@
 ;;; Copyright (c) 2012, Lorenz Moesenlechner <moesenle@in.tum.de>
-;;;               2019, Vanessa Hassouna <hassouna@uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
