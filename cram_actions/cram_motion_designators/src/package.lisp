1e0bf5fa385788e438c873d54311c5fb4c7ed3f1
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-05-20 16:43:16 +0200

    moved CRAM-URDF-ENV-MANIP to CRAM-3D-WORLD

--- cram_actions/cram_motion_designators/src/package.lisp
+++ cram_actions/cram_motion_designators/src/package.lisp
@@ -1,20 +1,19 @@
-;;;
-;;; Copyright (c) 2018, Christopher Pollok <cpollok@uni-bremen.de>
+;;; Copyright (c) 2012, Lorenz Moesenlechner <moesenle@in.tum.de>
 ;;; All rights reserved.
-;;;
+;;; 
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions are met:
-;;;
+;;; 
 ;;;     * Redistributions of source code must retain the above copyright
 ;;;       notice, this list of conditions and the following disclaimer.
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of the Institute for Artificial Intelligence/
-;;;       Universitaet Bremen nor the names of its contributors may be used to
-;;;       endorse or promote products derived from this software without
-;;;       specific prior written permission.
-;;;
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;; 
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,7 +28,14 @@
 
 (in-package :cl-user)
 
-(defpackage cram-urdf-environment-manipulation
-  (:nicknames #:env-man)
-  (:use #:common-lisp #:cram-prolog
-        #:desig #:exe))
+(defpackage cram-common-designators
+  (:use #:common-lisp ;#:cram-plan-occasions-events #:cram-robot-interfaces
+        #:prolog #:desig #:spec ;; #:cram-semantic-map
+        ;; #:cl-transforms-stamped
+        )
+  ;; (:import-from #:cram-tf
+  ;;               *fixed-frame* *transformer* *tf-default-timeout*)
+  (:export
+   ;; motions
+   #:move-base #:move-torso #:move-head #:detect #:inspect #:move-gripper-joint
+   #:move-tcp #:move-joints #:move-with-constraints))
