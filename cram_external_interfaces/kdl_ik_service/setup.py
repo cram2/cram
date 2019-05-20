af585d0702ae3a45653fbbe641715b6f679678f8
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-05-20 12:56:13 +0200

    [kdl_ik] KDL_ARM_KINEMATICS -> KDL_IK_SERVICE

--- cram_external_interfaces/kdl_ik_service/setup.py
+++ cram_external_interfaces/kdl_ik_service/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['kdl_arm_kinematics'],
+    packages=['kdl_ik_service'],
     package_dir={'': 'src'},
 )
 
