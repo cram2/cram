8a9e5a583e2af06115a39aa15f1f540aebf1feea
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-05-21 19:27:12 +0200

    moved CRAM_FETCH_DELIVER_PLANS from CRAM_PR2 to CRAM_3D_WORLD

--- cram_actions/cram_urdf_environment_manipulation/src/package.lisp
+++ cram_actions/cram_urdf_environment_manipulation/src/package.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Copyright (c) 2017, Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
+;;; Copyright (c) 2018, Christopher Pollok <cpollok@uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
@@ -10,10 +10,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of the Intelligent Autonomous Systems Group/
-;;;       Technische Universitaet Muenchen nor the names of its contributors 
-;;;       may be used to endorse or promote products derived from this software 
-;;;       without specific prior written permission.
+;;;     * Neither the name of the Institute for Artificial Intelligence/
+;;;       Universitaet Bremen nor the names of its contributors may be used to
+;;;       endorse or promote products derived from this software without
+;;;       specific prior written permission.
 ;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,8 +29,7 @@
 
 (in-package :cl-user)
 
-(defpackage cram-fetch-deliver-plans
-  (:nicknames #:fd-plans)
-  (:use #:common-lisp #:cram-prolog)
-  (:export))
-
+(defpackage cram-urdf-environment-manipulation
+  (:nicknames #:env-man)
+  (:use #:common-lisp #:cram-prolog
+        #:desig #:exe))
