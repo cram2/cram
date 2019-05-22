0600003550f124e8d2b7d0b6883f4649020cf041
Christopher Pollok <cpollok@uni-bremen.de>
2019-05-22 17:09:04 +0200

    [env-man] Fix package for environment-occasions.

--- cram_actions/cram_urdf_environment_manipulation/src/environment-occasions.lisp
+++ cram_actions/cram_urdf_environment_manipulation/src/environment-occasions.lisp
@@ -27,7 +27,7 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :pr2-em)
+(in-package :env-man)
 
 (prolog:def-fact-group environment-occasions ()
   (prolog:<- (:container-state ?container-designator ?distance)
