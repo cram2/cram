727c182bf78f9c3ac07e7638e60f50138f66d37b
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-06-20 20:46:03 +0200

    [pr2-demo] restrict pr2 from goint too far to the left to be safe

--- cram_integration/cram_robots/cram_pr2_pick_place_demo/src/costmaps.lisp
+++ cram_integration/cram_robots/cram_pr2_pick_place_demo/src/costmaps.lisp
@@ -31,15 +31,12 @@
 
 (defun make-restricted-area-cost-function ()
   (lambda (x y)
-    1.0
-    ;; (if (> x 1.0)
-    ;;     0.0
-    ;;     (if (and (> x 0.0) (> y -1.0) (< y 0.7 ;1.0
-    ;;                                      ))
-    ;;         1.0
-    ;;         (if (and (< x 0.0) (> x -1.0) (> y -1.0) (< y 2.5)) 1.0
-    ;;             0.0)))
-    ))
+    (if (> x 1.2)
+        0.0
+        (if (and (> x 0.0) (> y -1.5) (< y 1.7))
+            1.0
+            (if (and (< x 0.0) (> x -1.5) (> y -1.5) (< y 2.5)) 1.0
+                0.0)))))
 
 (defmethod location-costmap:costmap-generator-name->score ((name (eql 'restricted-area))) 5)
 
