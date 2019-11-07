2159a89d10164efccd4426af961c10a1b5e5ba75
Arthur Niedzwiecki <aniedz@cs.uni-bremen.de>
2019-11-07 16:42:03 +0100

    Change test to suit lisp-unit and make it assert something.

--- cram_bullet_world/cram_bullet_reasoning/tests/compound-meshes-tests.lisp
+++ cram_bullet_world/cram_bullet_reasoning/tests/compound-meshes-tests.lisp
@@ -1,4 +1,8 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
 ;;; Copyright (c) 2018, Mihai Pomarlan <pomarlan@uni-bremen.de>
+;;;               2019, Arthur Niedzwiecki <aniedz@cs.uni-bremen.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
@@ -26,24 +30,36 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :bullet-reasoning-tests) 
+(in-package :btr-tests) 
 
-(defun pile-of-plates (&optional (platenum 20) (simint 1000))
-  (let* ((ids (mapcar (lambda (num)
-                        (intern (format nil "PLATE-~a" num) :keyword))
-                      (alexandria:iota platenum))))
+(define-test pile-of-plates 
+  (let ((plate-num 5) (simulation-interval 10))
     (prolog:prolog '(btr:clear-bullet-world))
     (prolog:prolog '(and (btr:bullet-world ?world)
-                         (btr:debug-window ?world)))
+                     (btr:debug-window ?world)))
     (prolog:prolog '(and (btr:bullet-world ?world)
-                         (assert (btr:object ?world :static-plane :floor ((0 0 0) (0 0 0 1))
-                                             :normal (0 0 1) :constant 0))))
-    (read-char)
-    (mapcar (lambda (plate-id)
-              (prolog:prolog `(and (btr:bullet-world ?world)
-                                   (assert (btr:object ?world :mesh ,plate-id ((0 0 5) (0 0 0 1)) :mass 0.2 :color (1 0 0) :scale 5 :mesh :plate))))
-              (prolog:prolog `(and (btr:bullet-world ?world)
-                                   (btr:simulate ?world ,simint)))
-              (format t "Added ~a~%" plate-id))
-            ids)))
-
+                     (assert (btr:object ?world :static-plane :floor ((0 0 0) (0 0 0 1))
+                                                              :normal (0 0 1) :constant 0))))
+    (loop until (object btr:*current-bullet-world* :floor))
+    (labels ((spawn-stack (y-offset name-prefix mesh)
+                       (loop for id below plate-num
+                             for plate-id = (intern (format nil "~a~a" name-prefix id) :keyword)
+                             do (prolog:prolog
+                                 `(and (btr:bullet-world ?world)
+                                       (assert (btr:object ?world :mesh ,plate-id
+                                                           ((0 ,y-offset ,(+ 0.1 (* (coerce id 'float) 0.1)))
+                                                            (0 0 0 1))
+                                                           :mass 0.2
+                                                           :color (1 0 0)
+                                                           :scale 5
+                                                           :mesh ,mesh))))
+                                (prolog:prolog `(and (btr:bullet-world ?world)
+                                                     (btr:simulate ?world ,simulation-interval)))
+                                (format t "Added ~a~%" plate-id)))
+           (z-coord (obj-name)
+             (cl-tf:z (cl-tf:origin (pose (object btr:*current-bullet-world* obj-name))))))
+      (spawn-stack 0.0 "PLATE-COMPOUND-" :plate-compound)
+      (spawn-stack 2.0 "PLATE-" :plate)
+      (btr:simulate btr:*current-bullet-world* 100)
+      (lisp-unit:assert-true (< (z-coord :plate-compound-4)
+                                (z-coord :plate-4))))))
