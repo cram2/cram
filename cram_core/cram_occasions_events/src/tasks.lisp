46abfeaaa85756e95bff74c7237cd51203d4a73f
Arthur Niedzwiecki <aniedz@cs.uni-bremen.de>
2019-11-21 14:29:37 +0100

    [coe] Restore old code from demmel/lorenz.

--- cram_core/cram_occasions_events/src/tasks.lisp
+++ cram_core/cram_occasions_events/src/tasks.lisp
@@ -232,3 +232,232 @@
             (>= ?created-time-next-task ?created-time-other-next-task)))
   )
 
+#+the-rest-is-all-test-stuff-so-commented-it-out-to-avoid-dependency-problems
+(
+(defun test ()
+  (cet:enable-fluent-tracing)
+  (cpl-impl::remove-top-level-task-tree :top-level)
+
+  (pr2-proj:with-simulated-robot
+    (pr2-pp-demo::demo-random nil))
+
+  (cut:force-ll (prolog:prolog `(task-navigating-action :top-level ((demo-random))
+                                                        ?task ?designator))))
+
+(defun test-next-sibling-time ()
+  (cet:enable-fluent-tracing)
+  (cpl-impl::remove-top-level-task-tree :top-level)
+
+  (pr2-proj:with-simulated-robot
+    (pr2-pp-demo::demo-random nil))
+
+  (cut:force-ll
+   (prolog:prolog `(and (task-navigating-action :top-level ((demo-random)) ?task ?des)
+                        (task-next-sibling :top-level ?task ?next-task)
+                        (task-created-at :top-level ?task ?created)
+                        (task-created-at :top-level ?next-task ?next-created)
+                        (format "time: ~a   time next: ~a~%" ?created ?next-created)))))
+
+(defun test-failed-actions ()
+  (cet:enable-fluent-tracing)
+  (cpl-impl::remove-top-level-task-tree :top-level)
+
+  (pr2-proj:with-simulated-robot
+    (pr2-pp-demo::demo-random))
+
+  (cut:force-ll
+   (prolog:prolog `(and (task-specific-action :top-level ((demo-random)) :fetching ?task ?desig)
+                        (task-outcome ?task :failed)
+                        (format "desig: ~a~%" ?desig)))))
+
+(defun test-find-location-for-pick-up-using-occasions ()
+  (cet:enable-fluent-tracing)
+  (cpl-impl::remove-top-level-task-tree :top-level)
+
+  (proj:with-projection-environment pr2-proj:pr2-bullet-projection-environment
+    (cpl-impl::named-top-level (:name :top-level)
+      (pr2-pp-demo::demo-random nil))
+
+    (let ((top-level-name :top-level))
+      (cut:var-value
+       '?pick-location
+       (car
+        (prolog:prolog
+         `(and (task-fetching-action ,top-level-name ((demo-random)) ?fetching-task ?_)
+               (task-full-path ?fetching-task ?fetching-path)
+               (task-picking-up-action ,top-level-name ?fetching-path ?picking-up-task ?_)
+               (task-outcome ?picking-up-task :succeeded)
+               (task-started-at ,top-level-name ?picking-up-task ?picking-up-start)
+               (cram-robot-interfaces:robot ?robot)
+               (btr:timeline ?timeline)
+               (coe:holds ?timeline (cpoe:loc ?robot ?pick-location)
+                          (coe:at ?picking-up-start)))))))))
+)
+
+#+this-old-code-is-based-on-episodes-and-not-used-anymore-because-we-work-only-on-task-trees
+((def-fact-group fluents (holds)
+  ;; FLUENT
+  (<- (fluent ?fluent)
+    (not (bound ?fluent))
+    (lisp-fun cet:episode-knowledge-traced-fluent-names ?fluents)
+    (member ?fluent ?fluents))
+
+  (<- (fluent ?fluent)
+    ;; bit of a hack to increase performance
+    (bound ?fluent)
+    (lisp-pred symbolp ?fluent))
+
+  ;; HOLDS FLUENT-VALUE
+  (<- (holds (fluent-value ?fluent ?value) ?t)
+    (fluent ?fluent)
+    (lisp-fun cet:episode-knowledge-fluent-durations ?fluent ?durations)
+    (member (?value . ?duration) ?durations)
+    (duration-includes ?duration ?t))
+
+  ;; For execution-time fluent access
+  (<- (fluent-value ?fluent ?value)
+    (lisp-fun value ?fluent ?value)))
+
+(def-fact-group tasks (holds)
+
+  ;; TOP-LEVEL
+  (<- (top-level ?top-level-task)
+    (lisp-fun cet:episode-knowledge-task-tree ?top-level-task))
+  
+  ;; TASK
+  (<- (task ?task)
+    (lisp-fun cet:episode-knowledge-task-list ?tasks)
+    (member ?task ?tasks))
+
+  (<- (goal-task ?task)
+    (lisp-fun cet:episode-knowledge-goal-task-list ?tasks)
+    (member ?task ?tasks))
+
+  (<- (task-path ?task ?path)
+    (lisp-fun task-tree-node-path ?task ?path))
+  
+  ;; for dont use this optimization
+  ;; (<- (task ?task)
+  ;;   (bound ?task)
+  ;;   (lisp-pred task-tree-node-p ?task))
+
+  ;; SUBTASK
+  (<- (subtask ?task ?subtask)
+    (bound ?task)
+    (lisp-fun task-children ?task ?children)
+    (member ?subtask ?children))
+
+  (<- (subtask ?task ?subtask)
+    (not (bound ?task))
+    (bound ?subtask)
+    (lisp-fun task-tree-node-parent ?subtask ?task)
+    (not (== ?task nil)))
+
+  (<- (subtask ?task ?subtask)
+    (not (bound ?task))
+    (not (bound ?subtask))
+    (task ?task)
+    (subtask ?task ?subtask))
+
+  ;; SUBTASK+
+  (<- (subtask+ ?task ?subtask)
+    (subtask ?task ?subtask))
+
+  (<- (subtask+ ?task ?subtask)
+    (subtask ?task ?tmp)
+    (subtask+ ?tmp ?subtask))
+
+  ;; TASK-SIBLING
+  (<- (task-sibling ?task ?sibling)
+    (bound ?task)
+    (subtask ?parent ?task)
+    (subtask ?parent ?sibling)
+    (not (== ?sibling ?task)))
+
+  (<- (task-sibling ?task ?sibling)
+    (not (bound ?task))
+    (subtask ?parent ?sibling)
+    (subtask ?parent ?task)
+    (not (== ?sibling ?task)))
+
+  ;; TASK-NEXT-SIBLING
+  ;; FIXME: we should not simply use temporal, but rater causal relations
+  (<- (task-next-sibling ?task ?next)
+    (bound ?task)
+    (list-of ?sib (task-sibling ?task ?sib) ?siblings)
+    (member ?next ?siblings)
+    (task-created-at ?task ?ct-task)
+    (task-created-at ?next ?ct-next)
+    (<= ?ct-task ?ct-next)
+    (forall (and
+             (member ?other ?siblings)
+             (not (== ?next ?other))
+             (task-created-at ?other ?ct-other)
+             (<= ?ct-task ?ct-other))
+            (<= ?ct-next ?ct-other)))
+  
+  ;; TASK-STATUS-FLUENT
+  (<- (task-status-fluent ?task ?fluent)
+    (task ?task)
+    (lisp-fun task-status-fluent-name ?task ?fluent))
+
+  ;; TASK-GOAL
+  (<- (task-goal ?task ?goal)
+    (goal-task ?task)
+    (lisp-pred goal-task-tree-node-p ?task)
+    (lisp-fun goal-task-tree-node-goal ?task ?goal))
+
+  ;; TASK-OUTCOME
+  (<- (task-outcome ?task ?outcome)
+    (member ?outcome (:succeeded :failed :evaporated))
+    (holds (task-status ?task ?outcome) ?_))
+
+  ;; TASK-RESULT
+  (<- (task-result ?task ?result)
+    (task ?task)
+    (lisp-fun task-tree-node-result ?task ?result))
+
+  ;; TASK-ERROR
+  (<- (task-error ?task ?error)
+    (task-outcome ?task :failed)
+    (task-result ?task ?result)
+    (lisp-fun extract-task-error ?result ?error))
+
+  ;; ERROR-TYPE
+  (<- (error-type ?error ?type)
+    (bound ?error)
+    (lisp-fun type-of ?error ?type))
+
+  (<- (error-type ?error ?type)
+    (not (nound ?error))
+    (warn "Trying to call ERROR-TYPE on unbound variable."))
+
+  ;; HOLDS TASK-STATUS
+  (<- (holds (task-status ?task ?status) ?t)
+    (task-status-fluent ?task ?status-fluent)
+    (holds (fluent-value ?status-fluent ?status) ?t))
+
+  ;; TASK-CREATED-AT
+  (<- (task-created-at ?task ?time)
+    (holds (task-status ?task :created) (at ?time)))
+
+  (<- (task-started-at ?task ?time)
+    (task ?task)
+    (bagof ?t (holds (task-status ?task :running) (at ?t))
+           ?times)
+    (sort ?times < (?time . ?_)))
+
+  ;; TASK-ENDED-AT
+  (<- (task-ended-at ?task ?time)
+    (task ?task)
+    (member ?status (:succeeded :failed :evaporated))
+    (holds (task-status ?task ?status) (at ?time)))
+
+  ;; TODO(gaya-) this is a very weird place to define such a predicate.
+  ;; It's way too specific.
+  ;; (<- (task-location-context ?task ?loc)
+  ;;   (task ?task)
+  ;;   (subtask+ ?loc-task ?task)
+  ;;   (task-goal ?loc-task (at-location (?loc)))
+  ;;   (task ?loc-task))
+  ))
