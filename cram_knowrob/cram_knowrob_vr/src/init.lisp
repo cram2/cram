237ddc6657c3f73f6bad5699535a7d67f9bf5441
Gayane Kazhoyan <gayanek@gmail.com>
2019-12-03 10:59:11 +0100

    [kvr] commented out costmap init function definition

--- cram_knowrob/cram_knowrob_vr/src/init.lisp
+++ cram_knowrob/cram_knowrob_vr/src/init.lisp
@@ -129,17 +129,12 @@ semantic map kitchen."
                            object-types)))
       objects)))
 
+#+do-not-need-this-because-we-do-not-use-costmaps-here
 (defun init-location-costmap-parameters ()
   (def-fact-group costmap-metadata ()
     (<- (location-costmap:costmap-size 12 12))
     (<- (location-costmap:costmap-origin -6 -6))
     (<- (location-costmap:costmap-resolution 0.04))
-
-    (<- (location-costmap:costmap-padding 0.3))
-    (<- (location-costmap:costmap-manipulation-padding 0.4))
-    (<- (location-costmap:costmap-in-reach-distance 0.9))
-    (<- (location-costmap:costmap-reach-minimal-distance 0.2))
-    (<- (location-costmap:visibility-costmap-size 2))
     (<- (location-costmap:orientation-samples 2))
     (<- (location-costmap:orientation-sample-step 0.1))))
 
