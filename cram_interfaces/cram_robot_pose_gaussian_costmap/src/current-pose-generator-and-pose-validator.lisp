309ae8d45e7f17bf58e6e64f0eda1ad49d4b53f4
Gayane Kazhoyan <gayanek@gmail.com>
2019-12-03 23:07:13 +0100

    [gauss-cm] fixes for recent changes in VISIBILITY and REACHABILITY desigs

--- cram_interfaces/cram_robot_pose_gaussian_costmap/src/current-pose-generator-and-pose-validator.lisp
+++ cram_interfaces/cram_robot_pose_gaussian_costmap/src/current-pose-generator-and-pose-validator.lisp
@@ -67,29 +67,51 @@
 
 (defun reachable-location-validator (designator pose)
   (if (cram-robot-interfaces:reachability-designator-p designator)
-      (cut:with-vars-bound (?to-reach-point ?min-distance ?max-distance
-                                            ?orientation-samples ?orientation-sample-step)
+      (cut:with-vars-bound (?to-reach-pose ?min-distance ?max-distance
+                                           ?orientation-samples ?orientation-sample-step)
           (cut:lazy-car
            (prolog:prolog
-            `(and (bagof ?pose
-                         (cram-robot-interfaces:designator-reach-pose ,designator ?pose ?_)
-                         ?poses)
-                  (lisp-fun costmap:2d-pose-covariance ?poses 0.5 (?to-reach-point ?_))
-                  (costmap:costmap-reach-minimal-distance ?min-distance)
-                  (costmap:costmap-in-reach-distance ?max-distance)
-                  (costmap:orientation-samples ?orientation-samples)
-                  (costmap:orientation-sample-step ?orientation-sample-step))))
-        (if (or (cut:is-var ?to-reach-point)
+            `(and
+              ;; (bagof ?pose
+              ;;        (cram-robot-interfaces:designator-reach-pose ,designator ?pose ?_)
+              ;;        ?poses)
+              ;; (lisp-fun costmap:2d-pose-covariance ?poses 0.5 (?to-reach-point ?_))
+              (once (or (and (desig:desig-prop ,designator (:object ?some-object))
+                             (desig:current-designator ?some-object ?object)
+                             (lisp-fun man-int:get-object-pose-in-map ?object ?to-reach-pose)
+                             (lisp-pred identity ?to-reach-pose))
+                        (and (desig:desig-prop ,designator (:location ?some-location))
+                             (desig:current-designator ?some-location ?location)
+                             (desig:designator-groundings ?location ?to-reach-poses)
+                             (member ?to-reach-pose ?to-reach-poses))))
+              (costmap:costmap-reach-minimal-distance ?min-distance)
+              (costmap:costmap-in-reach-distance ?max-distance)
+              (costmap:orientation-samples ?orientation-samples)
+              (costmap:orientation-sample-step ?orientation-sample-step))))
+        (if (or (cut:is-var ?to-reach-pose)
                 (cut:is-var ?min-distance)
                 (cut:is-var ?max-distance))
             :unknown
-            (let ((dist (cl-transforms:v-dist ?to-reach-point (cl-transforms:origin pose)))
+            (let (;; DIST ist the distance from the suggested robot pose to to-see-pose
+                  (dist (cl-transforms:v-dist
+                         ;; project ?TO-REACH-POSE onto the floor,
+                         ;; to make distance calculations accurate
+                         (cl-transforms:copy-3d-vector
+                          (cl-transforms:origin ?to-reach-pose)
+                          :z 0)
+                         (cl-transforms:origin pose)))
+                  ;; PERFECT-ANGLE is the angle around Z to orient the robot in fixed frame
+                  ;; such that to-reach-pose is directly in front of the robot
                   (perfect-angle (costmap::angle-to-point-direction
                                   (cl-transforms:x (cl-transforms:origin pose))
                                   (cl-transforms:y (cl-transforms:origin pose))
-                                  ?to-reach-point))
+                                  (cl-transforms:origin ?to-reach-pose)))
+                  ;; ALLOWED-RANGE is the DELTA that the suggested robot pose angle
+                  ;; is allowed to deviate from PERFECT-ANGLE to still be a valid
+                  ;; robot base orientation to see to-reach-pose
                   (allowed-range (* ?orientation-sample-step
                                     (ceiling (/ ?orientation-samples 2))))
+                  ;; GENERATED-ANGLE is the suggested robot base pose angle around Z
                   (generated-angle (calculate-z-angle pose)))
               (if (and (< dist ?max-distance)
                        (> dist ?min-distance)
@@ -109,28 +131,42 @@
                                          ?orientation-samples ?orientation-sample-step)
           (cut:lazy-car
            (prolog:prolog
-            `(and (or (and (desig:desig-prop ,designator (:object ?some-object))
-                           (desig:current-designator ?some-object ?object)
-                           (btr-belief:object-designator-name ?object ?object-name)
-                           (btr:bullet-world ?world)
-                           (btr:object-pose ?world ?object-name ?to-see-pose))
-                      (and (desig:desig-prop ,designator (:location ?some-location))
-                           (desig:current-designator ?some-location ?location)
-                           (desig:desig-prop ?location (:pose ?to-see-pose))))
+            `(and (once (or (and (desig:desig-prop ,designator (:object ?some-object))
+                                 (desig:current-designator ?some-object ?object)
+                                 ;; (btr-belief:object-designator-name ?object ?object-name)
+                                 ;; (btr:bullet-world ?world)
+                                 ;; (btr:object-pose ?world ?object-name ?to-see-pose)
+                                 (lisp-fun man-int:get-object-pose-in-map ?object ?to-see-pose)
+                                 (lisp-pred identity ?to-see-pose))
+                            (and (desig:desig-prop ,designator (:location ?some-location))
+                                 (desig:current-designator ?some-location ?location)
+                                 (desig:designator-groundings ?location ?to-see-poses)
+                                 (member ?to-see-pose ?to-see-poses))))
                   (costmap:visibility-costmap-size ?max-distance)
                   (costmap:orientation-samples ?orientation-samples)
                   (costmap:orientation-sample-step ?orientation-sample-step))))
         (if (or (cut:is-var ?to-see-pose) (cut:is-var ?max-distance))
             :unknown
-            (let ((dist (cl-transforms:v-dist
-                         (cl-transforms:origin ?to-see-pose)
+            (let (;; DIST ist the distance from the suggested robot pose to to-see-pose
+                  (dist (cl-transforms:v-dist
+                         ;; project ?TO-SEE-POSE onto the floor,
+                         ;; to make distance calculations accurate
+                         (cl-transforms:copy-3d-vector
+                          (cl-transforms:origin ?to-see-pose)
+                          :z 0)
                          (cl-transforms:origin pose)))
+                  ;; PERFECT-ANGLE is the angle around Z to orient the robot in fixed frame
+                  ;; such that to-see-pose is directly in front of the robot
                   (perfect-angle (costmap::angle-to-point-direction
                                   (cl-transforms:x (cl-transforms:origin pose))
                                   (cl-transforms:y (cl-transforms:origin pose))
                                   ?to-see-pose))
+                  ;; ALLOWED-RANGE is the DELTA that the suggested robot pose angle
+                  ;; is allowed to deviate from PERFECT-ANGLE to still be a valid
+                  ;; robot base orientation to see to-see-pose
                   (allowed-range (* ?orientation-sample-step
                                     (ceiling (/ ?orientation-samples 2))))
+                  ;; GENERATED-ANGLE is the suggested robot base pose angle around Z
                   (generated-angle (calculate-z-angle pose)))
               (if (and (< dist ?max-distance)
                        (<= (abs (- perfect-angle generated-angle)) allowed-range))
