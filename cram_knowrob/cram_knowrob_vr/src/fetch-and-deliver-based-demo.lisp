65e96d5a08325595b79fd06810fffb13a1e15fa2
Gayane Kazhoyan <gayanek@gmail.com>
2019-12-06 00:41:45 +0100

    [kvr] logging only summary of failures and also durations

--- cram_knowrob/cram_knowrob_vr/src/fetch-and-deliver-based-demo.lisp
+++ cram_knowrob/cram_knowrob_vr/src/fetch-and-deliver-based-demo.lisp
@@ -193,16 +193,22 @@
 (defparameter *experiment-log-filename* "package://cram_knowrob_vr/experiments/failures")
 (defparameter *experiment-log-extension* ".csv")
 
-(defvar *experiment-log-current-object* "")
+(defparameter *experiment-log-detailed?* nil)
+
+(defvar *experiment-log-current-object* nil)
 
 (defvar *experiment-log-current-demo-run-vr* -1)
 (defvar *experiment-log-current-demo-run-heur* -1)
 
 (defparameter *experiment-log-failures-to-count*
   '(common-fail:searching-failed common-fail:fetching-failed common-fail:delivering-failed
-    common-fail:navigation-low-level-failure common-fail:manipulation-low-level-failure
-    common-fail:perception-low-level-failure common-fail:ptu-low-level-failure))
+    common-fail:perception-low-level-failure common-fail:navigation-low-level-failure
+    common-fail:manipulation-low-level-failure common-fail:ptu-low-level-failure))
 (defvar *experiment-log-current-demo-run-object-failures* nil)
+
+(defvar *experiment-log-current-execution-time-bowl* 0)
+(defvar *experiment-log-current-execution-time-cup* 0)
+(defvar *experiment-log-current-execution-time-spoon* 0)
 
 
 (defun experiment-log (string &key
@@ -210,16 +216,26 @@
                                 (demo-run (if *kvr-enabled*
                                               *experiment-log-current-demo-run-vr*
                                               *experiment-log-current-demo-run-heur*)))
-  (with-open-file (stream (physics-utils:parse-uri
-                           (format nil
-                                   "~a_~a~a"
-                                   *experiment-log-filename*
-                                   (if *kvr-enabled* "VR" "HEUR")
-                                   *experiment-log-extension*))
-                          :direction :output
-                          :if-exists :append
-                          :if-does-not-exist :create)
-    (format stream "~a,~a,~a~%" demo-run object-type string)))
+  (let ((file-path
+          (physics-utils:parse-uri
+           (format nil
+                   "~a_~a~a"
+                   *experiment-log-filename*
+                   (if *kvr-enabled* "VR" "HEUR")
+                   *experiment-log-extension*))))
+    (unless (probe-file file-path)
+      (with-open-file (stream file-path
+                              :direction :output
+                              :if-exists :error
+                              :if-does-not-exist :create)
+        (format stream "RUN_ID,OBJ_TYPE,FAIL_TYPE,TRANSPORT_FAIL,~
+                        SEARCH_FAIL,FETCH_FAIL,DELIVER_FAIL,~
+                        PERCEPT_FAIL,NAV_FAIL,MANIP_FAIL,TIME_SEC~%~%~%")))
+    (with-open-file (stream file-path
+                            :direction :output
+                            :if-exists :append
+                            :if-does-not-exist :error)
+      (format stream "~a,~a,~a~%" demo-run (or object-type "") string))))
 
 (defun read-last-line (file-uri)
   (with-open-file (stream (physics-utils:parse-uri file-uri)
@@ -271,22 +287,23 @@
             (string 'cpl:simple-plan-failure)
             (cpl:plan-failure (type-of (first args)))
             (t 'unknown-failure---------------------))))
-   (experiment-log
-    (btr-belief::replace-all
-     (format nil "~a" failure-symbol)
-     '(#\Newline) " "))
-    (mapc (lambda (tracked-failure-symbol)
-            (when (subtypep failure-symbol tracked-failure-symbol)
-              (incf (getf (getf *experiment-log-current-demo-run-object-failures*
-                                *experiment-log-current-object*)
-                          tracked-failure-symbol))))
-          *experiment-log-failures-to-count*)))
+    (when *experiment-log-detailed?*
+      (experiment-log
+       (btr-belief::replace-all
+        (format nil "~a" failure-symbol)
+        '(#\Newline) " ")))
+    (when (and *experiment-log-current-demo-run-object-failures*
+               *experiment-log-current-object*)
+      (if (getf *experiment-log-current-demo-run-object-failures*
+                *experiment-log-current-object*)
+          (mapc (lambda (tracked-failure-symbol)
+                  (when (subtypep failure-symbol tracked-failure-symbol)
+                    (incf (getf (getf *experiment-log-current-demo-run-object-failures*
+                                      *experiment-log-current-object*)
+                                tracked-failure-symbol))))
+                *experiment-log-failures-to-count*)
+          (warn "Object ~a is not tracked for failures!" *experiment-log-current-object*)))))
 
-(defun experiment-log-current-object-failures ()
-  (experiment-log (format nil "SUM CURRENT OBJECT FAILURES~%")))
-
-(defun experiment-log-current-demo-run-failures ()
-  (experiment-log (format nil "SUM CURRENT DEMO RUN FAILURES~%")))
 
 (defun generate-empty-failure-property-list ()
   (let (new-prop-list)
@@ -295,26 +312,121 @@
           *experiment-log-failures-to-count*)
     new-prop-list))
 
+(defun get-experiment-log-failures (failure-symbol &optional object-type)
+  (let ((failure-num
+          (if object-type
+              (getf (getf *experiment-log-current-demo-run-object-failures*
+                          object-type)
+                    failure-symbol)
+              (+ (getf (getf *experiment-log-current-demo-run-object-failures*
+                             'bowl)
+                       failure-symbol)
+                 (getf (getf *experiment-log-current-demo-run-object-failures*
+                             'cup)
+                       failure-symbol)
+                 (getf (getf *experiment-log-current-demo-run-object-failures*
+                             'spoon)
+                       failure-symbol)))))
+    (if (eql failure-symbol 'common-fail:perception-low-level-failure)
+        (/ failure-num 5)
+        failure-num)))
+
+(defun get-experiment-log-transport-duration (&optional object-type)
+  (if object-type
+      (case object-type
+        (bowl *experiment-log-current-execution-time-bowl*)
+        (cup *experiment-log-current-execution-time-cup*)
+        (spoon *experiment-log-current-execution-time-spoon*))
+      (+ *experiment-log-current-execution-time-bowl*
+         *experiment-log-current-execution-time-cup*
+         *experiment-log-current-execution-time-spoon*)))
+
+
+(defun experiment-log-current-demo-run-failures (&optional object-type)
+  (let* ((searching-failures
+           (get-experiment-log-failures
+            'common-fail:searching-failed object-type))
+         (fetching-failures
+           (get-experiment-log-failures
+            'common-fail:fetching-failed object-type))
+         (delivering-failures
+           (get-experiment-log-failures
+            'common-fail:delivering-failed object-type))
+         (transporting-failed
+           (+ searching-failures fetching-failures delivering-failures))
+         (navigation-failures
+           (get-experiment-log-failures
+            'common-fail:navigation-low-level-failure object-type))
+         (manipulation-failures
+           (get-experiment-log-failures
+            'common-fail:manipulation-low-level-failure object-type))
+         (perception-failures
+           (get-experiment-log-failures
+            'common-fail:perception-low-level-failure object-type))
+         (duration
+           (get-experiment-log-transport-duration object-type)))
+    (experiment-log (format nil "SUM,~a,~a,~a,~a,~a,~a,~a,~a"
+                            transporting-failed
+                            searching-failures fetching-failures delivering-failures
+                            perception-failures navigation-failures manipulation-failures
+                            duration))
+    (when *experiment-log-detailed?*
+      (experiment-log (format nil "~%")))))
+
+
 (defun experiment-log-start-demo-run ()
   (set-experiment-log-current-demo-run)
-  (setf *experiment-log-current-object* "")
-  (experiment-log (format nil "~%~%"))
+  (setf *experiment-log-current-object* nil)
+  (if *experiment-log-detailed?*
+      (experiment-log (format nil "~%~%"))
+      (experiment-log (format nil "")))
 
   (setf (getf *experiment-log-current-demo-run-object-failures* 'bowl)
         (generate-empty-failure-property-list))
   (setf (getf *experiment-log-current-demo-run-object-failures* 'cup)
         (generate-empty-failure-property-list))
   (setf (getf *experiment-log-current-demo-run-object-failures* 'spoon)
-        (generate-empty-failure-property-list)))
+        (generate-empty-failure-property-list))
+
+  (setf *experiment-log-current-execution-time-bowl* 0
+        *experiment-log-current-execution-time-cup* 0
+        *experiment-log-current-execution-time-spoon* 0))
 
 (defun experiment-log-finish-demo-run ()
-  (setf *experiment-log-current-object* "")
+  (setf *experiment-log-current-object* nil)
   (experiment-log-current-demo-run-failures)
-  (experiment-log (format nil "~%~%"))
+  (if *experiment-log-detailed?*
+      (experiment-log (format nil "~%~%"))
+      (experiment-log (format nil "")))
   (if *kvr-enabled*
       (incf *experiment-log-current-demo-run-vr*)
       (incf *experiment-log-current-demo-run-heur*)))
 
+(defun experiment-log-start-object-transport (object-type)
+  (setf *experiment-log-current-object* object-type)
+  (case object-type
+    (bowl (setf *experiment-log-current-execution-time-bowl* (roslisp:ros-time)))
+    (cup (setf *experiment-log-current-execution-time-cup* (roslisp:ros-time)))
+    (spoon (setf *experiment-log-current-execution-time-spoon* (roslisp:ros-time)))))
+
+(defun experiment-log-finish-object-transport-successful (object-type)
+  (when *experiment-log-detailed?*
+    (experiment-log (format nil "TRANSPORTING SUCCEEDED~%")))
+  (case object-type
+    (bowl (setf *experiment-log-current-execution-time-bowl*
+                (- (roslisp:ros-time) *experiment-log-current-execution-time-bowl*)))
+    (cup (setf *experiment-log-current-execution-time-cup*
+               (- (roslisp:ros-time) *experiment-log-current-execution-time-cup*)))
+    (spoon (setf *experiment-log-current-execution-time-spoon*
+                 (- (roslisp:ros-time) *experiment-log-current-execution-time-spoon*)))))
+
+(defun experiment-log-finish-object-transport-failed (object-type)
+  (when *experiment-log-detailed?*
+    (experiment-log (format nil "TRANSPORTING FAILED~%")))
+  (case object-type
+    (bowl (setf *experiment-log-current-execution-time-bowl* 0))
+    (cup (setf *experiment-log-current-execution-time-cup* 0))
+    (spoon (setf *experiment-log-current-execution-time-spoon* 0))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END OF EXPERIMENT LOG STUFF ;;;;;;;;;;;;;;;
 
@@ -335,12 +447,12 @@
   (unwind-protect
        (dolist (type list-of-objects)
 
-         (setf *experiment-log-current-object* type)
+         (experiment-log-start-object-transport type)
 
          (cpl:with-failure-handling
              ((common-fail:high-level-failure (e)
                 (declare (ignore e))
-                (experiment-log (format nil "TRANSPORTING FAILED~%"))
+                (experiment-log-finish-object-transport-failed type)
                 (return)))
 
            (if *kvr-enabled*
@@ -397,9 +509,9 @@
                                                (side front)))
                             (target (desig:a location (poses ?delivering-poses)))))))
 
-           (experiment-log (format nil "TRANSPORTING SUCCEEDED~%")))
+           (experiment-log-finish-object-transport-successful type))
 
-         (experiment-log-current-object-failures))
+         (experiment-log-current-demo-run-failures *experiment-log-current-object*))
 
     (experiment-log-finish-demo-run))
 
