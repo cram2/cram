260eb85c67d93ce23d4382c36d4fd5c5a1aa465b
Thomas Lipps <lipps.thomas@web.de>
2019-12-12 18:54:30 +0100

    setf kinda breaks stuff

--- cram_interfaces/cram_manipulation_interfaces/src/trajectories.lisp
+++ cram_interfaces/cram_manipulation_interfaces/src/trajectories.lisp
@@ -315,19 +315,20 @@ Gripper is defined by a convention where Z is pointing towards the object.")
                     object-type
                     maybe-attachment))
          (oTg-std
-           (get-object-type-to-gripper-transform
-            object-type object-name arm grasp)))
-             
-
-    ;; Add z-offset for dropping items on other items
-    (setf (slot-value
-           (slot-value
-            oTg-std
-            'cl-tf:translation)
-           'cl-tf:z)
-          (+ (cl-tf:z (cl-tf:copy-3d-vector
-                       (cl-tf:translation oTg-std)))
-             z-offset))
+           (let ((tmp-oTg-std (get-object-type-to-gripper-transform
+                               object-type object-name arm grasp)))
+             (cl-tf:transform->stamped-transform
+              (cl-tf:frame-id tmp-oTg-std)
+              (cl-tf:child-frame-id tmp-oTg-std)
+              (cl-tf:stamp tmp-oTg-std)
+              (cl-tf:make-transform
+               (cl-tf:make-3d-vector
+                (cl-tf:x (cl-tf:translation tmp-oTg-std))
+                (cl-tf:y (cl-tf:translation tmp-oTg-std))
+                (+ (cl-tf:z (cl-tf:translation tmp-oTg-std))
+                   z-offset))
+               (cl-tf:copy-quaternion
+                (cl-tf:rotation tmp-oTg-std)))))))
 
     (mapcar (lambda (label transforms)
               (make-traj-segment
