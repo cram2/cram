1e23744d8f9f49abdd98b3097033ff55781b5900
Arthur Niedzwiecki <aniedz@cs.uni-bremen.de>
2020-03-05 16:01:44 +0100

    [joints] Make robot-joint-state fluent update on given frequency via rostime.

--- cram_external_interfaces/cram_joint_states/src/joint-states.lisp
+++ cram_external_interfaces/cram_joint_states/src/joint-states.lisp
@@ -32,22 +32,20 @@
 (defvar *joint-state-sub* nil
   "Subscriber for robot's joint state topic.")
 
-(defvar *joint-state-sub-counter* 0
-  "Counter to decrease the frequency of the subscriber callback")
-
-(defparameter *joint-state-sub-frequency-cut* 100 "in times")
+(defparameter *joint-state-frequency* 20 "in Hz")
+(defvar *joint-state-timestamp* 0 "Holds time-stamp of the last fluent update.")
 
 (defvar *robot-joint-states-msg* (cpl:make-fluent :name :robot-joint-states)
   "ROS message containing robot's current joint states.")
 
 (defun init-joint-state-sub ()
-  "Initializes *joint-state-sub*"
+  "Initializes *joint-state-sub*,
+updating `*robot-joint-states-msg*' with frequency given in `*joint-state-frequency*'."
   (flet ((joint-state-sub-cb (joint-state-msg)
-           (incf *joint-state-sub-counter*)
-           (if (> *joint-state-sub-counter* *joint-state-sub-frequency-cut*)
-               (progn
-                 (setf *joint-state-sub-counter* 0)
-                 (setf (cpl:value *robot-joint-states-msg*) joint-state-msg)))))
+           (when (> (- (roslisp:ros-time) *joint-state-timestamp*)
+                    (/ 1.0 *joint-state-frequency*))
+             (setf *joint-state-timestamp* (roslisp:ros-time))
+             (setf (cpl:value *robot-joint-states-msg*) joint-state-msg))))
     (setf *joint-state-sub*
           (roslisp:subscribe "joint_states"
                              "sensor_msgs/JointState"
