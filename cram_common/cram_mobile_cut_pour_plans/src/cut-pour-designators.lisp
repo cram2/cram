fa390f86b6d009a8d0aa38903ffb310658b234c1
Vanessa-rin <hassouna@uni-bremen.de>
2020-03-18 09:45:21 +0100

    [mobile_cut_pour] clean up

--- cram_common/cram_mobile_cut_pour_plans/src/cut-pour-designators.lisp
+++ cram_common/cram_mobile_cut_pour_plans/src/cut-pour-designators.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Copyright (c) 2016, Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
+;;; Copyright (c) 2019, Vanessa Hassouna <kazhoyan@cs.uni-bremen.de>
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
