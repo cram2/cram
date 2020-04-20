3b0f8aeee5a74dcfa10031f53aa1830c9cac502b
Sebastian Koralewski <koralewski.sebastian@gmail.com>
2020-04-20 14:56:10 +0200

    feat:(*): Add comment to log when parameter is unknown.

--- cram_knowrob/cram_cloud_logger/src/action-parameter-handler.lisp
+++ cram_knowrob/cram_cloud_logger/src/action-parameter-handler.lisp
@@ -11,10 +11,11 @@
               (let ((logging-function (get-logging-function-for-action-designator-parameter parameter-name))
                     (logging-args (list action-designator-logging-id parameter-value)))
                 (execute-logging-function-with-arguments
-                  logging-function logging-args))))
+                  action-designator-logging-id parameter-name logging-function logging-args))))
           action-designator-parameters))
 
-(defun execute-logging-function-with-arguments
-    (logging-parameter-function logging-parameter-function-arguments)
-    (when logging-parameter-function 
-      (apply logging-parameter-function logging-parameter-function-arguments)))
+(defun execute-logging-function-with-arguments (action-designator-logging-id parameter-name logging-parameter-function logging-parameter-function-arguments)
+    (if logging-parameter-function 
+        (apply logging-parameter-function logging-parameter-function-arguments)
+        (let ((parameter-name-str (write-to-string parameter-name)))
+          (when (string-not-equal ":TYPE" parameter-name-str) (send-comment action-designator-logging-id (concatenate 'string "Unknown Parameter: " parameter-name-str))))))
