2581d227ff411030f6018aae9a81a461c61bd1fb
Sebastian Koralewski <koralewski.sebastian@gmail.com>
2020-04-28 14:01:58 +0200

    feat(action-parameter-handler.lisp): Parameter values will be stored as comment.

--- cram_knowrob/cram_cloud_logger/src/action-parameter-handler.lisp
+++ cram_knowrob/cram_cloud_logger/src/action-parameter-handler.lisp
@@ -11,11 +11,11 @@
               (let ((logging-function (get-logging-function-for-action-designator-parameter parameter-name))
                     (logging-args (list action-designator-logging-id parameter-value)))
                 (execute-logging-function-with-arguments
-                  action-designator-logging-id parameter-name logging-function logging-args))))
+                 action-designator-logging-id parameter-name logging-function logging-args))))
           action-designator-parameters))
 
 (defun execute-logging-function-with-arguments (action-designator-logging-id parameter-name logging-parameter-function logging-parameter-function-arguments)
     (if logging-parameter-function 
         (apply logging-parameter-function logging-parameter-function-arguments)
         (let ((parameter-name-str (write-to-string parameter-name)))
-          (when (string-not-equal ":TYPE" parameter-name-str) (send-comment action-designator-logging-id (concatenate 'string "Unknown Parameter: " parameter-name-str))))))
+          (when (string-not-equal ":TYPE" parameter-name-str) (send-comment action-designator-logging-id (concatenate 'string "Unknown Parameter: " parameter-name-str " -####- " (write-to-string (cadr logging-parameter-function-arguments))))))))
