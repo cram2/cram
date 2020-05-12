3552746b3d8d0119589075c5fcc970dff8ac5092
Jonas Dech <j.dech@gmx.net>
2020-05-12 13:42:16 +0200

    added new spawn-object method to startup

--- cram_pr2/cram_pr2_shopping_demo/src/setup.lisp
+++ cram_pr2/cram_pr2_shopping_demo/src/setup.lisp
@@ -235,6 +235,7 @@
 (roslisp-utilities:register-ros-init-function spawn-robot)
 (roslisp-utilities:register-ros-init-function spawn-shelf)
 ;;(roslisp-utilities:register-ros-init-function spawn-objects)
+(roslisp-utilities:register-ros-init-function spawn-objects-new)
 (roslisp-utilities:register-ros-init-function spawn-basket)
 
 (def-fact-group costmap-metadata (costmap:costmap-size
