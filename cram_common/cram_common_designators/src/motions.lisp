bd22b72744fd35d9dec8ce30695e822179ec8836
Thomas Lipps <lipps.thomas@web.de>
2020-05-22 16:35:31 +0200

    [common-desigs] rm world-state-detecting quick fix

--- cram_common/cram_common_designators/src/motions.lisp
+++ cram_common/cram_common_designators/src/motions.lisp
@@ -140,4 +140,4 @@
     (or (and (property ?object-designator (:pose ?_))
              (property ?object-designator (:type ?_))
              (property ?object-designator (:name ?_)))
-        (property ?object-designator (:type ?_)))))
+        (property ?object-designator (:name ?_)))))
