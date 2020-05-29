2032d81d87c555dcca1f2953eb723d7df78e8e0d
Thomas Lipps <lipps.thomas@web.de>
2020-05-29 17:43:12 +0200

    [btr] attach-obj: collision-info will be created before adding something in the attached-objects lists

--- cram_3d_world/cram_bullet_reasoning/src/items.lisp
+++ cram_3d_world/cram_bullet_reasoning/src/items.lisp
@@ -147,22 +147,27 @@ unidirectional. See `attach-object' above."
     (return-from attach-object))
   (unless skip-removing-loose
     (remove-loose-attachment-for object))
-  (push (cons (name object)
-              (cons
-               (list (make-attachment :object (name object)
-                                      :attachment attachment-type))
-               ;; Since robot objects are not in the attached-objects
-               ;; list of items, this has to be copied manuelly:
-               (if (object-attached (get-robot-object) object)
-                   (get-collision-information object (get-robot-object))
-                   (create-static-collision-information object))))
-        (slot-value other-object 'attached-objects))
-  (push (cons (name other-object)
-              (cons
-               (list (make-attachment :object (name other-object)
-                                      :loose loose :attachment attachment-type))
-               (create-static-collision-information other-object)))
-        (slot-value object 'attached-objects)))
+  (let ((object-collision-information 
+          ;; Since robot objects are not in the attached-objects
+          ;; list of items, this has to be copied manuelly:
+          (if (object-attached (get-robot-object) object)
+              (get-collision-information object (get-robot-object))
+              (create-static-collision-information object)))
+        (other-object-collision-information 
+          (create-static-collision-information other-object)))
+    
+    (push (cons (name object)
+                (cons
+                 (list (make-attachment :object (name object)
+                                        :attachment attachment-type))
+                 object-collision-information))
+          (slot-value other-object 'attached-objects))
+    (push (cons (name other-object)
+                (cons
+                 (list (make-attachment :object (name other-object)
+                                        :loose loose :attachment attachment-type))
+                 other-object-collision-information))
+          (slot-value object 'attached-objects))))
 
 (defmethod attach-object ((other-objects list) (object item)
                           &key attachment-type loose)
