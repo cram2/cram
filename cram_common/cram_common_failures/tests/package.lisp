955918b2d82529d2f8b97d8a00b75af16f379987
Amar Fayaz <amar@uni-bremen.de>
2020-06-02 10:33:18 +0200

    [common-fail]Fixed whitespace and year

--- cram_common/cram_common_failures/tests/package.lisp
+++ cram_common/cram_common_failures/tests/package.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2012, Amar Fayaz <amar@uni-bremen.de>
+;;; Copyright (c) 2020, Amar Fayaz <amar@uni-bremen.de>
 ;;; All rights reserved.
 ;;; 
 ;;; Redistribution and use in source and binary forms, with or without
