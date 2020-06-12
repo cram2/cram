fce6354ccd45eaf984394fc0c9ec4e1bc034bf1b
Amar Fayaz <amar@uni-bremen.de>
2020-06-12 14:40:33 +0200

    [ik-interface]Added feature to resample base pose and passto collision check

--- cram_external_interfaces/cram_ik_interface/src/ik.lisp
+++ cram_external_interfaces/cram_ik_interface/src/ik.lisp
@@ -136,10 +136,12 @@ If not valid solution was found, returns NIL."
         (roslisp:set-debug-level nil old-debug-lvl)))))
 
 
-(defmacro find-ik-for ((cartesian-pose
+(defmacro find-ik-for ((goal-pose
                         base-link tip-link
                         seed-state-message
-                        &optional solution-valid-p)
+                        &optional
+                          solution-valid-p
+                          robot-base-pose)
                        &body body)
   "Method to find inverse kinematics for a given cartesian pose using resampling
 Syntax:
@@ -153,22 +155,27 @@ Syntax:
      (unwind-protect
           (progn
             (roslisp:set-debug-level nil 9)
-            (let ((current-pose ,cartesian-pose)
+            (let ((offseted-goal-pose ,goal-pose)
                   (base-link-evaled ,base-link)
                   (tip-link-evaled ,tip-link)
                   (seed-state-msg-evaled ,seed-state-message)
                   (solution-valid-p-evaled ,solution-valid-p)
+                  (offseted-robot-base-pose ,robot-base-pose)
                   (current-joint-states))
               (macrolet ((with-resampling (&whole whole-form
                                              (current-value
                                               resampling-axis
                                               upper-limit lower-limit
                                               resampling-step
-                                              &optional joint-name)
+                                              &optional
+                                                joint-name
+                                                (pose-joint nil))
                                            &body body)
                            (let ((form-length (length whole-form)))
                              ;;Formulating a list of joint values toample
-                             `(let ((sampling-values
+                             `(let ((old-offseted-robot-base-pose
+                                      offseted-robot-base-pose)
+                                    (sampling-values
                                       (append
                                        (list ,current-value)
                                        (loop
@@ -179,22 +186,35 @@ Syntax:
                                        (list ,lower-limit)))
                                     ;; Calculating the pose offset for the
                                     ;; corresponding joint value
-                                    (pseudo-pose (lambda (offset)
-                                                   (if (eq ,resampling-axis
-                                                           :theta)
-                                                       (cram-tf:rotate-pose
-                                                        current-pose
-                                                        :z (- ,current-value offset))
-                                                       (cram-tf:translate-pose
-                                                        current-pose
-                                                        (ecase ,resampling-axis
-                                                          (:x :x-offset)
-                                                          (:y :y-offset)
-                                                          (:z :z-offset))
-                                                        (- ,current-value offset))))))
+                                    (calculate-pseudo-pose
+                                      (lambda (original-pose offset
+                                               &optional (operand :-))
+                                        (let ((operation
+                                                (ecase operand
+                                                  (:- #'-)
+                                                  (:+ #'+))))
+                                          (if (eq ,resampling-axis
+                                                  :theta)
+                                              (cram-tf:rotate-pose
+                                               original-pose
+                                               :z (funcall operation
+                                                           (- offset ,current-value)))
+                                              (cram-tf:translate-pose
+                                               original-pose
+                                               (ecase ,resampling-axis
+                                                 (:x :x-offset)
+                                                 (:y :y-offset)
+                                                 (:z :z-offset))
+                                               (funcall operation
+                                                        (- offset ,current-value))))))))
                                 (loop
                                   for value in sampling-values
                                   do
+                                     (if ,pose-joint
+                                         (setf offseted-robot-base-pose
+                                               (funcall calculate-pseudo-pose
+                                                        offseted-robot-base-pose
+                                                        value :+)))
                                      (multiple-value-bind
                                            (solution-msg joint-values)
                                          ;; Checking if the arguments contain
@@ -211,24 +231,29 @@ Syntax:
                                              ;; value on nested calls). Revert
                                              ;; it back after the execution of
                                              ;; &body is completed.
-                                             (let ((old-current-pose
-                                                     current-pose))
-                                               (setf current-pose
+                                             (let ((old-offseted-goal-pose
+                                                     offseted-goal-pose))
+                                               (setf offseted-goal-pose
                                                      (funcall
-                                                      pseudo-pose value))
+                                                      calculate-pseudo-pose
+                                                      offseted-goal-pose
+                                                      value))
                                                (unwind-protect
                                                     (progn ,@body)
-                                                 (setf current-pose
-                                                       old-current-pose)))
+                                                 (setf offseted-goal-pose
+                                                       old-offseted-goal-pose)))
 
                                              ;; else make the ik-service call
                                              (call-ik-service
-                                              (funcall pseudo-pose value)
+                                              (funcall calculate-pseudo-pose
+                                                       offseted-goal-pose
+                                                       value)
                                               base-link-evaled
                                               tip-link-evaled
                                               seed-state-msg-evaled))
 
-                                       (if (and current-joint-states
+                                       (if ,joint-name
+                                        (if (and current-joint-states
                                                 (assoc ,joint-name
                                                        current-joint-states
                                                        :test #'equal))
@@ -239,7 +264,7 @@ Syntax:
                                                  value)
                                            (setf current-joint-states
                                                  (cons (list ,joint-name value)
-                                                       current-joint-states)))
+                                                       current-joint-states))))
                                        ;; Parse the solution
                                        (when (and solution-msg
                                                   (or (not
@@ -247,7 +272,8 @@ Syntax:
                                                       (funcall
                                                        solution-valid-p-evaled
                                                        solution-msg
-                                                       current-joint-states)))
+                                                       current-joint-states
+                                                       offseted-robot-base-pose)))
                                          (return (values solution-msg
                                                          (cons
                                                           (cons
@@ -255,7 +281,8 @@ Syntax:
                                                                ,joint-name
                                                                ,resampling-axis)
                                                            value)
-                                                          joint-values))))))))))
-
+                                                          joint-values))))
+                                       (setf offseted-robot-base-pose
+                                           old-offseted-robot-base-pose)))))))
                 ,@body)))
        (roslisp:set-debug-level nil old-debug-level))))
