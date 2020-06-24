20970469c14373034d98b134b95a1815ef18d200
Thomas Lipps <lipps.thomas@web.de>
2020-06-24 20:12:02 +0200

    --"--

--- cram_interfaces/cram_designator_specification/src/specs.lisp
+++ cram_interfaces/cram_designator_specification/src/specs.lisp
@@ -112,7 +112,7 @@
     (lisp-pred typep ?designator desig:action-designator)
     (member ?keyword-or-list-key (:gripper :arm :direction :grasp
                                   :left-grasp :right-grasp :camera :type
-                                  :context :link :configuration
+                                  :context :link :configuration :park-arms
                                   :left-configuration :right-configuration))
     (property-member (?keyword-or-list-key ?value) ?designator)
     (assert-type ?value (or keyword list) "ACTION SPEC:PROPERTY"))
