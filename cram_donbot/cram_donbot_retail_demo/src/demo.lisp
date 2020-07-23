3479ce44a5c99e01b9c04dfe07523257de3dc6fc
Gayane Kazhoyan <gayanek@gmail.com>
2020-07-23 23:57:51 +0200

    [donbot-demo] weird stuff happens when another package has a copy of the files

--- cram_donbot/cram_donbot_retail_demo/src/demo.lisp
+++ cram_donbot/cram_donbot_retail_demo/src/demo.lisp
@@ -27,7 +27,7 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :demo)
+(in-package :cram-donbot-retail-demo)
 
 (defun spawn-objects-on-small-shelf (&optional (spawn? t))
   (sb-ext:gc :full t)
