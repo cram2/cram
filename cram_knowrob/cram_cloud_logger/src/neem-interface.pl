b358d3b4337f827b7c1de342b445b50cc5479fb7
Sebastian Koralewski <koralewski.sebastian@gmail.com>
2020-08-04 09:17:53 +0200

    feat(neem-interface.pl): Log tf of detected object with timestamp.

--- cram_knowrob/cram_cloud_logger/src/neem-interface.pl
+++ cram_knowrob/cram_cloud_logger/src/neem-interface.pl
@@ -2,15 +2,15 @@
 :- use_module(library('db/mongo/client')).
 
 mem_episode_start(Action) :- tripledb_load('package://knowrob/owl/knowrob.owl',[graph(tbox),namespace(knowrob)]),tell([is_episode(Episode), is_action(Action), has_type(Task,ease_act:'PhysicalTask'), executes_task(Action,Task),is_setting_for(Episode,Action)]),!.
-mem_episode_stop(NeemPath) :- get_time(CurrentTime), atom_concat(NeemPath,'/',X1), atom_concat(X1,CurrentTime,X2), memorize(X2), mng_export_collection('tf_projection',X2), mng_export_collection('tf',X2),tripledb_whipe(),forall(mng_collection(roslog,Coll),mng_drop(roslog,Coll)).
+mem_episode_stop(NeemPath) :- get_time(CurrentTime), atom_concat(NeemPath,'/',X1), atom_concat(X1,CurrentTime,X2), memorize(X2),tripledb_drop(),forall(mng_collection(roslog,Coll),mng_drop(roslog,Coll)).
 mem_event_set_failed(Action) :- tell(action_failed(Action)).
 mem_event_set_succeeded(Action) :- tell(action_succeeded(Action)).
 mem_event_add_diagnosis(Situation, Diagnosis) :- tell(satisfies(Situation, Diagnosis)).
 add_subaction_with_task(Action,SubAction,TaskType) :- tell([is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction)]), !.
 mem_event_end(Event) :- get_time(CurrentTime),tell([is_time_interval(TimeInterval),holds(Event,dul:hasTimeInterval,TimeInterval), occurs(Event) until CurrentTime]),!.
 mem_event_begin(Event) :- get_time(CurrentTime),tell([is_time_interval(TimeInterval),holds(Event,dul:hasTimeInterval,TimeInterval), occurs(Event) since CurrentTime]),!.
-%belief_perceived_at(ObjectType, Frame, Object) :- tell(has_type(Object,ObjectType),is_at(Object,Frame)).
-belief_perceived_at(ObjectType, Frame, Object) :- tell(has_type(Object,ObjectType)).
+belief_perceived_at(ObjectType, Frame, Object) :- get_time(CurrentTime),tell([has_type(Object,ObjectType),is_at(Object,Frame) since CurrentTime]).
+%belief_perceived_at(ObjectType, Frame, Object) :- tell(has_type(Object,ObjectType)).
 add_participant_with_role(Action, ObjectId, RoleType) :- tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0]]).
 add_parameter(Task,ParameterType,RegionType) :- tell([has_type(Parameter, ParameterType), has_type(Region,RegionType),has_assignment(Parameter,Region) during [0.0,0.1], has_parameter(Task, Parameter)]).
 add_grasping_parameter(Action,GraspingOrientationType) :- ask(executes_task(Action, Task)), tell([has_type(GraspingOrientation,GraspingOrientationType), has_type(GraspingOrientationConcept,'http://www.ease-crc.org/ont/EASE.owl#GraspingOrientation'), has_parameter(Task,GraspingOrientationConcept),holds(GraspingOrientationConcept, dul:classifies, GraspingOrientation),has_region(Action,GraspingOrientation)]),!.
