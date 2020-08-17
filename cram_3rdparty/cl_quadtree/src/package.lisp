4163f3220fb0d654ed106bc3525333c52124cec7
Gayane Kazhoyan <gayanek@gmail.com>
2020-08-17 12:14:41 +0200

    cleanup for PR #145

--- cram_3rdparty/cl_quadtree/src/package.lisp
+++ cram_3rdparty/cl_quadtree/src/package.lisp
@@ -1,20 +1,20 @@
 ;;;
 ;;; Copyright (c) 2019, Thomas Lipps <tlipps@uni-bremen.de>
 ;;; All rights reserved.
-;;; 
+;;;
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions are met:
-;;; 
+;;;
 ;;;     * Redistributions of source code must retain the above copyright
 ;;;       notice, this list of conditions and the following disclaimer.
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of the Intelligent Autonomous Systems Group/
-;;;       Technische Universitaet Muenchen nor the names of its contributors 
-;;;       may be used to endorse or promote products derived from this software 
-;;;       without specific prior written permission.
-;;; 
+;;;     * Neither the name of the Institute for Artificial Intelligence/
+;;;       Universitaet Bremen nor the names of its contributors may be used to
+;;;       endorse or promote products derived from this software without
+;;;       specific prior written permission.
+;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,14 +28,22 @@
 ;;; POSSIBILITY OF SUCH DAMAGE.
 ;;;
 
-(defsystem quadtree
-  :version "0.1"
-  :author "Thomas Lipps"
-  :depends-on (alexandria
-               cl-transforms)
-  :components ((:module 
-                "src"
-                :components
-                ((:file "package")
-                 (:file "quadtree" :depends-on ("package"))))))
+(in-package :cl-user)
 
+(defpackage cl-quadtree
+  (:nicknames :quadtree)
+  (:use :cl :cl-transforms)
+  (:export #:make-quadtree-internal
+           #:make-quadtree
+           #:matrix->quadtree
+           #:boundary
+           #:insert
+           #:query
+           #:clear-quadtree
+           #:get-in-quadtree
+           #:point-intersect-p
+           #:merge-quadtrees
+           #:object-equal
+           #:same-coords-as
+           #:normalize-quadtree
+           #:quadtree-map))
