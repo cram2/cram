df7277b5a1bb4b09c443321d766e7832b465e637
Gayane Kazhoyan <gayanek@gmail.com>
2020-09-24 01:04:05 +0200

    [giskard][env-manip] follow hand with gaze + allow collisions with all items attached to kitchen

--- cram_external_interfaces/cram_giskard/src/environment-manipulation-goals.lisp
+++ cram_external_interfaces/cram_giskard/src/environment-manipulation-goals.lisp
@@ -43,14 +43,18 @@
                   open-or-close arm handle-link joint-state)
                  (make-base-velocity-constraint
                   *base-max-velocity-slow-xy* *base-max-velocity-slow-theta*)
-                 (make-avoid-joint-limits-constraint))
+                 (make-avoid-joint-limits-constraint)
+                 (make-head-pointing-at-hand-constraint arm))
    :collisions (make-constraints-vector
                 ;; (make-avoid-all-collision)
-                (ecase open-or-close
-                  (:open (make-allow-hand-collision
-                          (list arm) (rob-int:get-environment-name) handle-link))
-                  (:close (make-allow-arm-collision
-                           (list arm) (rob-int:get-environment-name)))))))
+                (append
+                 (make-allow-collision-with-environment-attachments)
+                 (list
+                  (ecase open-or-close
+                    (:open (make-allow-hand-collision
+                            (list arm) (rob-int:get-environment-name) handle-link))
+                    (:close (make-allow-arm-collision
+                             (list arm) (rob-int:get-environment-name)))))))))
 
 (defun call-environment-manipulation-action (&key
                                                action-timeout
@@ -72,119 +76,3 @@
                                     (member status '(:preempted :aborted :timeout)))
                             (make-instance 'common-fail:manipulation-low-level-failure
                               :description "Giskard action failed.")))))
-
-
-#+the-plan
-(
- (setf (btr:joint-state (btr:get-environment-object) "iai_fridge_door_joint")
-       0.0)
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type parking-arms))))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type releasing)
-              (gripper right))))
- (giskard::call-grasp-bar-action :arm :right :bar-length 0.8)
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type gripping)
-              (gripper right))))
- (giskard::call-environment-manipulation-action :open-or-close :open
-                                                :arm :right
-                                                :handle-link :iai-fridge-door-handle)
- (giskard:call-giskard-cartesian-action
-  :goal-pose-right (cl-transforms-stamped:make-pose-stamped "r_gripper_tool_frame" 0.0 (cl-transforms:make-3d-vector -0.1 0 0) (cl-transforms:make-identity-rotation))
-  :collision-mode :allow-all)
- (coe:on-event (make-instance 'cpoe:robot-state-changed))
- (setf (btr:joint-state (btr:get-environment-object)
-                        "iai_fridge_door_joint") 1.45)
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- (giskard::call-environment-manipulation-action :open-or-close :close
-                                                :arm :right
-                                                :handle-link :iai-fridge-door-handle)
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type releasing)
-              (gripper right))))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type parking-arms))))
- (setf (btr:joint-state (btr:get-environment-object) "iai_fridge_door_joint")
-       0.0)
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- )
-
-#+the-dishwasher-plan
-(
- (setf (btr:joint-state (btr:get-environment-object)
-                        "sink_area_dish_washer_door_joint")
-       0.0)
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type parking-arms))))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type releasing)
-              (gripper right))))
- (giskard::call-grasp-bar-action
-  :arm :right
-  :bar-axis (cl-transforms-stamped:make-vector-stamped
-             "iai_kitchen/sink_area_dish_washer_door_handle" 0.0
-             (cl-transforms:make-3d-vector 0 1 0))
-  :bar-perpendicular-axis
-  (cl-transforms-stamped:make-vector-stamped
-   "iai_kitchen/sink_area_dish_washer_door_handle" 0.0
-   (cl-transforms:make-3d-vector 0 0 1))
-  :bar-center (cl-transforms-stamped:make-point-stamped
-               "iai_kitchen/sink_area_dish_washer_door_handle" 0.0
-               (cl-transforms:make-3d-vector 0.0 0 0))
-  :bar-length 0.2)
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type gripping)
-              (gripper right))))
- (giskard::call-environment-manipulation-action
-  :open-or-close :open
-  :arm :right
-  :handle-link :sink-area-dish-washer-door-handle
-  :joint-state (cram-math:degrees->radians 35))
- (coe:on-event (make-instance 'cpoe:robot-state-changed))
- (setf (btr:joint-state (btr:get-environment-object)
-                        "sink_area_dish_washer_door_joint")
-       (cram-math:degrees->radians 35))
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- (giskard::call-environment-manipulation-action
-  :open-or-close :close
-  :arm :right
-  :handle-link :sink-area-dish-washer-door-handle)
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type releasing)
-              (gripper right))))
- (pr2-pms:with-real-robot
-   (exe:perform
-    (desig:an action
-              (type parking-arms))))
- (setf (btr:joint-state (btr:get-environment-object)
-                        "sink_area_dish_washer_door_joint")
-       (cram-math:degrees->radians 0))
- (btr-belief::publish-environment-joint-state
-  (btr:joint-states (btr:get-environment-object)))
- )
