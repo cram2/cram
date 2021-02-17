f1c3906f08f8775e02fdec50510e46886dacbb33
Sebastian Koralewski <seba@cs.uni-bremen.de>
2021-02-17 16:08:29 +0100

    Update role logging

--- cram_knowrob/cram_cloud_logger/src/neem-interface.pl
+++ cram_knowrob/cram_cloud_logger/src/neem-interface.pl
@@ -12,14 +12,14 @@ AgentOwlIndiName,
 AgentUrdf) :- 
     retractall(execution_agent(_)),tf_logger_disable, tripledb_drop(),forall(mng_collection(roslog,Coll),mng_drop(roslog,Coll)), tf_logger_enable,tf_mem_clear,
     tripledb_load('package://knowrob/owl/knowrob.owl',[graph(tbox),namespace(knowrob)]),
-    %%tripledb_load(EnvOwl),
-    %%tripledb_load(AgentOwl),
-    %%urdf_load(AgentOwlIndiName, AgentUrdf, [load_rdf]),
-    %%urdf_load(EnvOwlIndiName, EnvUrdf, [load_rdf,prefix(EnvUrdfPrefix)]),
+    tripledb_load(EnvOwl),
+    tripledb_load(AgentOwl),
+    urdf_load(AgentOwlIndiName, AgentUrdf, [load_rdf]),
+    urdf_load(EnvOwlIndiName, EnvUrdf, [load_rdf,prefix(EnvUrdfPrefix)]),
     assertz(execution_agent(AgentOwlIndiName)),
     execution_agent(Agent),
     tell([is_episode(Episode), is_action(Action), has_type(Task,soma:'PhysicalTask'), 
-            executes_task(Action,Task),is_setting_for(Episode,Action), is_performed_by(Action,Agent),has_type(Role, soma:'AgentRole'), has_role(Agent,Role)]),notify_synchronize(event(Action)),!.
+            executes_task(Action,Task),is_setting_for(Episode,Action), is_performed_by(Action,Agent), has_type(Role, soma:'AgentRole'), has_role(Agent,Role)]),notify_synchronize(event(Action)),!.
     %%executes_task(Action,Task),is_setting_for(Episode,Action),is_setting_for(Episode,Agent)]),notify_synchronize(event(Action)),!.
 
 %is_recording_episode(Result) :- assertz(cramEpisodeName('None')), retract(cramEpisodeName('None')), (cramEpisodeName(Name) -> Result = Name ; Result = 'NoName').
@@ -29,25 +29,26 @@ mem_event_set_failed(Action) :- tell(action_failed(Action)).
 mem_event_set_succeeded(Action) :- tell(action_succeeded(Action)).
 mem_event_add_diagnosis(Situation, Diagnosis) :- tell(satisfies(Situation, Diagnosis)).
 
-add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent), tell([has_type(Role, soma:'AgentRole'), has_role(Agent,Role), is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction),is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
-%add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent), tell([has_type(Role, soma:'AgentRole'), has_role(Agent,Role), is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction),task_role(Task, Role),is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
+%add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent), tell([has_type(Role, soma:'AgentRole'), has_role(Agent,Role), is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction),is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
 
-%%add_subaction_with_task(Action,SubAction,TaskType) :- tell([is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction)]),notify_synchronize(event(Event)), !.
+add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent),tell([is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction), is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
 
-mem_event_end(Event) :- get_time(CurrentTime),ask(triple(Event,dul:'hasTimeInterval',TimeInterval)),tripledb_forget(TimeInterval, soma:'hasIntervalEnd', _),tell(holds(TimeInterval, soma:'hasIntervalEnd', CurrentTime)),!.
+mem_event_end(Event) :- execution_agent(Agent),get_time(CurrentTime), ask([triple(Event,dul:'hasTimeInterval',TimeInterval), triple(TimeInterval,soma:'hasIntervalBegin', Start), executes_task(Event,Task)]),tripledb_forget(TimeInterval, soma:'hasIntervalEnd', _),tell([holds(TimeInterval, soma:'hasIntervalEnd', CurrentTime),has_type(Role, soma:'AgentRole'), has_role(Agent,Role) during Event,task_role(Task, Role)]),!.
 
 mem_event_begin(Event) :- get_time(CurrentTime),tell(occurs(Event) since CurrentTime),!.
 
-%belief_perceived_at(ObjectType, Frame, Object) :- get_time(CurrentTime),tell([has_type(Object,ObjectType),is_at(Object,Frame) since CurrentTime]).
+%belief_perceived_at(ObjectType, Frame, Object) :- get_time(CurrentTime),execution_agent(Agent),tell([has_type(Object,ObjectType),is_at(Object,Frame) since CurrentTime]).
 
 belief_perceived_at(ObjectType, Mesh, Rotation, Object) :- tell([has_type(Object,ObjectType),has_type(ShapeRegion, soma:'MeshShape'), has_type(Shape, soma:'Shape'), triple(Object, soma:'hasShape', Shape), triple(Shape, dul:'hasRegion', ShapeRegion), triple(ShapeRegion, soma:'hasFilePath', Mesh),has_type(Origin,soma:'Origin'),triple(ShapeRegion,'http://knowrob.org/kb/urdf.owl#hasOrigin',Origin),triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasPositionVector', term([0.0,0.0,0.0])),triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasOrientationVector',term(Rotation))]).
 
 belief_perceived_at(ObjectType, Object) :- tell([has_type(Object,ObjectType)]).
 
 
-%add_participant_with_role(Action, ObjectId, RoleType) :- ask(executes_task(Action, Task)), tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0],task_role(Task, Role)]).
+add_participant_with_role(Action, ObjectId, RoleType) :- 
+ask([executes_task(Action, Task),triple(Event,dul:'hasTimeInterval',TimeInterval),triple(TimeInterval,soma:'hasIntervalBegin',Start),triple(TimeInterval,soma:'hasIntervalEnd',End)]), 
+tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during Action,task_role(Task, Role)]).
 
-add_participant_with_role(Action, ObjectId, RoleType) :- ask(executes_task(Action, Task)), tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0]]).
+%add_participant_with_role(Action, ObjectId, RoleType) :- ask(executes_task(Action, Task)), tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0]]).
 
 add_parameter(Task,ParameterType,RegionType) :- tell([has_type(Parameter, ParameterType), has_type(Region,RegionType),has_assignment(Parameter,Region) during [0.0,0.1], has_parameter(Task, Parameter)]).
 
