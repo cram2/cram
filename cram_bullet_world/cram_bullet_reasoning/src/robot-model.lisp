08df4dc02cde2645845457c51a62647e37b58621
Gayane Kazhoyan <gayanek@gmail.com>
2022-03-29 23:09:33 +0200

    [btr] make robot and environment urdfs half-transparent

--- cram_bullet_world/cram_bullet_reasoning/src/robot-model.lisp
+++ cram_bullet_world/cram_bullet_reasoning/src/robot-model.lisp
@@ -336,9 +336,13 @@ Otherwise, the attachment is only used as information but does not affect the wo
                                                      (when (and (cl-urdf:visual link)
                                                                 (cl-urdf:material
                                                                  (cl-urdf:visual link)))
-                                                       (cl-urdf:color
-                                                        (cl-urdf:material
-                                                         (cl-urdf:visual link))))
+                                                       (let ((urdf-color
+                                                               (cl-urdf:color
+                                                                (cl-urdf:material
+                                                                 (cl-urdf:visual link)))))
+                                                         (when urdf-color
+                                                           (setf (nth 3 urdf-color) 0.5)
+                                                           urdf-color)))
                                                      (let ((some-gray (/ (+ (random 5) 3) 10.0)))
                                                        (list some-gray some-gray some-gray
                                                              (or *robot-model-alpha* 1.0)))))
