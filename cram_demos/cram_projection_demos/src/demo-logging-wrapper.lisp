135bddf7909777ce43c33b40df2938b2719ec4fb
HaSu <hawkin@uni-bremen.de>
2024-02-23 13:01:36 +0100

    [proj-demos] adding wrapper functionality for real robot logging

--- cram_demos/cram_projection_demos/src/demo-logging-wrapper.lisp
+++ cram_demos/cram_projection_demos/src/demo-logging-wrapper.lisp
@@ -20,17 +20,34 @@
   ;; startup ROS
   (roslisp-utilities:startup-ros)
   ;;change origin of robot. otherwise robot might get stuck in the wall...
-  (btr-utils:move-robot '((1.5 1.5 0)(0 0 0 1)))
-  )
+  (btr-utils:move-robot '((1.5 1.5 0)(0 0 0 1))))
 
-(defun pouring-neem ()
+(defun setup-pr2-logging ()
+  ;;disbale tf broadcasting in order to not confuse tf
+  (setf cram-tf:*tf-broadcasting-topic* "tf_bullet")
+  (setf cram-tf:*tf-broadcasting-enabled* NIL)
+  
+  ;;set logging dependencies
+  (setf ccl::*environment-owl* "'package://iai_apartment/owl/iai-apartment.owl'")
+  (setf ccl::*environment-owl-individual-name* "'http://knowrob.org/kb/iai-apartment.owl#apartment_root'")
+  (setf ccl::*environment-urdf* "'package://iai_apartment/urdf/iai-apartment.urdf'")
+  (setf ccl::*environment-urdf-prefix* "'iai_apartment/'")
+  (setf ccl::*agent-owl* "'package://knowrob/owl/robots/PR2.owl'")
+  (setf ccl::*agent-owl-individual-name* "'http://knowrob.org/kb/PR2.owl#PR2_0'")
+  (setf ccl::*agent-urdf* "'package://knowrob/urdf/pr2.urdf'")
+
+  ;(setf ccl::*episode-name* "PR2 Pouring")
+  ;; startup ROS
+  (roslisp-utilities:startup-ros))
+
+(defun pouring-neem-simulated ()
   (urdf-proj:with-simulated-robot
     (ccl::start-episode)
     (apartment-demo-merged :step 0)
     (sleep 3)
     (ccl::stop-episode)))
   
-(defun pouring-neems (repeats)
+(defun pouring-neems-simulated (repeats)
   (urdf-proj:with-simulated-robot
     (ccl::start-episode)
     (dotimes (n repeats)
@@ -38,3 +55,11 @@
       (format t "repeating for the ~a~% time" n)
       (sleep 3))
     (ccl::stop-episode)))
+
+;; had to be commented out due to compilation reasons when running with bullet only
+;; run within:
+;;  (pr2-pms:with-real-robot
+(defun pouring-neem-real ()
+  (ccl::start-episode)
+  (apartment-demo-merged :step 0)
+  (ccl::stop-episode))
